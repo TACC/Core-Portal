9c5cf8bdd8e6a39bd8f102c55e5bed75042842c5
Joe Meiring <jmeiring@tacc.utexas.edu>
2018-01-26 16:59:04 -0600

    client side and some settings

--- server/portal/apps/auth/urls.py
+++ server/portal/apps/auth/urls.py
@@ -12,33 +12,8 @@ from django.utils.translation import ugettext_lazy as _
 from portal.apps.auth import views
 
 urlpatterns = [
-    url(r'^$', views.login_options, name='login'),
     url(r'^logged-out/$', views.logged_out, name='logout'),
     url(r'^agave/$', views.agave_oauth, name='agave_oauth'),
     url(r'^agave/callback/$', views.agave_oauth_callback, name='agave_oauth_callback'),
     url(r'^agave/session-error/$', views.agave_session_error, name='agave_session_error'),
 ]
-
-
-def menu_items(**kwargs):
-    if 'type' in kwargs and kwargs['type'] == 'account':
-        return [
-            {
-                'label': _('Login'),
-                'url': reverse('login'),
-                'children': [],
-                'visible': False
-            },
-            {
-                'label': _('Login'),
-                'url': reverse('designsafe_auth:login'),
-                'children': [],
-                'visible': False
-            },
-            {
-                'label': _('Agave'),
-                'url': reverse('designsafe_auth:agave_session_error'),
-                'children': [],
-                'visible': False
-            }
-        ]
--- server/portal/static/README.md
+++
@@ -1,4 +0,0 @@
-# BASE STATIC FILES
-
-This directory is meant to hold all custom static files necessary.
-Meaning, any front end scripting file or image needed for the project which is meant to be used across the project or does not falls into a specific django app.
