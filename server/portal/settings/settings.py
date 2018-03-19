bfd3e66428b4062897c789520248dbfbd4ff207c
taoteg <taoteg@gmail.com>
2018-03-19 14:41:43 -0500

    Cleanup on settings.

--- server/portal/settings/settings.py
+++ server/portal/settings/settings.py
@@ -18,6 +18,7 @@ from kombu import Exchange, Queue
 
 
 logger = logging.getLogger(__file__)
+
 #pylint: disable=invalid-name
 gettext = lambda s: s
 #pylint: enable=invalid-name
@@ -329,7 +330,6 @@ SETTINGS: CELERY
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-#BROKER_URL = 'amqp://designsafe:pwd@rabbitmq:5672//'
 _BROKER_URL_PROTOCOL = 'amqp://'
 _BROKER_URL_USERNAME = settings_secret._BROKER_URL_USERNAME
 _BROKER_URL_PWD = settings_secret._BROKER_URL_PWD
@@ -341,7 +341,6 @@ CELERY_BROKER_URL = ''.join([_BROKER_URL_PROTOCOL,_BROKER_URL_USERNAME, ':',
                       _BROKER_URL_PWD, '@', _BROKER_URL_HOST, ':',
                       _BROKER_URL_PORT, '/',_BROKER_URL_VHOST])
 
-#BROKER_URL = 'redis://redis:6379/0'
 _RESULT_BACKEND_PROTOCOL = 'redis://'
 _RESULT_BACKEND_USERNAME = settings_secret._RESULT_BACKEND_USERNAME
 _RESULT_BACKEND_PWD = settings_secret._RESULT_BACKEND_PWD
@@ -359,7 +358,6 @@ CELERY_RESULT_SERIALIZER = 'json'
 CELERYD_HIJACK_ROOT_LOGGER = False
 CELERYD_LOG_FORMAT = '[DJANGO] $(processName)s %(levelname)s %(asctime)s %(module)s '\
                      '%(name)s.%(funcName)s:%(lineno)s: %(message)s'
-
 #CELERY_ANOTATIONS = {'designsafe.apps.api.tasks.reindex_agave': {'time_limit': 60 * 15}}
 CELERY_DEFAULT_EXCHANGE_TYPE = 'direct'
 CELERY_QUEUES = (
