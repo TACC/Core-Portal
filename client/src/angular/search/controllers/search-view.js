e15d359a058c3ccae574179870d035749f7653c6
Jake Rosenberg <jrosenberg@tacc.utexas.edu>
2018-04-02 17:06:14 -0500

    search works for files

--- client/src/angular/search/controllers/search-view.js
+++ client/src/angular/search/controllers/search-view.js
@@ -1,26 +1,73 @@
 
 export default class SearchViewCtrl {
 
-  constructor (SearchService, $window, $location, $state) {
+  constructor (SearchService, $scope, $window, $location, $state) {
     'ngInject';
-    this.SearchService = SearchService;
-    this.$window = $window;
-    this.$state = $state;
-    this.$location = $location;
-    this.query = this.$location.search();
-    this.text = this.query.q;
-    if (this.text) this.search(this.text);
+    $scope.SearchService = SearchService;
+    $scope.data = {}
+    $scope.$window = $window;
+    $scope.$state = $state;
+    $scope.$location = $location;
+    $scope.query = $scope.$location.search();
+    $scope.data.text = $scope.query.q;
+    $scope.page_num = 0
+    $scope.max_pages = 1;
+    $scope.offset = 0;
+    $scope.limit = 10;
+    $scope.total_hits = 0;
+    $scope.hits = {};
+    $scope.data.type_filter = 'cms'
+    $scope.prettyFilterName = {
+      'cms': 'Web Content', 
+      'private_files': 'My Data' ,
+      'published': 'Published Projects', 
+      'public_files': 'Public Files'
+    }
+
+    if ($scope.text) $scope.SearchService.search($scope.text);
+
+    $scope.next = function () {
+      $scope.page_num = $scope.page_num + 1;
+      $scope.search();
+    };
+
+    $scope.prev = function () {
+      $scope.page_num--;
+      if ($scope.page_num < 0) $scope.page_num = 0;
+      $scope.search();
+    };
+
+    $scope.filter = function(ftype) {
+      $scope.data.type_filter = ftype;
+      $scope.page_num = 0;
+      $scope.search();
+    }
+
+    $scope.search = function(reset) {
+      if (reset) {
+        $scope.page_num = 0;
+      }
+      $scope.offset = $scope.page_num * $scope.limit
+      $scope.SearchService.search($scope.data.text, $scope.limit, $scope.offset, $scope.data.type_filter).then( (resp) => {
+        $scope.data.search_results = resp.response
+        //this.hits = resp.hits;
+        console.log('in search callback.')
+        console.log(resp);
+        //$scope.data.hits = resp.hits;
+        $scope.total_hits = $scope.data.search_results.total_hits
+        $scope.max_pages = Math.ceil($scope.data.search_results.total_hits / $scope.limit)
+      });
+    };
+  
+    $scope.makeUrl = function(listing) {
+      let url = $scope.$state.href('db.communityData', {systemId: listing.system, filePath:listing.path});
+      //url='hello'
+      return url;
+    }
+
+
   }
 
-  search(text) {
-    this.SearchService.search(text).then( (resp) => {
-      this.results = resp;
-    });
-  }
-
-  makeUrl(listing) {
-    let url = this.$state.href('db.communityData', {systemId: listing.system, filePath:listing.path});
-    return url;
-  }
+  
 
 }
