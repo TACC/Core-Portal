efea061799feb6e21bdaacdb62c512a2f652a9f0
Josue Balandrano Coronel <xirdneh@gmail.com>
2018-05-05 20:39:55 -0500

    Add component for modal

---
+++ client/src/angular/workbench/components/index.js
@@ -0,0 +1,7 @@
+import SystemPushKeysModal from './systems/push-keys.modal.js'
+
+let mod = angular.module('portal.workbench.components', []);
+
+mod.component('systemPushKeysModal', SystemPushKeysModal);
+
+export default mod
