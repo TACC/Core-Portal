9fb93f71a420c28f2767bdaf0f549aee0dad49f6
Joe Meiring <jmeiring@tacc.utexas.edu>
2018-05-24 14:25:19 -0500

    webpack config for prod/dev

--- client/src/angular/workspace/controllers/jobs-status.js
+++ client/src/angular/workspace/controllers/jobs-status.js
@@ -1,91 +1,63 @@
 
-  function JobsStatusCtrl($scope, $controller, $rootScope, $uibModal, Jobs) {
-    'ngInject';
+function JobsStatusCtrl($scope, $controller, $rootScope, $uibModal, Jobs) {
+  'ngInject';
 
-    $controller('WorkspacePanelCtrl', {$scope: $scope});
-    $scope.data = {
-      hasMoreJobs: true,
-      limit: 10
-    };
+  $controller('WorkspacePanelCtrl', {$scope: $scope});
+  $scope.data = {
+    hasMoreJobs: true,
+    limit: 10
+  };
 
-    $scope.jobDetails = function(job) {
-      Jobs.get(job.id).then(function(resp) {
-        $scope.data.interactive = false;
-        if(resp.status === 'RUNNING' && resp._embedded.metadata) {
-          for(var i=0; i < resp._embedded.metadata.length; i++){
-            if(resp._embedded.metadata[i].name === 'interactiveJobDetails') {
-              var meta = resp._embedded.metadata[i];
-              $scope.data.interactive = true;
-              $scope.data.connection_address = meta.value.extra.target_uri;
-              break;
-            }
+  $scope.jobDetails = function(job) {
+    Jobs.get(job.id).then(function(resp) {
+      $scope.data.interactive = false;
+      if(resp.status === 'RUNNING' && resp._embedded.metadata) {
+        for(var i=0; i < resp._embedded.metadata.length; i++){
+          if(resp._embedded.metadata[i].name === 'interactiveJobDetails') {
+            var meta = resp._embedded.metadata[i];
+            $scope.data.interactive = true;
+            $scope.data.connection_address = meta.value.extra.target_uri;
+            break;
           }
         }
+      }
 
-        $uibModal.open({
-          templateUrl: '/static/src/angular/workspace/templates/job-details-modal.html',
-          controller: 'JobDetailsModalCtrl',
-          scope: $scope,
-          resolve: {
-            job: resp,
-          }
-        });
+      $uibModal.open({
+        templateUrl: '/static/src/angular/workspace/templates/job-details-modal.html',
+        controller: 'JobDetailsModalCtrl',
+        scope: $scope,
+        resolve: {
+          job: resp,
+        }
       });
-    };
-
-    $scope.refresh = function() {
-      $scope.data.loading = true;
-      Jobs.list({limit: $scope.data.limit}).then(
-        function(resp) {
-          $scope.data.loading = false;
-          $scope.data.jobs = resp;
-        });
-    };
-    $scope.refresh();
-
-    $scope.loadMore = function() {
-      $scope.data.limit += 10;
-      $scope.refresh();
-    };
-
-    $scope.$on('job-submitted', function(e, data) {
-      $scope.refresh();
     });
-
-    $scope.$on('jobs-refresh', function(e, data) {
-      $scope.refresh();
-    });
-
-    /*
-     * Receives the webhook notification from the vnc-type job and opens the
-     * modal dialog in the workspace letting the user know their job is ready
-     * to connect to.
-     */
-    // $scope.$on('ds.wsBus:default', function update_job(e, msg){
-    // $scope.$on('ds.wsBus:notify', function update_job(e, msg){
-    //   if('event_type' in msg && msg.event_type === 'VNC') {
-    //     $uibModal.open({
-    //       templateUrl: 'local/vncjob-details-modal.html',
-    //       controller: 'VNCJobDetailsModalCtrl',
-    //       scope: $scope,
-    //       resolve: {
-    //         msg: msg
-    //       }
-    //     });
-    //   }
-    //   else {
-    //     for (var i=0; i < $scope.data.jobs.length; i++){
-    //         if ($scope.data.jobs[i]['id'] == msg.extra.id) {
-    //           $scope.data.jobs[i]['status'] = msg.extra.status;
-    //           $scope.$apply();
-    //           break;
-    //         }
-    //     }
-    //   }
-    // });
-
-
   };
+
+  $scope.refresh = function() {
+    $scope.data.loading = true;
+    Jobs.list({limit: $scope.data.limit}).then(
+      function(resp) {
+        $scope.data.loading = false;
+        $scope.data.jobs = resp;
+      });
+  };
+  $scope.refresh();
+
+  $scope.loadMore = function() {
+    $scope.data.limit += 10;
+    $scope.refresh();
+  };
+
+  $scope.$on('job-submitted', function(e, data) {
+    $scope.refresh();
+  });
+
+  $scope.$on('jobs-refresh', function(e, data) {
+    $scope.refresh();
+  });
+
+
+};
 
 function JobDetailsModalCtrl($scope, $uibModalInstance, $http, Jobs, job) {
   'ngInject';
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,86 +1,97 @@
 // webpack plugins
 // const CommonsChunkPlugin = require('webpack/lib/optimize/CommonsChunkPlugin');
-const ngAnnotatePlugin = require('ng-annotate-webpack-plugin');
+// const ngAnnotatePlugin = require('ng-annotate-webpack-plugin');
 const LiveReloadPlugin = require('webpack-livereload-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const webpack = require('webpack');
 
+// console.log(ngAnnotatePlugin)
 
-module.exports = {
-  devtool: 'cheap-module-eval-source-map',
-  entry: './src/index.js',
-  output: {
-    publicPath: '/static/build/',
-    path: __dirname + '/build',
-    filename: "bundle.[hash].js",
+module.exports = function(env, argv){
+  var smap = false;
+  env == 'prod' ? smap = false : smap='cheap-module-eval-source-map';
+  return {
+    devtool: smap,
+    entry: './src/index.js',
+    output: {
+      publicPath: '/static/build/',
+      path: __dirname + '/build',
+      filename: "bundle.[hash].js",
 
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: ExtractTextPlugin.extract({use: 'css-loader'}),
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: [
-          {loader: 'babel-loader'},
-          {
-            loader: 'eslint-loader',
-            options: {
-             failonError: true 
+    },
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: ExtractTextPlugin.extract({use: 'css-loader'}),
+        },
+        {
+          test: /\.js$/,
+          use: [
+            {
+              loader: 'babel-loader',
+              options: {
+                "presets": ["es2015"],
+                "plugins": ["angularjs-annotate"]
+              }
+            },
+            {
+              loader: 'eslint-loader',
+              options: {
+               failonError: true
+             }
            }
-         }
-       ]},
-      {
-        test: /\.html$/,
-        exclude: /node_modules/,
-        loader: 'html-loader'
-      },
-      // {
-      //   test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-      //   loader: "file-loader?limit=10000&mimetype=application/font-woff"
-      // },
-      // {
-      //   test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-      //   loader: "file-loader"
-      // },
+         ]
+        },
+        {
+          test: /\.html$/,
+          exclude: /node_modules/,
+          loader: 'html-loader'
+        },
+        // {
+        //   test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        //   loader: "file-loader?limit=10000&mimetype=application/font-woff"
+        // },
+        // {
+        //   test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        //   loader: "file-loader"
+        // },
 
+      ]
+    },
+    plugins: [
+      new CleanWebpackPlugin(['./build', '../server/portal/templates/base.html'], { watch: true}),
+      // new ngAnnotatePlugin({add:true}),
+      new LiveReloadPlugin(),
+      new HtmlWebpackPlugin(
+        {
+          inject : false,
+          template : '../server/portal/templates/base.j2',
+          filename: '../../server/portal/templates/base.html',
+        }
+      ),
+
+
+      new ExtractTextPlugin({
+  		  filename: "bundle.[hash].css",
+        allChunks: true,
+      }),
+      // new webpack.optimize.CommonsChunkPlugin({
+      //   name: 'vendor',
+      //   filename: 'vendor.[hash].js',
+      //   // minChunks: 2
+      //   minChunks (module) {
+      //     return module.context &&
+      //            module.context.indexOf('node_modules') >= 0;
+      //   }
+      // }),
+      new webpack.ProvidePlugin({
+         jQuery: 'jquery',
+         $: 'jquery',
+         jquery: 'jquery'
+     })
     ]
-  },
-  plugins: [
-    new CleanWebpackPlugin(['./build', '../server/portal/templates/base.html'], { watch: true}),
-    new ngAnnotatePlugin({add:true}),
-    new LiveReloadPlugin(),
-    new HtmlWebpackPlugin(
-      {
-        inject : false,
-        template : '../server/portal/templates/base.j2',
-        filename: '../../server/portal/templates/base.html',
-      }
-    ),
-
-
-    new ExtractTextPlugin({
-		  filename: "bundle.[hash].css",
-      allChunks: true,
-    }),
-    // new webpack.optimize.CommonsChunkPlugin({
-    //   name: 'vendor',
-    //   filename: 'vendor.[hash].js',
-    //   // minChunks: 2
-    //   minChunks (module) {
-    //     return module.context &&
-    //            module.context.indexOf('node_modules') >= 0;
-    //   }
-    // }),
-    new webpack.ProvidePlugin({
-       jQuery: 'jquery',
-       $: 'jquery',
-       jquery: 'jquery'
-   })
-  ]
+  };
 };
