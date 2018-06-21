3967cfd7b5079d2df197968a3fed6a40ec68fc80
Jarosenb <jrosenberg@tacc.utexas.edu>
2018-06-21 11:19:22 -0500

    Prevent duplicate indexing for files.

--- server/portal/apps/search/tasks.py
+++ server/portal/apps/search/tasks.py
@@ -1,4 +1,5 @@
 import logging
+import urllib
 import os
 from django.conf import settings
 from django.contrib.auth.models import User
@@ -6,23 +7,34 @@ from celery import shared_task
 from agavepy.agave import Agave
 from elasticsearch_dsl import Q, Search
 from portal.libs.elasticsearch.docs.base import IndexedFile
+from portal.libs.elasticsearch.exceptions import DocumentNotFound
 logger = logging.getLogger(__name__)
 
 # Crawl and index agave files
-@shared_task(queue='indexing', rate_limit="50/m", time_limit=60)
-def agave_indexer(systemId, username=None, filePath='/', recurse=True):
+@shared_task(bind=True)
+def agave_indexer(self, systemId, username=None, filePath='/', recurse=True):
     try:
         # Get a listing and index the results.
         if username != None:
             # get user object from agave.
             u = User.objects.get(username=username)
+            pems_username = username
             client = u.agave_oauth.client
         else:
             client = Agave(api_server=settings.AGAVE_TENANT_BASEURL, token=settings.AGAVE_SUPER_TOKEN)
+            pems_username = settings.PORTAL_ADMIN_USERNAME
+        
         listing = client.files.list(systemId=systemId, filePath=filePath)
         for child in listing[1:]:
-            f = IndexedFile(**child)
-            f.save()
+            pems = client.files.listPermissions(systemId=child['system'], filePath=urllib.quote(child['path']))
+            child['pems'] = pems
+            try:
+                f = IndexedFile.from_path(pems_username, child['system'], child['path'])
+                f.update(**child)
+            except DocumentNotFound:
+                f = IndexedFile(**child)
+                f.save()
+            
             # Recurse on any dirs.
             if recurse and child['mimeType'] == 'text/directory' and not child.name.startswith('.'):
                 try:
@@ -32,26 +44,28 @@ def agave_indexer(systemId, username=None, filePath='/', recurse=True):
                     )
                 except Exception as e:
                     logger.info(e)
-    except:
+    except Exception as e:
+        logger.info(e)
         logger.info("Error, could not index {system}:{path}".format(system=systemId, path=filePath))
 
-@shared_task
-def index_community_data():
-    s = IndexedFile.search()
-    s = s.query("match", **{"system._exact": settings.AGAVE_COMMUNITY_DATA_SYSTEM})
-    resp = s.delete()
+@shared_task(bind=True)
+def index_community_data(self):
+    # s = IndexedFile.search()
+    # s = s.query("match", **{"system._exact": settings.AGAVE_COMMUNITY_DATA_SYSTEM})
+    # resp = s.delete()
     agave_indexer.apply_async(args=[settings.AGAVE_COMMUNITY_DATA_SYSTEM])
 
 # Indexing task for My Data.
-@shared_task
-def index_my_data():
+@shared_task(bind=True)
+def index_my_data(self):
     users = User.objects.all()
     for user in users:
         uname = user.username
-        systemId = "data-tacc-work-" + uname
-        s = IndexedFile.search()
-        s = s.query("match", **{"system._exact": systemId})
-        resp = s.delete()
+        systemId = '.'.join([
+            settings.PORTAL_DATA_DEPOT_USER_SYSTEM_PREFIX, uname])
+        # s = IndexedFile.search()
+        # s = s.query("match", **{"system._exact": systemId})
+        # resp = s.delete()
         agave_indexer.apply_async(
             args=[systemId],
             kwargs={'username': uname, 'filePath': '/'}
