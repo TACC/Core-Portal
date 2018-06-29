60f7174789b6972d119a4f347ff6e7cfa8183aec
Jarosenb <jrosenberg@tacc.utexas.edu>
2018-06-29 10:39:10 -0500

    Add recursive deletion and call indexing task from file operations.

--- server/portal/apps/search/tasks.py
+++ server/portal/apps/search/tasks.py
@@ -11,9 +11,20 @@ from portal.libs.elasticsearch.docs.base import IndexedFile
 from portal.libs.elasticsearch.exceptions import DocumentNotFound
 logger = logging.getLogger(__name__)
 
+def delete_recursive(object_to_delete):
+    ES_child_search = IndexedFile.search()\
+        .filter(Q({'term': {'system._exact': object_to_delete.system}}))\
+        .query(Q({'term': {'basePath._exact': object_to_delete.path}}))
+    ES_children = ES_child_search.execute()
+
+    for child in ES_children:
+        delete_recursive(child)
+    
+    object_to_delete.delete()
+
 # Crawl and index agave files
 @shared_task(bind=True)
-def agave_indexer(self, systemId, username=None, filePath='/', recurse=True):
+def agave_indexer(self, systemId, username=None, filePath='/', recurse=True, update_pems = False):
     try:
         # Get a listing and index the results.
         if username != None:
@@ -24,18 +35,22 @@ def agave_indexer(self, systemId, username=None, filePath='/', recurse=True):
         else:
             client = Agave(api_server=settings.AGAVE_TENANT_BASEURL, token=settings.AGAVE_SUPER_TOKEN)
             pems_username = settings.PORTAL_ADMIN_USERNAME
-        
+
         listing = client.files.list(systemId=systemId, filePath=filePath)
         for child in listing[1:]:
-            pems = client.files.listPermissions(systemId=child['system'], filePath=urllib.quote(child['path']))
-            child['pems'] = pems
+            child['basePath'] = os.path.dirname(child['path'])
             try:
                 f = IndexedFile.from_path(pems_username, child['system'], child['path'])
+                if update_pems:
+                    pems = client.files.listPermissions(systemId=child['system'], filePath=urllib.quote(child['path']))
+                    child['pems'] = pems
                 f.update(**child)
             except DocumentNotFound:
+                pems = client.files.listPermissions(systemId=child['system'], filePath=urllib.quote(child['path']))
+                child['pems'] = pems
                 f = IndexedFile(**child)
                 f.save()
-            
+
             # Recurse on any dirs.
             if recurse and child['mimeType'] == 'text/directory' and not child.name.startswith('.'):
                 try:
@@ -45,6 +60,14 @@ def agave_indexer(self, systemId, username=None, filePath='/', recurse=True):
                     )
                 except Exception as e:
                     logger.info(e)
+
+        q = Q({'term': {'basePath._exact': filePath}})
+        s = IndexedFile.search().query(q)
+        listing_paths = [child['path'] for child in listing[1:]]
+        for result in s.execute():
+            if result.path not in listing_paths:
+                delete_recursive(result)
+        
     except Exception as e:
         logger.info(e)
         logger.info("Error, could not index {system}:{path}".format(system=systemId, path=filePath))
