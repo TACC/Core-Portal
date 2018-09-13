362a009417bf79c33e3e4ae015b4e118220d51df
Sal Tijerina <r.sal.tijerina@gmail.com>
2018-09-13 16:32:15 -0500

    Move job notification handler to view, build webhook classes

--- server/portal/apps/webhooks/urls.py
+++ server/portal/apps/webhooks/urls.py
@@ -4,6 +4,7 @@ from django.conf.urls import url
 from portal.apps.webhooks import views
 
 urlpatterns = [
-    url(r'^$', views.generic_webhook_handler, name='generic_wh_handler'),
     url(r'^jobs/$', views.JobsWebhookView.as_view(), name='jobs_wh_handler'),
+    url(r'^interactive/$', views.InteractiveWebhookView.as_view(), name='interactive_wh_handler'),
+    url(r'^onboarding/$', views.OnboardingWebhookView.as_view(), name='onboarding_wh_handler'),
 ]
--- server/portal/apps/webhooks/views.py
+++ server/portal/apps/webhooks/views.py
@@ -1,17 +1,20 @@
 import json
 import logging
+import os
 from django.contrib.auth import get_user_model
 from django.core.urlresolvers import reverse
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
+from django.db import transaction
 from django.http import HttpResponse
 from django.conf import settings
 from django.views.decorators.http import require_POST
 from requests import HTTPError
 from agavepy.agave import Agave, AgaveException
 from portal.apps.notifications.models import Notification
+from django.core.exceptions import ObjectDoesNotExist
+from portal.apps.search.tasks import agave_indexer
 from portal.views.base import BaseApiView
-from .tasks import process_job_status
 
 logger = logging.getLogger(__name__)
 
@@ -27,97 +30,230 @@ class JobsWebhookView(BaseApiView):
     def dispatch(self, *args, **kwargs):
         return super(JobsWebhookView, self).dispatch(*args, **kwargs)
 
-    def get(self, request, *args, **kwargs):
+    def post(self, request, *args, **kwargs):
+        """Notifies the user of the job status by instantiating and saving
+        a Notification instance.
 
-        if settings.WEBHOOK_POST_URL:
-            return HttpResponse(settings.WEBHOOK_POST_URL + reverse('webhooks:jobs_wh_handler'))
-        else:
-            return HttpResponse(request.build_absolute_uri(reverse('webhooks:jobs_wh_handler')))
+        If the job is finished, we also index the job and  alert the user to the
+        URL of the job's location in the data depot.
+
+        Args:
+            job (dict): Dictionary containing the webhook data.
+
+        """
+        job = json.loads(request.body)
+
+        try:
+            username = job['owner']
+            job_id = job['id']
+            archiveSystem = job['archiveSystem']
+            archivePath = job['archivePath']
+            job_status = job['status']
+            job_name = job['name']
+
+            try:
+                job['submitTime'] = str(job['submitTime'])
+                job['endTime'] = str(job['endTime'])
+            except KeyError as e:
+                pass
+
+            logger.debug(job_status)
+            event_data = {
+                Notification.EVENT_TYPE: 'job',
+                Notification.JOB_ID: job_id,
+                Notification.STATUS: '',
+                Notification.USER: username,
+                Notification.MESSAGE: '',
+                Notification.EXTRA: job
+            }
+            archive_id = 'agave/{}/{}'.format(archiveSystem, archivePath.split('/'))
+
+            if job_status == 'FAILED':
+                logger.debug('JOB FAILED: id={} status={}'.format(job_id, job_status))
+                event_data[Notification.STATUS] = Notification.ERROR
+                event_data[Notification.MESSAGE] = "Job '{}' Failed. Please try again...".format(job_name)
+                event_data[Notification.OPERATION] = 'job_failed'
+
+                with transaction.atomic():
+                    last_notification = Notification.objects.filter(jobId=job_id).last()
+                    should_notify = True
+
+                    if last_notification:
+                        last_status = last_notification.to_dict()['extra']['status']
+                        logger.debug('last status: ' + last_status)
+
+                        if job_status == last_status:
+                            logger.debug('duplicate notification received.')
+                            should_notify = False
+
+                    if should_notify:
+                        n = Notification.objects.create(**event_data)
+                        n.save()
+
+            elif job_status == 'FINISHED':
+                logger.debug('JOB STATUS CHANGE: id={} status={}'.format(job_id, job_status))
+
+                logger.debug('archivePath: {}'.format(archivePath))
+                target_path = reverse('data:data_depot')
+                os.path.join(target_path, 'agave', archive_id.strip('/'))
+                event_data[Notification.STATUS] = Notification.SUCCESS
+                event_data[Notification.EXTRA]['job_status'] = 'FINISHED'
+                event_data[Notification.EXTRA]['target_path'] = target_path
+                event_data[Notification.MESSAGE] = "Job '{}' finished!".format(job_name)
+                event_data[Notification.OPERATION] = 'job_finished'
+
+                with transaction.atomic():
+                    last_notification = Notification.objects.filter(jobId=job_id).last()
+                    should_notify = True
+
+                    if last_notification:
+                        last_status = last_notification.to_dict()['extra']['status']
+                        logger.debug('last status: ' + last_status)
+
+                        if job_status == last_status:
+                            logger.debug('duplicate notification received.')
+                            should_notify = False
+
+                    if should_notify:
+                        n = Notification.objects.create(**event_data)
+                        n.save()
+                        logger.debug('Event data with action link {}'.format(event_data))
+
+                        try:
+                            logger.debug('Preparing to Index Job Output job={}'.format(job_name))
+
+                            agave_indexer.delay(
+                                archiveSystem, username, archivePath)
+                            logger.debug(
+                                'Finished Indexing Job Output job={}'.format(job_name))
+                        except Exception as e:
+                            logger.exception('Error indexing job output: {}'.format(e))
+                            return HttpResponse(json.dumps(e.message), content_type='application/json', status=400)
+
+            else:
+                # notify
+                logger.debug('JOB STATUS CHANGE: id={} status={}'.format(job_id, job_status))
+                event_data[Notification.STATUS] = Notification.INFO
+                event_data[Notification.MESSAGE] = "Job '{}' updated to {}.".format(job_name, job_status)
+                event_data[Notification.OPERATION] = 'job_status_update'
+
+                with transaction.atomic():
+                    last_notification = Notification.objects.filter(jobId=job_id).last()
+
+                    should_notify = True
+
+                    if last_notification:
+                        last_status = last_notification.to_dict()['extra']['status']
+                        logger.debug('last status: ' + last_status)
+
+                        if job_status == last_status:
+                            logger.debug('duplicate notification received.')
+                            should_notify = False
+
+                    if should_notify:
+                        n = Notification.objects.create(**event_data)
+                        n.save()
+
+                        logger.debug(n.pk)
+
+            return HttpResponse('OK')
+
+        except ObjectDoesNotExist:
+            logger.exception('Unable to locate local user account: {}'.format(username))
+            return HttpResponse(json.dumps(e.message), content_type='application/json', status=400)
+
+        except AgaveException as e:
+            logger.warning('Agave API error')
+            return HttpResponse(json.dumps(e.message), content_type='application/json', status=400)
+
+        except Exception as e:
+            return HttpResponse(json.dumps(e.message), content_type='application/json', status=400)
+
+
+class InteractiveWebhookView(BaseApiView):
+    """
+    Dispatches notifications when receiving a POST request from interactive jobs (e.g. VNC or DCV)
+    """
+    @method_decorator(csrf_exempt)
+    def dispatch(self, *args, **kwargs):
+        return super(InteractiveWebhookView, self).dispatch(*args, **kwargs)
 
     def post(self, request, *args, **kwargs):
         """
-        Calls process_job_status on webhook JSON body
-        to notify the user of the progress of the job.
+        Creates a notification with a link to the interactive job event.
 
         """
 
-        job = json.loads(request.body)
-        # logger.debug(job)
-
-        process_job_status(job)
-        return HttpResponse('OK')
-
-
-@require_POST
-@csrf_exempt
-def generic_webhook_handler(request):
-    event_type = request.POST.get('event_type', None)
-    if event_type == 'WEB':
-        # This is for jobs that just point to a URL that gets created
-        # like the Potree Viewer Application
-        job_owner = request.POST.get('owner', '')
-        address = request.POST.get('address', '')
-        event_data = {
-            Notification.EVENT_TYPE: event_type,
-            Notification.STATUS: Notification.INFO,
-            Notification.OPERATION: 'web_link',
-            Notification.USER: job_owner,
-            Notification.MESSAGE: 'Ready to view.',
-            Notification.EXTRA: {
-                'address': address,
+        event_type = request.POST.get('event_type', None)
+        if event_type == 'WEB':
+            # This is for jobs that just point to a URL that gets created
+            # like the Potree Viewer Application
+            job_owner = request.POST.get('owner', '')
+            address = request.POST.get('address', '')
+            event_data = {
+                Notification.EVENT_TYPE: event_type,
+                Notification.STATUS: Notification.INFO,
+                Notification.OPERATION: 'web_link',
+                Notification.USER: job_owner,
+                Notification.MESSAGE: 'Ready to view.',
+                Notification.EXTRA: {
+                    'address': address,
+                }
             }
-        }
-        n = Notification.objects.create(**event_data)
-        n.save()
-        return HttpResponse('OK')
-    elif event_type == 'VNC':
-        job_owner = request.POST.get('owner', '')
-        host = request.POST.get('host', '')
-        port = request.POST.get('port', '')
-        password = request.POST.get('password', '')
-        address = request.POST.get('address', '')
-        job_uuid = password
+            n = Notification.objects.create(**event_data)
+            n.save()
+            return HttpResponse('OK')
+        elif event_type == 'VNC':
+            job_owner = request.POST.get('owner', '')
+            host = request.POST.get('host', '')
+            port = request.POST.get('port', '')
+            password = request.POST.get('password', '')
+            address = request.POST.get('address', '')
+            job_uuid = password
 
-        target_uri = \
-            'https://{host}/no-vnc/vnc.html?'\
-            'hostname={host}&port={port}&autoconnect=true&password={pw}' \
-            .format(host=host, port=port, pw=password)
-        event_data = {
-            Notification.EVENT_TYPE: event_type,
-            Notification.STATUS: Notification.INFO,
-            Notification.OPERATION: 'vnc_session_start',
-            Notification.USER: job_owner,
-            Notification.MESSAGE: 'Your VNC session is ready.',
-            Notification.ACTION_LINK: target_uri,
-            Notification.EXTRA: {
-                'host': host,
-                'port': port,
-                'address': address,
-                'password': password,
-                'associationIds': job_uuid,
-                'target_uri': target_uri
+            target_uri = \
+                'https://{host}/no-vnc/vnc.html?'\
+                'hostname={host}&port={port}&autoconnect=true&password={pw}' \
+                .format(host=host, port=port, pw=password)
+            event_data = {
+                Notification.EVENT_TYPE: event_type,
+                Notification.STATUS: Notification.INFO,
+                Notification.OPERATION: 'vnc_session_start',
+                Notification.USER: job_owner,
+                Notification.MESSAGE: 'Your VNC session is ready.',
+                Notification.ACTION_LINK: target_uri,
+                Notification.EXTRA: {
+                    'host': host,
+                    'port': port,
+                    'address': address,
+                    'password': password,
+                    'associationIds': job_uuid,
+                    'target_uri': target_uri
+                }
             }
-        }
-        n = Notification.objects.create(**event_data)
-        n.save()
+            n = Notification.objects.create(**event_data)
+            n.save()
 
-        # create metadata for VNC connection and save to agave metadata?
-        try:
-            agave_job_meta = {
-                'name': 'interactiveJobDetails',
-                'value': event_data,
-                'associationIds': [job_uuid],
-            }
-            user = get_user_model().objects.get(username=job_owner)
-            agave = user.agave_oauth.client
-            agave.meta.addMetadata(body=json.dumps(agave_job_meta))
+            # create metadata for VNC connection and save to agave metadata?
+            try:
+                agave_job_meta = {
+                    'name': 'interactiveJobDetails',
+                    'value': event_data,
+                    'associationIds': [job_uuid],
+                }
+                user = get_user_model().objects.get(username=job_owner)
+                agave = user.agave_oauth.client
+                agave.meta.addMetadata(body=json.dumps(agave_job_meta))
 
-        except (HTTPError, AgaveException) as e:
-            logger.exception(
-                'Could not add interactive connection data to metadata')
-            return HttpResponse(json.dumps(e.message), content_type='application/json',
-                                status=400)
+            except (HTTPError, AgaveException) as e:
+                logger.exception(
+                    'Could not add interactive connection data to metadata')
+                return HttpResponse(json.dumps(e.message), content_type='application/json', status=400)
 
-        return HttpResponse('OK')
-    else:
-        return HttpResponse('Unexpected', status=400)
+            return HttpResponse('OK')
+        else:
+            return HttpResponse('Unexpected event_type', status=400)
+
+
+class OnboardingWebhookView(BaseApiView):
+    pass
