3d8f95837953a80846b54b1ebcef539b3790723f
Joe Meiring <jmeiring@tacc.utexas.edu>
2018-09-15 10:15:18 -0500

    directive unit tests working, had to update the webpack config for karma to use the html loader for the templates to get included

--- client/src/angular/data_depot/directives/dd-breadcrumb.js
+++ client/src/angular/data_depot/directives/dd-breadcrumb.js
@@ -13,7 +13,6 @@ export default function ddBreadcrumb() {
       itemHref: '&'
     },
     link: function(scope) {
-      console.log(scope.listing)
       scope.offset = 0;
       if (scope.skipRoot || scope.customRoot) {
         scope.offset = 1;
