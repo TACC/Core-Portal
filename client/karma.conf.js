7617d2f1a004d80845c9cdc6611a8706491b6c15
Joe Meiring <jmeiring@tacc.utexas.edu>
2018-10-03 15:55:29 -0500

    cobertura reports

--- client/karma.conf.js
+++ client/karma.conf.js
@@ -37,9 +37,13 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress', 'coverage-istanbul'],
-
-
+    reporters: ['progress', 'coverage'],
+    coverageReporter: {
+      dir: 'coverage',
+      reporters: [
+        { type: 'cobertura', subdir: '.', file: 'cobertura.xml' },
+      ]
+    },
     // web server port
     port: 9876,
 
