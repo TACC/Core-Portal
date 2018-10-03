14046d35eeab4297920a1ed5826b1ac7825ce1d6
Joe Meiring <jmeiring@tacc.utexas.edu>
2018-10-03 16:15:55 -0500

    watermarks

--- client/karma.conf.js
+++ client/karma.conf.js
@@ -37,11 +37,17 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress', 'coverage'],
+    reporters: ['progress', 'coverage-istanbul'],
     coverageReporter: {
+      watermarks: {
+        statements: [ 50, 75 ],
+        functions: [ 50, 75 ],
+        branches: [ 50, 75 ],
+        lines: [ 50, 75 ]
+      },
       dir: 'coverage',
       reporters: [
-        { type: 'cobertura', subdir: '.', file: 'cobertura.xml' },
+        { type: 'html', subdir: 'report-html' },
       ]
     },
     // web server port
