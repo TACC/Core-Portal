a656c70cb82e73921e177017f68a4002ecba98c3
Joe Meiring <jmeiring@tacc.utexas.edu>
2018-10-03 16:28:12 -0500

    working source highlighting

--- client/karma.conf.js
+++ client/karma.conf.js
@@ -29,7 +29,7 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      'test-context.js': ['webpack'],
+      'test-context.js': ['webpack', 'coverage'],
       'src/**/*.spec.js': ['webpack', 'coverage']
     },
 
