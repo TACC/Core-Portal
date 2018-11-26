7d75bb4036c57757fef86b3dfd75e53c2dd23cd1
Joe Meiring <jmeiring@tacc.utexas.edu>
2018-11-26 15:58:42 -0600

    Added a 401 interceptor to catch it when a users is logged out and still making ajax requests

--- client/src/angular/common/index.js
+++ client/src/angular/common/index.js
@@ -1,10 +1,11 @@
 
 import UserService from './user-service';
 import SystemsService from './systems-service';
+import AuthInterceptor from './auth-interceptor';
 
 let mod = angular.module('portal.common', []);
 
 mod.service('UserService', UserService);
 mod.service('SystemsService', SystemsService);
-
+mod.service('AuthInterceptor', AuthInterceptor);
 export default mod;
--- client/src/angular/main.module.js
+++ client/src/angular/main.module.js
@@ -42,7 +42,7 @@ function config($httpProvider, $locationProvider, $urlRouterProvider, $stateProv
  $httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
  $httpProvider.defaults.xsrfCookieName = 'csrftoken';
  $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
-
+ $httpProvider.interceptors.push('AuthInterceptor');
  $urlRouterProvider.when('/workbench/', '/workbench/dashboard');
 
  $stateProvider
