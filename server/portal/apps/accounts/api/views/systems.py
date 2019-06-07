4a3beb252b392c2e8fe7ad577db0d54a5223f27b
Jake Rosenberg <jrosenberg@tacc.utexas.edu>
2019-06-07 15:10:48 +0000

    Merged in task/CEPWMA-511 (pull request #314)
    task/CEPWMA-511: Reset keys before pushing them.
    
    Approved-by: Joseph Meiring <jmeiring@tacc.utexas.edu>

--- server/portal/apps/accounts/api/views/systems.py
+++ server/portal/apps/accounts/api/views/systems.py
@@ -157,6 +157,12 @@ class SystemKeysView(BaseApiView):
         :param request: Django's request object
         :param str system_id: System id
         """
+
+        AccountsManager.reset_system_keys(
+            request.user.username,
+            system_id
+        )
+
         success, result, http_status = AccountsManager.add_pub_key_to_resource(
             request.user.username,
             password=body['form']['password'],
