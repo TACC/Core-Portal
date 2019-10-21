67414a04f53c36f623e4dcd44775f2366285b1bb
Sal Tijerina <r.sal.tijerina@gmail.com>
2019-10-21 14:01:48 -0500

    add babel eslint loader/parser

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -40,9 +40,7 @@ module.exports = {
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader'
-        }
+        use: ['babel-loader', 'eslint-loader']
       },
       {
         test: /\.html$/,
