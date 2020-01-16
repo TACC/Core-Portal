231f0f10a53889712e6e8d822fa3021ac54cbf0c
Wesley Bomar <wbomar@tacc.utexas.edu>
2020-01-16 20:45:14 +0000

    Merged in task/FP-88-improve-app-layout-styles (pull request #15)
    Task/FP-88 improve app layout styles
    
    * Quick: Return  to original lines
    
    * FP-88: Remove use of
    
    * Quick: Remove ineffectual class assignement
    
    * FP-88: Migrate ext. layout styles to `index.scss`
    
    * FP-88: Remove unused `app-layout` styles
    
    * Quick: Move `tickets-wrapper` to original lines
    
    * Quick: Restore export of Sidebar to original line
    
    * FP-88: Remove obtrusive code and excess comments
    
    * Merge branch 'master' into task/FP-88-improve-app-layout-styles
    
    * FP-88: Move style to appropriate location
    
    Mistake in much earlier set of commits, led to erroneous move of style.
    
    Approved-by: Sal Tijerina

--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -7,54 +7,52 @@ import './Dashboard.scss';
 
 function Dashboard() {
   return (
-    <div className="workbench-dashboard">
+    <div className="dashboard-wrapper">
       <div className="dashboard-header">
         <h5>Dashboard</h5>
         <a href="/accounts/profile" className="wb-link">
           <h6>Manage Account</h6>
         </a>
       </div>
-      <div className="dashboard-items">
-        <Container className="wb-grid-container">
-          <Row>
-            <Col lg="8" className="border-right">
-              <div className="jobs-wrapper dash-grid-item">
-                <div className="dashboard-item-header">
-                  <h6>My Recent Jobs</h6>
-                  <Button color="link">
-                    <h6>View History</h6>
-                  </Button>
-                </div>
-                <JobsView />
+      <Container className="dashboard-items">
+        <Row>
+          <Col lg="8" className="border-right">
+            <div className="jobs-wrapper dash-grid-item">
+              <div className="dashboard-item-header">
+                <h6>My Recent Jobs</h6>
+                <Button color="link">
+                  <h6>View History</h6>
+                </Button>
               </div>
-            </Col>
-            <Col lg="4">
-              <div className="sysmon-wrapper dash-grid-item">
-                <div className="dashboard-item-header">
-                  <h6>System Status</h6>
-                  <Button color="link">
-                    <h6>Customize</h6>
-                  </Button>
-                </div>
-                <Sysmon />
+              <JobsView />
+            </div>
+          </Col>
+          <Col lg="4">
+            <div className="sysmon-wrapper dash-grid-item">
+              <div className="dashboard-item-header">
+                <h6>System Status</h6>
+                <Button color="link">
+                  <h6>Customize</h6>
+                </Button>
               </div>
-            </Col>
-          </Row>
-          <Row>
-            <Col lg="8" className="border-right">
-              <div className="tickets-wrapper dash-grid-item">
-                <div className="dashboard-item-header">
-                  <h6>My Tickets</h6>
-                  <Button color="link">
-                    <h6>Add Ticket</h6>
-                  </Button>
-                </div>
-                <Tickets />
+              <Sysmon />
+            </div>
+          </Col>
+        </Row>
+        <Row>
+          <Col lg="8" className="border-right">
+            <div className="tickets-wrapper dash-grid-item">
+              <div className="dashboard-item-header">
+                <h6>My Tickets</h6>
+                <Button color="link">
+                  <h6>Add Ticket</h6>
+                </Button>
               </div>
-            </Col>
-          </Row>
-        </Container>
-      </div>
+              <Tickets />
+            </div>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
--- client/src/components/Sidebar/Sidebar.js
+++ client/src/components/Sidebar/Sidebar.js
@@ -6,113 +6,104 @@ import { faDesktop, faCog } from '@fortawesome/free-solid-svg-icons';
 import { faBell, faFolder } from '@fortawesome/free-regular-svg-icons';
 import './Sidebar.scss';
 
+/** A navigation list for the application */
 export default function Sidebar() {
   return (
-    <div className="side-nav">
-      <Nav id="sidebar" vertical>
-        <NavItem>
-          <NavLink
-            tag={RRNavLink}
-            exact
-            to="/workbench/dashboard/"
-            activeClassName="active"
-          >
-            <div className="nav-content">
-              <FontAwesomeIcon
-                icon={faDesktop}
-                size="1x"
-                className="side-nav-icon"
-              />
-              <span className="nav-text">Dashboard</span>
-            </div>
-          </NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink
-            tag={RRNavLink}
-            exact
-            to="/workbench/data/"
-            activeClassName="active"
-          >
-            <div className="nav-content">
-              <FontAwesomeIcon
-                icon={faFolder}
-                size="1x"
-                className="side-nav-icon"
-              />
-              <span className="nav-text">Data Files</span>
-            </div>
-          </NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink
-            tag={RRNavLink}
-            exact
-            to="/workbench/applications/"
-            activeClassName="active"
-          >
-            <div className="nav-content">
-              <FontAwesomeIcon
-                icon={faCog}
-                size="1x"
-                className="side-nav-icon"
-              />
-              <span className="nav-text">Applications</span>
-            </div>
-          </NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink
-            tag={RRNavLink}
-            exact
-            to="/workbench/allocations/"
-            activeClassName="active"
-          >
-            <div className="nav-content">
-              <FontAwesomeIcon
-                icon={faCog}
-                size="1x"
-                className="side-nav-icon"
-              />
-              <span className="nav-text">Allocations</span>
-            </div>
-          </NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink
-            tag={RRNavLink}
-            exact
-            to="/workbench/publications/"
-            activeClassName="active"
-          >
-            <div className="nav-content">
-              <FontAwesomeIcon
-                icon={faFolder}
-                size="1x"
-                className="side-nav-icon"
-              />
-              <span className="nav-text">Publications</span>
-            </div>
-          </NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink
-            tag={RRNavLink}
-            exact
-            to="/workbench/history/"
-            activeClassName="active"
-          >
-            <div className="nav-content">
-              <FontAwesomeIcon
-                icon={faBell}
-                size="1x"
-                className="side-nav-icon"
-              />
-              <span className="nav-text">History</span>
-            </div>
-          </NavLink>
-        </NavItem>
-      </Nav>
-    </div>
+    <Nav id="sidebar" className="side-nav" vertical>
+      <NavItem>
+        <NavLink
+          tag={RRNavLink}
+          exact
+          to="/workbench/dashboard/"
+          activeClassName="active"
+        >
+          <div className="nav-content">
+            <FontAwesomeIcon
+              icon={faDesktop}
+              className="side-nav-icon"
+              size="1x"
+            />
+            <span className="nav-text">Dashboard</span>
+          </div>
+        </NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink
+          tag={RRNavLink}
+          exact
+          to="/workbench/data/"
+          activeClassName="active"
+        >
+          <div className="nav-content">
+            <FontAwesomeIcon
+              icon={faFolder}
+              className="side-nav-icon"
+              size="1x"
+            />
+            <span className="nav-text">Data Files</span>
+          </div>
+        </NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink
+          tag={RRNavLink}
+          exact
+          to="/workbench/applications/"
+          activeClassName="active"
+        >
+          <div className="nav-content">
+            <FontAwesomeIcon icon={faCog} className="side-nav-icon" size="1x" />
+            <span className="nav-text">Applications</span>
+          </div>
+        </NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink
+          tag={RRNavLink}
+          exact
+          to="/workbench/allocations/"
+          activeClassName="active"
+        >
+          <div className="nav-content">
+            <FontAwesomeIcon icon={faCog} size="1x" className="side-nav-icon" />
+            <span className="nav-text">Allocations</span>
+          </div>
+        </NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink
+          tag={RRNavLink}
+          exact
+          to="/workbench/publications/"
+          activeClassName="active"
+        >
+          <div className="nav-content">
+            <FontAwesomeIcon
+              icon={faFolder}
+              className="side-nav-icon"
+              size="1x"
+            />
+            <span className="nav-text">Publications</span>
+          </div>
+        </NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink
+          tag={RRNavLink}
+          exact
+          to="/workbench/history/"
+          activeClassName="active"
+        >
+          <div className="nav-content">
+            <FontAwesomeIcon
+              icon={faBell}
+              className="side-nav-icon"
+              size="1x"
+            />
+            <span className="nav-text">History</span>
+          </div>
+        </NavLink>
+      </NavItem>
+    </Nav>
   );
 }
