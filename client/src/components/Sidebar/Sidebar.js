d45b9ac4cfe9980a731b20002785ad8dde52eb64
Wesley Bomar <wbomar@tacc.utexas.edu>
2020-04-17 14:59:58 -0500

    FP-103: Componentized JS with Styled Components

--- client/src/components/Sidebar/Sidebar.js
+++ client/src/components/Sidebar/Sidebar.js
@@ -2,63 +2,70 @@ import React from 'react';
 import { Nav, NavItem, NavLink } from 'reactstrap';
 import { NavLink as RRNavLink, useRouteMatch } from 'react-router-dom';
 import * as ROUTES from '../../constants/routes';
-import './Sidebar.scss';
+import {
+  NavStyled,
+  NavLinkStyled,
+  SidebarLinkContent,
+  SidebarLinkText,
+  NavIcon
+} from './Sidebar.styles.js';
+import './Sidebar.css';
 
 /** A navigation list for the application */
 const Sidebar = () => {
   const { path } = useRouteMatch();
   return (
-    <Nav id="sidebar" className="side-nav" vertical>
+    <NavStyled vertical>
       <NavItem>
-        <NavLink
+        <NavLinkStyled
           tag={RRNavLink}
           exact
           to={`${path}${ROUTES.DASHBOARD}`}
           activeClassName="active"
         >
-          <div className="nav-content">
-            <i className="icon-nav icon-nav-dashboard" />
-            <span className="nav-text">Dashboard</span>
-          </div>
-        </NavLink>
+          <SidebarLinkContent>
+            <NavIcon className="icon-nav icon-nav-dashboard" />
+            <SidebarLinkText>Dashboard</SidebarLinkText>
+          </SidebarLinkContent>
+        </NavLinkStyled>
       </NavItem>
       <NavItem>
-        <NavLink
+        <NavLinkStyled
           tag={RRNavLink}
           to={`${path}${ROUTES.DATA}`}
           activeClassName="active"
         >
-          <div className="nav-content">
-            <i className="icon-nav icon-nav-folder" />
-            <span className="nav-text">Data Files</span>
-          </div>
-        </NavLink>
+          <SidebarLinkContent>
+            <NavIcon className="icon-nav icon-nav-folder" />
+            <SidebarLinkText>Data Files</SidebarLinkText>
+          </SidebarLinkContent>
+        </NavLinkStyled>
       </NavItem>
       <NavItem>
-        <NavLink
+        <NavLinkStyled
           tag={RRNavLink}
           to={`${path}${ROUTES.APPLICATIONS}`}
           activeClassName="active"
         >
-          <div className="nav-content">
-            <i className="icon-nav icon-nav-application" />
-            <span className="nav-text">Applications</span>
-          </div>
-        </NavLink>
+          <SidebarLinkContent>
+            <NavIcon className="icon-nav icon-nav-application" />
+            <SidebarLinkText>Applications</SidebarLinkText>
+          </SidebarLinkContent>
+        </NavLinkStyled>
       </NavItem>
       <NavItem>
-        <NavLink
+        <NavLinkStyled
           tag={RRNavLink}
           to={`${path}${ROUTES.ALLOCATIONS}`}
           activeClassName="active"
         >
-          <div className="nav-content">
-            <i className="icon-nav icon-nav-allocation" />
-            <span className="nav-text">Allocations</span>
-          </div>
-        </NavLink>
+          <SidebarLinkContent>
+            <NavIcon className="icon-nav icon-nav-allocation" />
+            <SidebarLinkText>Allocations</SidebarLinkText>
+          </SidebarLinkContent>
+        </NavLinkStyled>
       </NavItem>
-    </Nav>
+    </NavStyled>
   );
 };
 
---
+++ client/src/components/Sidebar/Sidebar.styles.js
@@ -0,0 +1,37 @@
+import styled from 'styled-components';
+import { Nav, NavLink } from 'reactstrap';
+
+export const NavStyled = styled(Nav)`
+  min-width: 215px;
+  max-width: 215px;
+  width: 215px;
+  background: #f4f4f4;
+  border-right: 1px solid rgba(112, 112, 112, 0.25);
+  padding-top: 20px;
+`;
+
+export const NavLinkStyled = styled(NavLink)`
+  color: #707070;
+
+  &.active {
+    background: #dfdaf5
+  }
+`;
+
+export const SidebarLinkContent = styled.div`
+  padding-left: 20px;
+  display: flex;
+  align-items: center;
+`;
+
+export const SidebarLinkText = styled.span`
+  padding-left: 20px;
+  font-size: 0.78em;
+  font-weight: 500;
+`;
+
+export const NavIcon = styled.i`
+  font-size: 1.5em;
+  display: flex;
+  height: 1em;
+`;
