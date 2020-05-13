bc6ed6a171b744efe8529b9bb38646580c32154c
Wesley Bomar <wbomar@tacc.utexas.edu>
2020-05-13 17:48:03 -0500

    FP-103: Add support for styleName

--- client/src/components/Sidebar/Sidebar.js
+++ client/src/components/Sidebar/Sidebar.js
@@ -2,25 +2,25 @@ import React from 'react';
 import { Nav, NavItem, NavLink } from 'reactstrap';
 import { NavLink as RRNavLink, useRouteMatch } from 'react-router-dom';
 import * as ROUTES from '../../constants/routes';
-import './Sidebar.global.scss';
-import classNames from './Sidebar.module.scss';
+import './Sidebar.global.scss'; // XXX: Global stylesheet imported in component
+import './Sidebar.module.scss';
 
 /** A navigation list for the application */
 const Sidebar = () => {
   const { path } = useRouteMatch();
   return (
-    <Nav className={classNames.root} vertical>
+    <Nav styleName="root" vertical>
       <NavItem>
         <NavLink
           tag={RRNavLink}
           exact
           to={`${path}${ROUTES.DASHBOARD}`}
-          className={classNames.link}
-          activeClassName={classNames['link--active']}
+          styleName="link"
+          activeStyleName="link--active"
         >
-          <div className={classNames.content}>
+          <div styleName="content">
             <i className="icon-nav icon-nav-dashboard" />
-            <span className={classNames.text}>Dashboard</span>
+            <span styleName="text">Dashboard</span>
           </div>
         </NavLink>
       </NavItem>
@@ -28,12 +28,12 @@ const Sidebar = () => {
         <NavLink
           tag={RRNavLink}
           to={`${path}${ROUTES.DATA}`}
-          className={classNames.link}
-          activeClassName={classNames['link--active']}
+          styleName="link"
+          activeStyleName="link--active"
         >
-          <div className={classNames.content}>
+          <div styleName="content">
             <i className="icon-nav icon-nav-folder" />
-            <span className={classNames.text}>Data Files</span>
+            <span styleName="text">Data Files</span>
           </div>
         </NavLink>
       </NavItem>
@@ -41,12 +41,12 @@ const Sidebar = () => {
         <NavLink
           tag={RRNavLink}
           to={`${path}${ROUTES.APPLICATIONS}`}
-          className={classNames.link}
-          activeClassName={classNames['link--active']}
+          styleName="link"
+          activeStyleName="link--active"
         >
-          <div className={classNames.content}>
+          <div styleName="content">
             <i className="icon-nav icon-nav-application" />
-            <span className={classNames.text}>Applications</span>
+            <span styleName="text">Applications</span>
           </div>
         </NavLink>
       </NavItem>
@@ -54,12 +54,12 @@ const Sidebar = () => {
         <NavLink
           tag={RRNavLink}
           to={`${path}${ROUTES.ALLOCATIONS}`}
-          className={classNames.link}
-          activeClassName={classNames['link--active']}
+          styleName="link"
+          activeStyleName="link--active"
         >
-          <div className={classNames.content}>
+          <div styleName="content">
             <i className="icon-nav icon-nav-allocation" />
-            <span className={classNames.text}>Allocations</span>
+            <span styleName="text">Allocations</span>
           </div>
         </NavLink>
       </NavItem>
