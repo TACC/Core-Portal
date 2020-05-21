d411246f28db6098d8b7be6377c3d2c1f84db8de
jchuahtacc <jchuah@tacc.utexas.edu>
2020-05-21 14:14:49 -0500

    Rename jobs.fixture

--- client/src/components/Jobs/Jobs.test.js
+++ client/src/components/Jobs/Jobs.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import Jobs from './Jobs';
-import { default as jobsList } from './JobsFixture';
+import { default as jobsList } from './Jobs.fixture';
 import { Provider } from 'react-redux';
 import configureStore from 'redux-mock-store';
 import '@testing-library/jest-dom/extend-expect';
