bcde6659f36b462eb98cdee1eb117a47c26c2b31
Owais Jamil <47395902+owaisj@users.noreply.github.com>
2020-05-21 17:59:03 -0500

    Task/FP-152: Move Manage Account to React App (#34)
    * /manage/account now sends user to react frontend
    
    * Wireframe for table
    
    * JSON serialized profile data with sagas
    
    * Styling and placeholders
    
    * Modals and state integration
    
    * Form field population from backend
    
    * Layout and styling
    
    * layout, styling, and backend adjustments
    
    * Functioning password change form
    
    * Edit PortalProfile model portion of user profile
    
    * Added fields to model and optional information form
    
    * Update tas information with form
    
    * Changed field components
    
    * deps fix
    
    * small styling fixes
    
    * Successful change UI and error handling with styling and backend view changes; Removed useFetch
    
    * Modal header & form styling and yup schema edits
    
    Styling
    
    * Professional interest field; URL regex
    
    * Layout breakpoints and removed unused django views
    
    * Styling and markup changes
    
    * Additional styling and validation changes
    
    * Inline submitting spinner; Right align license button
    
    * Last changed status for password
    
    * yup fix; Alerts no longer dismissible
    
    * Password conditions regex
    
    * various fixes
    
    Check for empty password changed statuses
    Remove website validation
    License modal header
    
    * table-striped color override
    
    * Fix selector
    
    * Various fixes
    
    btn-link style override
    Refactored ManageAccountFields (defaultValue, props, and handle functions)
    Change other label; Other text field goes below select
    
    * remove placeholder and bio ellipsis
    
    * Changed Requirements to component
    
    * Other field spacing and add disabled prop for citizenship
    
    * Less spacing
    
    * remove stray disabled check

---
+++ client/src/components/ManageAccount/ManageAccountForms/ChangePasswordForm.js
@@ -0,0 +1,119 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { Button } from 'reactstrap';
+import { object as obj, string as str, ref } from 'yup';
+import { Formik, Form } from 'formik';
+import { isEmpty } from 'lodash';
+import { bool } from 'prop-types';
+import { LoadingSpinner } from '_common';
+import { ManageAccountInput } from './ManageAccountFields';
+
+const ChangePasswordFormBody = ({ canSubmit }) => {
+  const isChecking = useSelector(state => state.profile.checkingPassword);
+  const Requirements = () => (
+    <div style={{ color: '#707070', fontStyle: 'italic' }}>
+      <span>Passwords must meet the following criteria:</span>
+      <ul style={{ paddingLeft: '1rem' }}>
+        <li>Must not contain your username or parts of your full name;</li>
+        <li>Must be a minimum of 8 characters in length</li>
+        <li>Must contain characters from at least three of the following:</li>
+        <li style={{ marginLeft: '1rem' }}>
+          Uppercase letters, lowercase letters, numbers, symbols
+        </li>
+      </ul>
+    </div>
+  );
+  return (
+    <Form className="change-password-form">
+      <ManageAccountInput
+        label="Current Password"
+        name="currentPW"
+        type="password"
+      />
+      <ManageAccountInput label="New Password" name="newPW" type="password" />
+      <ManageAccountInput
+        label="Confirm New Password"
+        name="confirmNewPW"
+        type="password"
+      />
+      <Requirements />
+      <Button
+        className="manage-account-submit-button"
+        type="submit"
+        style={{ alignSelf: 'flex-end' }}
+        disabled={!canSubmit}
+      >
+        {isChecking && <LoadingSpinner placement="inline" />}
+        <span style={isChecking ? { marginLeft: '1rem' } : {}}>
+          Change Password
+        </span>
+      </Button>
+    </Form>
+  );
+};
+ChangePasswordFormBody.propTypes = { canSubmit: bool.isRequired };
+
+export default function() {
+  const { restrictions } = useSelector(state => {
+    const { data } = state.profile;
+    const { username, firstName, lastName } = data.demographics;
+    return {
+      restrictions: { username, firstName, lastName }
+    };
+  });
+  const dispatch = useDispatch();
+  const handleSubmit = (values, { setSubmitting, resetForm }) => {
+    dispatch({
+      type: 'CHANGE_PASSWORD',
+      values,
+      callback: params => {
+        setSubmitting(false);
+        if (params.reset) resetForm();
+      }
+    });
+  };
+  const formSchema = obj().shape({
+    currentPW: str().required('Please enter your current password'),
+    newPW: str()
+      .min(8, 'Must be a minimum of 8 characters in length')
+      .test('check-first-name', 'Can not contain your first name', value => {
+        const matcher = new RegExp(restrictions.firstName, 'i');
+        return !matcher.test(value);
+      })
+      .test('check-last-name', 'Can not contain your last name', value => {
+        const matcher = new RegExp(restrictions.lastName, 'i');
+        return !matcher.test(value);
+      })
+      .test('check-username', 'Can not contain your username', value => {
+        const matcher = new RegExp(restrictions.username, 'i');
+        return !matcher.test(value);
+      })
+      .notOneOf(
+        [ref('currentPW')],
+        'Your new password must be different from your old password'
+      )
+      .matches(
+        /(?=.{8,})((?=.*\d)(?=.*[a-z])(?=.*[A-Z])|(?=.*\d)(?=.*[a-zA-Z])(?=.*[\W_])|(?=.*[a-z])(?=.*[A-Z])(?=.*[\W_])).*/,
+        'Must Contain at least three of the following: One Uppercase, One Lowercase, One Number and One special character'
+      )
+      .required('Required'),
+    confirmNewPW: str()
+      .oneOf([ref('newPW')], 'Passwords do not match')
+      .required('Required')
+  });
+  const initialValues = {
+    currentPW: '',
+    newPW: '',
+    confirmNewPW: ''
+  };
+  const hasErrors = errors => isEmpty(Object.keys(errors));
+  return (
+    <Formik
+      initialValues={initialValues}
+      onSubmit={handleSubmit}
+      validationSchema={formSchema}
+    >
+      {({ errors }) => <ChangePasswordFormBody canSubmit={hasErrors(errors)} />}
+    </Formik>
+  );
+}
