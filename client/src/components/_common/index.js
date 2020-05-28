b7ba0cc96e2f7db48c4ce38f7443be4a40d2c70a
jchuahtacc <jchuah@tacc.utexas.edu>
2020-05-28 11:09:03 -0500

    task/FP-409 job listing app icons (#32)
    * Promote GET_APPS dispatch to workbench
    
    * Find app icon
    
    * AppIcon component
    
    * Unit tests for AppIcon
    
    * Create appIcons dictionary in apps saga
    
    * Fixed unit tests

--- client/src/components/_common/index.js
+++ client/src/components/_common/index.js
@@ -2,3 +2,4 @@ export { default as LoadingSpinner } from './LoadingSpinner';
 export { default as SystemPushKeysModal } from './SystemsPushKeysModal';
 export { FormField } from './Form';
 export { FileInputDropZoneFormField } from './Form';
+export { default as AppIcon } from './AppIcon';
