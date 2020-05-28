2a0593533ed67416ed324390c33225812fdc7bda
W. Bomar <62723358+tacc-wbomar@users.noreply.github.com>
2020-05-28 14:08:30 -0500

    FP-389: Fix Apps Section Nav Overlap & Column Layout (#19)
    * FP-389: Eliminate overlap bug and fix app layout
    
    * FP-389: Move two styles for accuracy and clarity
    
    * FP-389: Revert to padding (because CSS Grid not flex)
    
    * FP-389: Fix comments
    
    * FP-389: Color background of app tray
    
    * FP-389: Define min width of sidebar and columns
    
    * FP-417: Static height for "app browser"
    
    * FP-389: Remove excess variable
    
    Co-authored-by: Sal Tijerina <r.sal.tijerina@gmail.com>

--- client/src/components/Applications/AppBrowser/AppBrowser.js
+++ client/src/components/Applications/AppBrowser/AppBrowser.js
@@ -55,7 +55,7 @@ const AppBrowser = () => {
 
   return (
     <div id="appBrowser-wrapper">
-      <Nav vertical tabs id="appBrowser-sidebar">
+      <Nav id="appBrowser-sidebar">
         {Object.keys(categoryDict).map(category => (
           <NavItem key={category}>
             <NavLink
@@ -64,14 +64,14 @@ const AppBrowser = () => {
                 toggle(category);
               }}
             >
-              <div className="nav-content">
+              <span className="nav-content">
                 <FontAwesomeIcon
                   icon={faDesktop}
                   size="1x"
                   className="side-nav-icon"
                 />
                 <span className="nav-text">{`${category} [${categoryDict[category].length}]`}</span>
-              </div>
+              </span>
             </NavLink>
           </NavItem>
         ))}
