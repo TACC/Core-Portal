6b17fb5a94c214e64fec73583f3582c7e508ba62
jchuahtacc <jchuah@tacc.utexas.edu>
2020-06-11 16:53:23 -0500

    Unused import

--- client/src/components/Tickets/TicketsLayout.js
+++ client/src/components/Tickets/TicketsLayout.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useCallback } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { Button } from 'reactstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faExclamationTriangle } from '@fortawesome/free-solid-svg-icons';
 import { InfiniteScrollTable } from '_common';
