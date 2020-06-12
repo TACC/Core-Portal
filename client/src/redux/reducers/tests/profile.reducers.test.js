38e30fd665c0ccedd1a686aadd29338923dcf7a9
Owais Jamil <ojamil@tacc.utexas.edu>
2020-06-12 14:45:07 -0500

    Manage Account sagas tests

---
+++ client/src/redux/reducers/tests/profile.reducers.test.js
@@ -0,0 +1,10 @@
+import profileReducer, { initialState } from '../profile.reducers';
+
+describe('Profile/Manage Account Reducer', () => {
+  test('Loading actions', () => {
+    
+  });
+  test.todo('Storing actions');
+  test.todo('Modal actions');
+  test.todo('Error actions');
+})
--- client/src/redux/sagas/profile.sagas.js
+++ client/src/redux/sagas/profile.sagas.js
@@ -1,11 +1,10 @@
 import { put, takeLatest, call } from 'redux-saga/effects';
 import { omit, isEmpty } from 'lodash';
-import 'cross-fetch';
 import { fetchUtil } from 'utils/fetchUtil';
 
 const ROOT_SLUG = '/accounts/api/profile';
 
-const getPasswordStatus = h => {
+export const getPasswordStatus = h => {
   const passwordChanged = h.filter(entry =>
     entry.comment.includes('Password changed')
   );
