8452f0ef682f2f728e7f5720d62029d88c29b048
Owais Jamil <ojamil@tacc.utexas.edu>
2020-06-16 11:29:10 -0500

    Allocation modals tests
    Refactored allocation request
    
    UI flow for team view modal tests

--- client/src/components/Allocations/tests/AllocationsModals.test.js
+++ client/src/components/Allocations/tests/AllocationsModals.test.js
@@ -1,30 +1,98 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import { BrowserRouter as Wrapper } from 'react-router-dom';
-import { NewAllocReq } from '../AllocationsModals';
+import { render, fireEvent } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import configureStore from 'redux-mock-store';
+import { Provider } from 'react-redux';
+import { NewAllocReq, TeamView } from '../AllocationsModals';
 
-describe('Allocations Request Form', () => {
-  const isOpen = true;
-  it('should tell the user what form they are viewing', () => {
-    const { getByText } = render(
-      <Wrapper>
-        <NewAllocReq isOpen toggle={() => !isOpen} />
-      </Wrapper>
+const mockStore = configureStore();
+
+describe('New Allocations Request Modal', () => {
+  test('Allocations Request UI', () => {
+    const { getByText, getByTestId } = render(
+      <BrowserRouter>
+        <NewAllocReq isOpen toggle={() => null} />
+      </BrowserRouter>
     );
     expect(getByText('Manage Allocations')).toBeDefined();
-  });
-  it('have a body with content for the user', () => {
-    const { getByTestId } = render(
-      <Wrapper>
-        <NewAllocReq isOpen toggle={() => !isOpen} />
-      </Wrapper>
-    );
     expect(getByTestId('request-body')).toBeDefined();
   });
 });
 
 describe('View Team Modal', () => {
-  it.todo(
-    'assert that the components of view team modal are showing relevant information for the user'
-  );
+  test('View Team Modal UI', () => {
+    const testProps = {
+      isOpen: true,
+      toggle: () => null,
+      pid: 1234
+    };
+
+    const testStore = mockStore({
+      allocations: {
+        teams: {
+          '1234': [
+            {
+              id: '123456',
+              username: 'testuser1',
+              role: 'Standard',
+              firstName: 'Test',
+              lastName: 'User1',
+              email: 'user1@gmail.com',
+              usageData: []
+            },
+            {
+              id: '012345',
+              username: 'testuser2',
+              role: 'Standard',
+              firstName: 'Test',
+              lastName: 'User2',
+              email: 'user2@gmail.com',
+              usageData: [
+                {
+                  usage: 0.5,
+                  resource: 'stampede2.tacc.utexas.edu',
+                  allocationId: 1,
+                  percentUsed: 0.005
+                },
+                {
+                  usage: 10,
+                  resource: 'frontera.tacc.utexas.edu',
+                  allocationId: 2,
+                  percentUsed: 10
+                }
+              ]
+            }
+          ]
+        },
+        loadingUsernames: {
+          '1234': {
+            loading: false
+          }
+        },
+        errors: {}
+      }
+    });
+
+    // Render Modal
+    const { getByText, queryByText, getByRole } = render(
+      <Provider store={testStore}>
+        <TeamView {...testProps} />
+      </Provider>
+    );
+    
+    // Check for the list of users
+    expect(getByText(/View Team/)).toBeDefined();
+    expect(getByText(/Test User1/)).toBeDefined();
+    expect(getByText(/Test User2/)).toBeDefined();
+    
+    // View Information for the user without usage
+    fireEvent.click(getByText(/Test User1/));
+    expect(getByText(/Username:/)).toBeDefined();
+    expect(getByText(/Email:/)).toBeDefined();
+    expect(queryByText(/Usage/)).toBeNull();
+    
+    // View information for the user with usage
+    fireEvent.click(getByText(/Test User2/));
+    expect(getByText(/Usage/)).toBeDefined();
+  });
 });
