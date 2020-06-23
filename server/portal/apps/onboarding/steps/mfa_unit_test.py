2ad1126e459b9dd6e99ca8d8cacf1e197f76a0d1
Nathan Franklin <nathan.franklin@gmail.com>
2020-06-23 15:21:06 -0500

    Convert to pytest

--- server/portal/apps/onboarding/steps/mfa_unit_test.py
+++ server/portal/apps/onboarding/steps/mfa_unit_test.py
@@ -1,79 +1,77 @@
-from django.test import TestCase, RequestFactory
-from django.contrib.auth import get_user_model
-from mock import patch, ANY, MagicMock
+from mock import ANY, MagicMock
 from portal.apps.onboarding.steps.mfa import MFAStep
 import pytest
 
 
-@pytest.mark.django_db(transaction=True)
-class MFAStepTest(TestCase):
-    def setUp(self):
-        super(MFAStepTest, self).setUp()
-        self.user = get_user_model().objects.create_user('test', 'test@user.com', 'test')
+@pytest.fixture
+def mock_mfa_check(mocker):
+    yield mocker.patch('portal.apps.onboarding.steps.mfa_unit_test.MFAStep.mfa_check', autospec=True)
 
-        # Mock the step's complete function so we can spy on it
-        self.mock_complete_patcher = patch(
-            'portal.apps.onboarding.steps.mfa_unit_test.MFAStep.complete'
+
+@pytest.fixture
+def mock_mfa_log(mocker):
+    yield mocker.patch.object(MFAStep, 'log')
+
+
+@pytest.fixture
+def mock_mfa_complete(mocker):
+    yield mocker.patch.object(MFAStep, 'complete')
+
+
+@pytest.fixture
+def mock_mfa_request(mocker):
+    response = MagicMock(
+        json=MagicMock(
+            return_value={
+                "result": [
+                    {"type": "tacc-soft-token"}
+                ]
+            }
         )
-        self.mock_complete = self.mock_complete_patcher.start()
+    )
+    yield mocker.patch('portal.apps.onboarding.steps.mfa.requests.get',
+                       return_value=response,
+                       autospec=True)
 
-        # Mock the step's fail function so we can spy on it
-        self.mock_log_patcher = patch(
-            'portal.apps.onboarding.steps.mfa_unit_test.MFAStep.log'
-        )
-        self.mock_log = self.mock_log_patcher.start()
 
-        self.mock_response = MagicMock(
-            json=MagicMock(
-                return_value={
-                    "result": [
-                        {"type": "tacc-soft-token"}
-                    ]
-                }
-            )
-        )
+@pytest.fixture
+def mock_mfa_prepare(mocker):
+    yield mocker.patch.object(MFAStep, 'prepare')
 
-        # Mock get allocations
-        self.mock_requests_get_patcher = patch(
-            'portal.apps.onboarding.steps.mfa.requests.get',
-            return_value=self.mock_response
-        )
-        self.mock_requests_get = self.mock_requests_get_patcher.start()
 
-        self.step = MFAStep(self.user)
+def test_mfa_found(authenticated_user, mock_mfa_check, mock_mfa_complete):
+    mock_mfa_check.return_value = True
+    step = MFAStep(authenticated_user)
+    step.process()
+    mock_mfa_complete.assert_called_with(
+        "Multi-factor authentication pairing verified"
+    )
 
-    def tearDown(self):
-        super(MFAStepTest, self).tearDown()
-        self.mock_complete_patcher.stop()
-        self.mock_log_patcher.stop()
-        self.mock_requests_get_patcher.stop()
 
-    @patch('portal.apps.onboarding.steps.mfa_unit_test.MFAStep.mfa_check')
-    def test_mfa_found(self, mock_mfa_check):
-        mock_mfa_check.return_value = True
-        self.step.process()
-        self.mock_complete.assert_called_with(
-            "Multi-factor authentication pairing verified"
-        )
+def test_mfa_not_found(authenticated_user, mock_mfa_check, mock_mfa_log):
+    step = MFAStep(authenticated_user)
+    mock_mfa_check.return_value = False
+    step.process()
+    mock_mfa_log.assert_called_with(ANY, data=ANY)
 
-    @patch('portal.apps.onboarding.steps.mfa_unit_test.MFAStep.mfa_check')
-    def test_mfa_not_found(self, mock_mfa_check):
-        mock_mfa_check.return_value = False
-        self.step.process()
-        self.mock_log.assert_called_with(ANY, data=ANY)
 
-    def test_mfa_check(self):
-        result = self.step.mfa_check()
-        self.assertEqual(result, True)
+def test_mfa_check(authenticated_user, mock_mfa_request):
+    step = MFAStep(authenticated_user)
+    mock_mfa_request.return_value.json.return_value["result"] = [{"type": "tacc-soft-token"}]
+    result = step.mfa_check()
+    assert result is True
 
-    def test_mfa_check_failure(self):
-        self.mock_response.json.return_value["result"] = []
-        result = self.step.mfa_check()
-        self.assertEqual(result, False)
 
-    @patch('portal.apps.onboarding.steps.mfa_unit_test.MFAStep.prepare')
-    def test_client_action(self, mock_prepare):
-        request = RequestFactory().post("/api/onboarding/user/test")
-        request.user = self.user
-        self.step.client_action("user_confirm", {}, request)
-        mock_prepare.assert_called_with()
+def test_mfa_check_failure(authenticated_user, mock_mfa_request):
+    step = MFAStep(authenticated_user)
+    mock_mfa_request.return_value.json.return_value["result"] = []
+    result = step.mfa_check()
+    assert result is False
+
+
+def test_client_action(rf, authenticated_user, mock_mfa_prepare):
+    step = MFAStep(authenticated_user)
+    request = rf.post("/api/onboarding/user/test")
+    request.user = authenticated_user
+    step.client_action("user_confirm", {}, request)
+    mock_mfa_prepare.assert_called_with()
