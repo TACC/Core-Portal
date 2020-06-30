7c14a9d4c4411858b7b69d5ad5a8c366e1596055
Nathan Franklin <nathan.franklin@gmail.com>
2020-06-30 16:43:49 -0500

    Refactor top level onboarding view tests

--- server/conftest.py
+++ server/conftest.py
@@ -2,9 +2,11 @@ import pytest
 from portal.apps.auth.models import AgaveOAuthToken
 from portal.apps.accounts.models import PortalProfile
 
+
 @pytest.fixture
 def mock_agave_client(mocker):
     yield mocker.patch('portal.apps.auth.models.AgaveOAuthToken.client', autospec=True)
+
 
 @pytest.fixture
 def authenticated_user(client, django_user_model, django_db_reset_sequences, mock_agave_client):
@@ -24,6 +26,7 @@ def authenticated_user(client, django_user_model, django_db_reset_sequences, moc
     client.login(username="username", password='password')
     yield user
 
+
 @pytest.fixture
 def staff_user(client, django_user_model, django_db_reset_sequences, mock_agave_client):
     django_user_model.objects.create_user(username='staff', password='password')
@@ -39,4 +42,21 @@ def staff_user(client, django_user_model, django_db_reset_sequences, mock_agave_
         created=1523633447)
     token.save()
     client.login(username="staff", password='password')
+    yield user
+
+
+@pytest.fixture
+def user2(client, django_user_model, django_db_reset_sequences, mock_agave_client):
+    django_user_model.objects.create_user(username='username2', password='password')
+    user = django_user_model.objects.get(username='username2')
+    token = AgaveOAuthToken.objects.create(
+        user=user,
+        token_type="bearer",
+        scope="default",
+        access_token="1234fsf",
+        refresh_token="123123123",
+        expires_in=14400,
+        created=1523633447)
+    token.save()
+    profile = PortalProfile.objects.create(user=user)
     yield user
--- server/portal/apps/onboarding/views_unit_test.py
+++ server/portal/apps/onboarding/views_unit_test.py
@@ -1,8 +1,6 @@
-from mock import patch, ANY
-from django.contrib.auth import get_user_model
+from builtins import type
+
 from django.http import HttpResponseRedirect
-from django.test import TestCase, RequestFactory
-from portal.apps.auth.models import AgaveOAuthToken
 from portal.apps.onboarding.views import SetupStatusView
 import pytest
 
@@ -10,82 +8,59 @@ import pytest
 pytestmark = pytest.mark.django_db
 
 
-class TestSetupStatusView(TestCase):
-    def setUp(self):
-        super(TestSetupStatusView, self).setUp()
-        User = get_user_model()
-        self.user = User.objects.create_user("mockuser", "mock@user.com", "password")
-        self.user.first_name = "mock"
-        self.user.last_name = "user"
-        token = AgaveOAuthToken(
-            token_type="bearer",
-            scope="default",
-            access_token="1234fsf",
-            refresh_token="123123123",
-            expires_in=14400,
-            created=1523633447)
-        token.user = self.user
-        token.save()
-        self.user.save()
+@pytest.fixture
+def render_mock(mocker):
+    yield mocker.patch('portal.apps.onboarding.views.render')
 
-        self.rf = RequestFactory()
 
-        self.render_patcher = patch('portal.apps.onboarding.views.render')
-        self.mock_render = self.render_patcher.start()
+@pytest.fixture
+def logout_mock(mocker):
+    yield mocker.patch('portal.apps.onboarding.views.logout')
 
-        self.mock_client_patcher = patch('portal.apps.auth.models.AgaveOAuthToken.client')
-        self.mock_client = self.mock_client_patcher.start()
 
-        self.mock_logout_patcher = patch('portal.apps.onboarding.views.logout')
-        self.mock_logout = self.mock_logout_patcher.start()
+def test_get_self(rf, authenticated_user, render_mock):
+    # Test when view is called with a username parameter that matches
+    # the requesting user, but the requester is not a staff member
+    request = rf.get("/accounts/setup")
+    request.user = authenticated_user
 
-        self.view = SetupStatusView()
+    view = SetupStatusView()
+    view.get(request, authenticated_user.username)
+    render_mock.assert_called_with(
+        request,
+        'portal/apps/onboarding/setup.html',
+        {
+            "first_name": authenticated_user.first_name,
+            "last_name": authenticated_user.last_name,
+            "email":  authenticated_user.email,
+            "username": authenticated_user.username
+        }
+    )
 
-    def tearDown(self):
-        super(TestSetupStatusView, self).tearDown()
-        self.render_patcher.stop()
-        self.mock_client_patcher.stop()
-        self.mock_logout_patcher.stop()
 
-    def test_get_self(self):
-        # Test when view is called with a username parameter that matches
-        # the requesting user, but the requester is not a staff member
-        request = self.rf.get("/accounts/setup")
-        request.user = self.user
-        self.view.get(request, "mockuser")
-        self.mock_render.assert_called_with(
-            request,
-            'portal/apps/onboarding/setup.html',
-            {
-                "first_name": "mock",
-                "last_name": "user",
-                "email": "mock@user.com",
-                "username": "mockuser"
-            }
-        )
+def test_get_as_staff(rf, user2, staff_user, render_mock):
+    # Test when a staff member gets another user
+    request = rf.get("/accounts/setup")
+    request.user = staff_user
+    view = SetupStatusView()
+    view.get(request, user2.username)
+    render_mock.assert_called_with(
+        request,
+        'portal/apps/onboarding/setup.html',
+        {
+            "first_name": user2.first_name,
+            "last_name": user2.last_name,
+            "email":  user2.email,
+            "username": user2.username
+        }
+    )
 
-    def test_get_as_staff(self):
-        # Test when a staff member gets another user
-        request = self.rf.get("/accounts/setup")
-        self.user.username = "staff"
-        self.user.is_staff = True
-        request.user = self.user
-        self.view.get(request, "mockuser")
-        self.mock_render.assert_called_with(
-            request,
-            'portal/apps/onboarding/setup.html',
-            {
-                "first_name": "mock",
-                "last_name": "user",
-                "email": "mock@user.com",
-                "username": "mockuser"
-            }
-        )
 
-    def test_get_forbidden(self):
-        # Test when a non-staff user tries to get another user
-        request = self.rf.get("/accounts/setup")
-        request.user = self.user
-        response = self.view.get(request, "otheruser")
-        self.mock_logout.assert_called_with(ANY)
-        self.assertEqual(type(response), HttpResponseRedirect)
+def test_get_forbidden(mocker, rf, authenticated_user, user2, logout_mock):
+    # Test when a non-staff user tries to get another user
+    request = rf.get("/accounts/setup")
+    request.user = authenticated_user
+    view = SetupStatusView()
+    response = view.get(request, user2.username)
+    logout_mock.assert_called_with(mocker.ANY)
+    assert type(response) == HttpResponseRedirect
