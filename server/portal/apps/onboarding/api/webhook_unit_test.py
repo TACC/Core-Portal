756bdb248b4df25278e0d9ca4543eab6ccf3fc2e
jchuahtacc <jchuah@tacc.utexas.edu>
2020-07-02 14:52:58 -0500

    Fixtures and route unit tests for webhooks

--- server/portal/apps/onboarding/api/webhook_unit_test.py
+++ server/portal/apps/onboarding/api/webhook_unit_test.py
@@ -17,6 +17,54 @@ from unittest import skip
 pytestmark = pytest.mark.django_db
 
 
+@pytest.fixture(autouse=True)
+def mocked_executor(mocker):
+    yield mocker.patch('portal.apps.onboarding.api.webhook.execute_setup_steps')
+
+
+@pytest.fixture
+def mock_webhook_step(regular_user):
+    ev = SetupEvent.objects.create(
+        user=regular_user,
+        step="portal.apps.onboarding.steps.test_steps.MockWebhookStep",
+        state=SetupState.WEBHOOK
+    )
+    ev.save()
+    yield ev
+
+
+@pytest.fixture
+def webhook_request_data():
+    yield {
+        "username": "username",
+        "step": "portal.apps.onboarding.steps.test_steps.MockWebhookStep",
+        "webhook_data": {"key": "value"}
+    }
+
+
+@pytest.fixture
+def webhook_request(rf):
+    # Return a request post with authoriation header of
+    # Basic: dev:dev (base 64 encoded)
+    yield rf.post(
+        "/webhooks/onboarding/",
+        content_type="application/json",
+        HTTP_AUTHORIZATION="Basic: ZGV2OmRldg=="
+    )
+
+
+def test_route(client, mock_webhook_step, webhook_request_data):
+    """
+    Test the route to see that the HTTP Basic Auth works
+    """
+    response = client.post(
+        "/webhooks/onboarding/",
+        content_type="application/json",
+        data=json.dumps(webhook_request_data),
+        **{"HTTP_AUTHORIZATION": "Basic: ZGV2OmRldg=="}
+    )
+    assert response.status_code == 200
+
 @skip("Need to rewrite onboarding unit tests with fixtures")
 class SetupStepWebhookTest(TestCase):
     def setUp(self):
