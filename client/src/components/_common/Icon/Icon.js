da4a6989765c8165525fd6144a568b8309de95f5
Wesley Bomar <wbomar@tacc.utexas.edu>
2020-07-13 14:02:58 -0500

    FP-354: Accessible icon text. Missing icon class.

--- client/src/components/_common/Icon/Icon.js
+++ client/src/components/_common/Icon/Icon.js
@@ -2,15 +2,20 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './Icon.module.css';
 
-const Icon = ({ name, className }) => {
-  const iconClassName = `icon-${name}`;
+const Icon = ({ children, name, className }) => {
+  const iconClassName = `icon icon-${name}`;
   const fullClassName = [className, iconClassName].join(' ');
 
-  return <i className={fullClassName} data-testid="icon" />;
+  return (
+    <i className={fullClassName} data-testid="icon">
+      {children}
+    </i>
+  );
 };
 Icon.propTypes = {
   name: PropTypes.string.isRequired,
-  className: PropTypes.string
+  className: PropTypes.string,
+  children: PropTypes.string.isRequired
 };
 Icon.defaultProps = {
   className: ''
