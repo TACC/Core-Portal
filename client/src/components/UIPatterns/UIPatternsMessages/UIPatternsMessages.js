aaac1e1e76acc4c38065fb5b0ec01a1d244b4449
W. Bomar <62723358+tacc-wbomar@users.noreply.github.com>
2020-07-15 17:00:22 -0500

    FP-354/FP-537: Add Message UI Patterns (#95)
    Follow up to FP-354 that was pending FP-537

--- client/src/components/UIPatterns/UIPatternsMessages/UIPatternsMessages.js
+++ client/src/components/UIPatterns/UIPatternsMessages/UIPatternsMessages.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import { Message } from '_common';
+import './UIPatternsMessages.module.css';
 
 function UIPatternsMessages() {
   return (
-    <span>
-      <span role="img" aria-label="Warning">
-        ⚠️
-      </span>{' '}
-      <span>This is unstyled placeholder message markup</span>
-    </span>
+    <div styleName="container">
+      <Message type="info" styleName="item">
+        All your information, are belong to us.
+      </Message>
+      <Message type="success" styleName="item">
+        All your success, are belong to us.
+      </Message>
+      <Message type="warn" styleName="item">
+        All your warning, are come from us.
+      </Message>
+      <Message type="error" styleName="item">
+        All your error, are belong to you.
+      </Message>
+    </div>
   );
 }
 
