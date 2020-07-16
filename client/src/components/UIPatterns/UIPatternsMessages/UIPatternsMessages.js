da221f242d0e27a29e71a711fda9feed64874ea8
Wesley Bomar <wbomar@tacc.utexas.edu>
2020-07-16 13:13:51 -0500

    FP-354/FP-537: Fix Unit Test i.e. Remove styleName

--- client/src/components/UIPatterns/UIPatternsMessages/UIPatternsMessages.js
+++ client/src/components/UIPatterns/UIPatternsMessages/UIPatternsMessages.js
@@ -3,38 +3,30 @@ import { Message } from '_common';
 
 function UIPatternsMessages() {
   return (
-    <dl styleName="container">
+    <dl>
       <dt>
         Information (<code>info</code>)
       </dt>
       <dd>
-        <Message type="info" styleName="item">
-          All your information, are belong to us.
-        </Message>
+        <Message type="info">All your information, are belong to us.</Message>
       </dd>
       <dt>
         Success (<code>success</code>)
       </dt>
       <dd>
-        <Message type="success" styleName="item">
-          All your success, are belong to us.
-        </Message>
+        <Message type="success">All your success, are belong to us.</Message>
       </dd>
       <dt>
         Warning (<code>warn</code>)
       </dt>
       <dd>
-        <Message type="warn" styleName="item">
-          All your warning, are come from us.
-        </Message>
+        <Message type="warn">All your warning, are come from us.</Message>
       </dd>
       <dt>
         Error (<code>error</code>)
       </dt>
       <dd>
-        <Message type="error" styleName="item">
-          All your error, are belong to you.
-        </Message>
+        <Message type="error">All your error, are belong to you.</Message>
       </dd>
     </dl>
   );
