696e74f1c14bbb922d7e61cd642b8d1d12e31f78
Wesley Bomar <wbomar@tacc.utexas.edu>
2020-07-20 10:16:16 -0500

    FP-354: Render/Style Links in UPatternsMessages

--- client/src/components/UIPatterns/UIPatternsMessages/UIPatternsMessages.js
+++ client/src/components/UIPatterns/UIPatternsMessages/UIPatternsMessages.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import { Message } from '_common';
 
+const EXAMPLE_LINK = (
+  <a href="#" className="wb-link">
+    Read more.
+  </a>
+);
+
 function UIPatternsMessages() {
   return (
     <dl>
@@ -8,25 +14,33 @@ function UIPatternsMessages() {
         Information (<code>info</code>)
       </dt>
       <dd>
-        <Message type="info">All your information, are belong to us.</Message>
+        <Message type="info">
+          All your information, are belong to us. {EXAMPLE_LINK}
+        </Message>
       </dd>
       <dt>
         Success (<code>success</code>)
       </dt>
       <dd>
-        <Message type="success">All your success, are belong to us.</Message>
+        <Message type="success">
+          All your success, are belong to us. {EXAMPLE_LINK}
+        </Message>
       </dd>
       <dt>
         Warning (<code>warn</code>)
       </dt>
       <dd>
-        <Message type="warn">All your warning, are come from us.</Message>
+        <Message type="warn">
+          All your warning, are come from us. {EXAMPLE_LINK}
+        </Message>
       </dd>
       <dt>
         Error (<code>error</code>)
       </dt>
       <dd>
-        <Message type="error">All your error, are belong to you.</Message>
+        <Message type="error">
+          All your error, are belong to you. {EXAMPLE_LINK}
+        </Message>
       </dd>
     </dl>
   );
