d9e583983ac5c0b35282997cdd426ce0680c3df1
jchuahtacc <jchuah@tacc.utexas.edu>
2020-07-21 11:51:17 -0500

    Strip down fixture

--- client/src/components/DataFiles/DataFilesModals/tests/DataFilesCopyModal.fixture.js
+++ client/src/components/DataFiles/DataFilesModals/tests/DataFilesCopyModal.fixture.js
@@ -4,12 +4,7 @@ const DataFilesCopyModalFixture = {
     modal: false
   },
   operationStatus: {
-    rename: null,
-    move: {},
-    copy: {},
-    select: {},
-    upload: {},
-    trash: {}
+    copy: {}
   },
   loadingScroll: {
     FilesListing: false,
@@ -75,39 +70,10 @@ const DataFilesCopyModalFixture = {
     modal: true
   },
   modals: {
-    preview: false,
-    move: false,
     copy: true,
-    select: false,
-    upload: false,
-    mkdir: false,
-    rename: false,
-    pushKeys: false,
-    trash: false
   },
   modalProps: {
-    preview: {},
-    move: {},
-    copy: {
-      selectedFiles: [
-        {
-          name: 'testfile',
-          path: '/testfile',
-          lastModified: '2020-07-01T10:12:36-05:00',
-          length: 4096,
-          permissions: 'ALL',
-          format: 'folder',
-          system: 'test.system',
-          mimeType: 'text/directory',
-          type: 'dir'
-        }
-      ]
-    },
-    select: {},
-    upload: {},
-    mkdir: {},
-    rename: {},
-    pushKeys: {}
+    copy: {},
   },
   previewHref: ''
 };
