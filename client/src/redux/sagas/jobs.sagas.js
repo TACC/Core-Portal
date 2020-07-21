0a57e88f01a5ae633270b6983dfc9ce03e262cb3
Nathan Franklin <nathan.franklin@gmail.com>
2020-07-20 22:24:49 -0500

    Use app info to improve job display

--- client/src/redux/sagas/jobs.sagas.js
+++ client/src/redux/sagas/jobs.sagas.js
@@ -69,25 +69,6 @@ function* submitJob(action) {
   }
 }
 
-/*
-if (!param.value.visible || param.id.startsWith('_')) {
-  return;
-}
-function getParameterInputInformatin(id, value, appEntryInfo){
-  const result = { name: id, value, visible: true };
-  if (id.startsWith('_')) {
-    result.visible = false;
-  }
-  derive better name for ui from app definition if therje is one
-  and if visible
-  and if enum get key
-  if(appInfo && result.visible) {
-  }
-
-  return result;
-}
-*/
-
 export function* getJobDetails(action) {
   const { jobId } = action.payload;
   yield put({
@@ -100,19 +81,24 @@ export function* getJobDetails(action) {
       params: { job_id: jobId }
     });
     const job = jobsReponse.response;
+    /* todo filter out any input/params that startsWith('_') */
     const display = {
       applicationName: job.appId,
       systemName: job.systemId,
-      inputs: Object.entries(job.inputs).map(([key, val]) => ({
-        label: key,
-        id: key,
-        value: val
-      })),
-      parameters: Object.entries(job.parameters).map(([key, val]) => ({
-        label: key,
-        id: key,
-        value: val
-      }))
+      inputs: Object.entries(job.inputs)
+        .map(([key, val]) => ({
+          label: key,
+          id: key,
+          value: val
+        }))
+        .filter(obj => !obj.id.startsWith('_')),
+      parameters: Object.entries(job.parameters)
+        .map(([key, val]) => ({
+          label: key,
+          id: key,
+          value: val
+        }))
+        .filter(obj => !obj.id.startsWith('_'))
     };
 
     let app = null;
@@ -122,14 +108,54 @@ export function* getJobDetails(action) {
         params: { app_id: job.appId }
       });
       app = res.response;
+    } catch (error) {
+      /* ignore if we can't get app info */
+    }
 
+    if (app) {
       // Improve any values with app information
       display.applicationName = app.label;
 
       // Improve input/parameters
-      // TODO
-    } catch (error) {
-      /* ignore if we can't get app info */
+      display.inputs.forEach(input => {
+        const matchingParameter = app.inputs.find(obj => {
+          return input.id === obj.id;
+        });
+        if (matchingParameter) {
+          // eslint-disable-next-line no-param-reassign
+          input.label = matchingParameter.details.label;
+        }
+      });
+      display.parameters.forEach(input => {
+        const matchingParameter = app.parameters.find(obj => {
+          return input.id === obj.id;
+        });
+        if (matchingParameter) {
+          // eslint-disable-next-line no-param-reassign
+          input.label = matchingParameter.details.label;
+        }
+      });
+      // filter non-visible
+      display.inputs.filter(input => {
+        const matchingParameter = app.inputs.find(obj => {
+          return input.id === obj.id;
+        });
+        if (matchingParameter) {
+          return matchingParameter.value.visible;
+        }
+        return true;
+      });
+      display.parameters.filter(input => {
+        const matchingParameter = app.parameters.find(obj => {
+          return input.id === obj.id;
+        });
+        if (matchingParameter) {
+          return matchingParameter.value.visible;
+        }
+        return true;
+      });
+      // todo fix order of inputs/parameters (?)
+      // todo show enum display value but actual value
     }
 
     yield put({
