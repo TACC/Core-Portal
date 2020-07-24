607ded96643fcd49d7319d8d0482d237f44c8957
Strmiska <keith.strmiska@gmail.com>
2020-07-24 16:13:52 -0500

    add store allocation information from tas during onboarding and conditionally display active systems

--- server/portal/apps/accounts/managers/accounts.py
+++ server/portal/apps/accounts/managers/accounts.py
@@ -102,6 +102,7 @@ def setup(username, system):
     home_dir = mgr.get_or_create_dir(user)
     home_sys = mgr.get_or_create_system(user)
     if not user.profile.setup_complete:
+        # Will this ever run if "profile.setup_complete" is set to True before this is ever called?
         logger.info("Executing setup steps for %s", username)
         execute_setup_steps(user.username)
 
