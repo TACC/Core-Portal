b3f82dce95b123c97d2cac968ec75d0f822cb815
Nathan Franklin <nathan.franklin@gmail.com>
2020-07-27 22:30:44 -0500

    Fix breadcrumbs fontsize for modals and in non-modal listing

--- client/src/components/DataFiles/DataFilesBreadcrumbs/DataFilesBreadcrumbs.js
+++ client/src/components/DataFiles/DataFilesBreadcrumbs/DataFilesBreadcrumbs.js
@@ -58,7 +58,14 @@ BreadcrumbLink.propTypes = {
   children: PropTypes.element.isRequired
 };
 
-const DataFilesBreadcrumbs = ({ api, scheme, system, path, section }) => {
+const DataFilesBreadcrumbs = ({
+  api,
+  scheme,
+  system,
+  path,
+  section,
+  className
+}) => {
   const paths = [];
   const pathComps = [];
   path
@@ -83,7 +90,7 @@ const DataFilesBreadcrumbs = ({ api, scheme, system, path, section }) => {
   })();
 
   return (
-    <div className="breadcrumbs">
+    <div className={`breadcrumbs ${className}`}>
       <BreadcrumbLink
         api={api}
         scheme={scheme}
@@ -124,7 +131,12 @@ DataFilesBreadcrumbs.propTypes = {
   scheme: PropTypes.string.isRequired,
   system: PropTypes.string.isRequired,
   path: PropTypes.string.isRequired,
-  section: PropTypes.string.isRequired
+  section: PropTypes.string.isRequired,
+  /** Additional className for the root element */
+  className: PropTypes.string
+};
+DataFilesBreadcrumbs.defaultProps = {
+  className: ''
 };
 
 export default DataFilesBreadcrumbs;
