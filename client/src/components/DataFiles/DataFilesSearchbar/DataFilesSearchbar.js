cc12391098b163c3a7920358f8192e7135b8d3d1
Wesley Bomar <wbomar@tacc.utexas.edu>
2020-08-03 11:21:00 -0500

    FP-341: UI/UX: FP-508: Add dropdown

--- client/src/components/DataFiles/DataFilesSearchbar/DataFilesSearchbar.js
+++ client/src/components/DataFiles/DataFilesSearchbar/DataFilesSearchbar.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import { useHistory } from 'react-router-dom';
 import { Button } from 'reactstrap';
 import queryString from 'query-string';
-import { Icon } from '_common';
+import { Icon, DropdownSelector } from '_common';
 // FP-563: Support count in status message
 // import DataFilesSearchbarStatus from './DataFilesSearchbarStatus';
 
@@ -11,6 +11,7 @@ import './DataFilesSearchbar.module.css';
 
 const DataFilesSearchbar = ({ api, scheme, system, className }) => {
   const [query, setQuery] = useState('');
+  const [filter, setFilter] = useState('');
   const history = useHistory();
   const sectionName = 'My Data';
 
@@ -19,7 +20,10 @@ const DataFilesSearchbar = ({ api, scheme, system, className }) => {
       ? `?${queryString.stringify({ query_string: query })}`
       : '';
 
+    // !!!: How to filter?
     history.push(`/workbench/data/${api}/${scheme}/${system}/${qs}`);
+    console.log({ filter });
+    // history.push(`/workbench/data/${api}/${scheme}/${system}/${qs}/${filter}`);
   };
   const emptySearch = () => {};
 
@@ -32,6 +36,7 @@ const DataFilesSearchbar = ({ api, scheme, system, className }) => {
     e.preventDefault();
   };
   const onChange = e => setQuery(e.target.value);
+  const onFilter = e => setFilter(e.target.value);
 
   return (
     <form
@@ -71,9 +76,20 @@ const DataFilesSearchbar = ({ api, scheme, system, className }) => {
       </fieldset> */}
       {/* FP-505: Implement Filter Dropdown */}
       <fieldset styleName="filter-fieldset">
-        <select>
-          <option>No filter options, yet</option>
-        </select>
+        <DropdownSelector type="single" onChange={onFilter}>
+          <option value="any">All Types</option>
+          <option value="audio">Audio</option>
+          <option value="code">Code</option>
+          <option value="doc">Documents</option>
+          <option value="folder">Folders</option>
+          <option value="image">Images</option>
+          <option value="jupyter">Jupyter Notebook</option>
+          <option value="pdf">PDF</option>
+          <option value="presentation">Presentation</option>
+          <option value="spreadsheet">Spreadsheet</option>
+          <option value="shape">Shape File</option>
+          <option value="zip">ZIP</option>
+        </DropdownSelector>
       </fieldset>
       <Button
         type="reset"
