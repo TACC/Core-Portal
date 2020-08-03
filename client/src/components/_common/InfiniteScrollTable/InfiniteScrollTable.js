6ebf87f2b352f6df4e95a8bdd344f4c66da7a476
W. Bomar <62723358+tacc-wbomar@users.noreply.github.com>
2020-08-03 17:33:10 -0500

    Task/fp 464 table loading icon (#62)
    * FP-464: Quick fix to use big loading icon
    
    * FP-464: Improve loading UI for InfiniteScrollTable
    
    * FP-464: Icon Placement/Size Depends on Data
    
    * FP-464: Remove commented hack test code

--- client/src/components/_common/InfiniteScrollTable/InfiniteScrollTable.js
+++ client/src/components/_common/InfiniteScrollTable/InfiniteScrollTable.js
@@ -4,26 +4,55 @@ import PropTypes from 'prop-types';
 import LoadingSpinner from '../LoadingSpinner';
 import './InfiniteScrollTable.module.scss';
 
+/**
+ * Get a modifier for a class name, based on given conditions
+ * @param {string} baseName - A className on which to append the modifier name
+ * @param {object} conditions - Conditions to allow modifier name to be determined
+ * @param {object} conditions.isLoading - Whether data is loading
+ * @param {object} conditions.hasData - Whether data is available
+ */
+function getClassNameModifier(baseName, { isLoading, hasData }) {
+  let modifiers = [];
+
+  if (isLoading) {
+    modifiers.push('is-loading');
+  }
+  if (hasData) {
+    modifiers.push('has-data');
+  } else {
+    modifiers.push('no-data');
+  }
+
+  // Add the base name
+  modifiers = modifiers.map(modifier => modifier + baseName);
+
+  return modifiers.join(' ');
+}
+
 const rowContentPropType = PropTypes.oneOfType([
   PropTypes.string,
   PropTypes.element,
   PropTypes.oneOf([React.Fragment])
 ]);
 
-const InfiniteScrollLoadingRow = ({ isLoading }) => {
+const InfiniteScrollLoadingRow = ({ isLoading, hasData }) => {
   if (!isLoading) {
     return null;
   }
+
+  const placement = hasData ? 'inline' : 'section';
+
   return (
-    <tr styleName="status status--is-loading">
+    <tr styleName="status">
       <td styleName="status__message">
-        <LoadingSpinner placement="inline" />
+        <LoadingSpinner placement={placement} />
       </td>
     </tr>
   );
 };
 InfiniteScrollLoadingRow.propTypes = {
-  isLoading: PropTypes.bool.isRequired
+  isLoading: PropTypes.bool.isRequired,
+  hasData: PropTypes.bool.isRequired
 };
 
 const InfiniteScrollNoDataRow = ({ display, noDataText }) => {
@@ -31,7 +60,7 @@ const InfiniteScrollNoDataRow = ({ display, noDataText }) => {
     return null;
   }
   return (
-    <tr styleName="status status--no-data">
+    <tr styleName="status">
       <td styleName="status__message  cell cell--has-text-nodes">
         {noDataText}
       </td>
@@ -54,6 +83,11 @@ const InfiniteScrollTable = ({
 }) => {
   const columns = React.useMemo(() => tableColumns, []);
   const data = React.useMemo(() => tableData, [tableData]);
+  const hasData = tableData.length !== 0;
+  const modifierStyleName = getClassNameModifier('', {
+    isLoading,
+    hasData
+  });
 
   const {
     getTableProps,
@@ -74,8 +108,8 @@ const InfiniteScrollTable = ({
   return (
     <table
       {...getTableProps()}
-      styleName="container"
-      className={`${className}  o-fixed-header-table`}
+      className={`${className} o-fixed-header-table`}
+      styleName={`container ${modifierStyleName}`}
     >
       <thead>
         {headerGroups.map(headerGroup => (
@@ -112,9 +146,9 @@ const InfiniteScrollTable = ({
             </tr>
           );
         })}
-        <InfiniteScrollLoadingRow isLoading={isLoading} />
+        <InfiniteScrollLoadingRow isLoading={isLoading} hasData={hasData} />
         <InfiniteScrollNoDataRow
-          display={!isLoading && tableData.length === 0}
+          display={!isLoading && !hasData}
           noDataText={noDataText}
         />
       </tbody>
