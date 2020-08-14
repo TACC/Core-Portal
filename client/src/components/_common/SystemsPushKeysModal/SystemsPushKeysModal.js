ca8390ba7badfee06ff76eec1780716ce717e685
Jarosenb <jrosenberg@tacc.utexas.edu>
2020-08-14 14:46:31 -0500

    Add validation to push keys form

--- client/src/components/_common/SystemsPushKeysModal/SystemsPushKeysModal.js
+++ client/src/components/_common/SystemsPushKeysModal/SystemsPushKeysModal.js
@@ -1,19 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory, useLocation } from 'react-router-dom';
-import {
-  Button,
-  Modal,
-  ModalHeader,
-  ModalBody,
-  ModalFooter,
-  Form,
-  FormGroup,
-  Label,
-  Input
-} from 'reactstrap';
-import Icon from '../Icon';
-import LoadingSpinner from '../LoadingSpinner';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import { Formik, Form } from 'formik';
+import * as Yup from 'yup';
+import { FormField, Message, LoadingSpinner, Icon } from '_common';
 
 const SystemsPushKeysModal = () => {
   const dispatch = useDispatch();
@@ -22,8 +13,6 @@ const SystemsPushKeysModal = () => {
   const { error, onSuccess, system, submitting } = useSelector(
     state => state.pushKeys.modalProps.pushKeys
   );
-  const [password, setPassword] = useState('');
-  const [token, setToken] = useState('');
 
   const history = useHistory();
   const location = useLocation();
@@ -31,10 +20,7 @@ const SystemsPushKeysModal = () => {
     history.push(location.pathname);
   };
 
-  const onClosed = () => {
-    setPassword('');
-    setToken('');
-  };
+  const onClosed = () => {};
 
   const toggle = () => {
     dispatch({
@@ -43,7 +29,7 @@ const SystemsPushKeysModal = () => {
     });
   };
 
-  const pushKeys = () => {
+  const pushKeys = ({ password, token }) => {
     const hostnames = system.login
       ? [system.login.host, system.storage.host]
       : [system.storage.host];
@@ -63,11 +49,24 @@ const SystemsPushKeysModal = () => {
     });
   };
 
+  const validationSchema = Yup.object().shape({
+    password: Yup.string()
+      .min(1)
+      .required('Please enter your TACC password.'),
+    token: Yup.string()
+      .min(1)
+      .required('Please provide a valid TACC token.')
+  });
+
+  const initialValues = {
+    password: '',
+    token: ''
+  };
+
   let buttonIcon;
   if (submitting) {
     buttonIcon = <LoadingSpinner placement="inline" />;
-  }
-  if (error) {
+  } else if (error) {
     buttonIcon = <Icon name="alert">Warning</Icon>;
   }
 
@@ -81,79 +80,70 @@ const SystemsPushKeysModal = () => {
           onClosed={onClosed}
           toggle={toggle}
         >
-          <Form>
-            <ModalHeader toggle={toggle}>
-              Authenticate with TACC Token
-            </ModalHeader>
-            {error ? <div style={{ color: 'red' }}>{error.message}</div> : null}
-            <ModalBody>
-              <p>
-                To proceed, you must authenticate to this system with a
-                six-digit one time passcode from the TACC Token mobile app at
-                least once. A public key will be pushed to your{' '}
-                <code>authorized_keys</code> file on the system below. This will
-                allow you to submit jobs to this system from this portal.
-              </p>
-              <FormGroup>
-                <Label for="pushKeysSysId">System ID</Label>
-                <Input name="pushKeysSysId" disabled value={system.id} />
-              </FormGroup>
-              <FormGroup>
-                <Label for="pushKeysSysType">System Type</Label>
-                <Input name="pushKeysSysType" disabled value={system.type} />
-              </FormGroup>
-              {system.login && (
-                <FormGroup>
-                  <Label for="pushKeysSysLogin">Login Host</Label>
-                  <Input
+          <Formik
+            initialValues={initialValues}
+            validationSchema={validationSchema}
+            onSubmit={pushKeys}
+          >
+            <Form>
+              <ModalHeader toggle={toggle}>
+                Authenticate with TACC Token
+              </ModalHeader>
+              <ModalBody>
+                {error && !submitting && (
+                  <Message type="error">{error.message}</Message>
+                )}
+                <p>
+                  To proceed, you must authenticate to this system with a
+                  six-digit one time passcode from the TACC Token mobile app at
+                  least once. A public key will be pushed to your{' '}
+                  <code>authorized_keys</code> file on the system below. This
+                  will allow you to submit jobs to this system from this portal.
+                </p>
+                <FormField
+                  name="pushKeysSysId"
+                  label="System ID"
+                  disabled
+                  value={system.id}
+                />
+                <FormField
+                  name="pushKeysSysType"
+                  label="System Type"
+                  disabled
+                  value={system.type}
+                />
+                {system.login && (
+                  <FormField
                     name="pushKeysSysLogin"
+                    label="Login Host"
                     disabled
                     value={system.login.host}
                   />
-                </FormGroup>
-              )}
-              <FormGroup>
-                <Label for="pushKeysSysStorage">Storage Host</Label>
-                <Input
+                )}
+                <FormField
                   name="pushKeysSysStorage"
+                  label="Storage Host"
                   disabled
                   value={system.storage.host}
                 />
-              </FormGroup>
-              <FormGroup>
-                <Label for="pushKeysPassword">Password</Label>
-                <Input
-                  name="pushKeysPassword"
+                <FormField
+                  name="password"
+                  label="Password"
                   type="password"
-                  autoComplete="tacc-password"
-                  onChange={e => setPassword(e.target.value)}
-                  value={password}
+                  required
                 />
-              </FormGroup>
-              <FormGroup>
-                <Label for="pushKeysToken">TACC Token</Label>
-                <Input
-                  name="pushKeysToken"
-                  autoComplete="one-time-code"
-                  onChange={e => setToken(e.target.value)}
-                  value={token}
-                />
-              </FormGroup>
-            </ModalBody>
-            <ModalFooter>
-              <Button
-                type="submit"
-                color="primary"
-                onClick={pushKeys}
-                disabled={submitting}
-              >
-                {buttonIcon} <span>Authenticate</span>
-              </Button>
-              <Button color="secondary" onClick={toggle}>
-                Close
-              </Button>
-            </ModalFooter>
-          </Form>
+                <FormField name="token" label="TACC Token" required />
+              </ModalBody>
+              <ModalFooter>
+                <Button type="submit" color="primary" disabled={submitting}>
+                  {buttonIcon} <span>Authenticate</span>
+                </Button>
+                <Button color="secondary" onClick={toggle}>
+                  Close
+                </Button>
+              </ModalFooter>
+            </Form>
+          </Formik>
         </Modal>
       )}
     </>
