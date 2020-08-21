8c2c9c7a72fc9ca5d6aa323df2ae93abcacec812
jchuahtacc <jchuah@tacc.utexas.edu>
2020-08-21 13:15:27 -0500

    task/FP-327 - Onboarding welcome messages (#191)
    * Alert messages
    
    * Revert "Alert messages"
    
    This reverts commit d6a404ad14bf507642604d6b6ff78c9b1a8de9e4.
    
    * Refactor messages to workbench component
    
    * Add welcome message state reducers and sagas
    
    * Refactor css
    
    * Dismissable messages
    
    * Dismissal is working
    
    * Fix workbench-content height artifact
    
    * eslint
    
    * New language from thbrown
    
    * eslint
    
    * eslint
    
    * eslint
    
    * eslint
    
    * Change color of messages to secondary
    
    Co-authored-by: Sal Tijerina <r.sal.tijerina@gmail.com>

--- client/src/components/Workbench/Workbench.js
+++ client/src/components/Workbench/Workbench.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import { Route, Switch, useRouteMatch, Redirect } from 'react-router-dom';
+import { Alert } from 'reactstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import Dashboard from '../Dashboard';
 import Allocations from '../Allocations';
@@ -22,6 +23,7 @@ function Workbench() {
   const showUIPatterns = isDebug;
   // Get systems and any other initial data we need from the backend
   useEffect(() => {
+    dispatch({ type: 'FETCH_WELCOME' });
     dispatch({ type: 'FETCH_SYSTEMS' });
     dispatch({ type: 'FETCH_AUTHENTICATED_USER' });
     dispatch({ type: 'FETCH_WORKBENCH' });
@@ -32,11 +34,78 @@ function Workbench() {
     dispatch({ type: 'FETCH_NOTIFICATIONS' });
   }, []);
 
+  const welcomeMessages = useSelector(state => state.welcomeMessages);
+
+  const onDismissWelcome = section => {
+    const newMessagesState = {
+      ...welcomeMessages,
+      [section]: false
+    };
+    dispatch({ type: 'SAVE_WELCOME', payload: newMessagesState });
+  };
+
   return (
     <div className="workbench-wrapper">
       <NotificationToast />
       <Sidebar />
       <div className="workbench-content">
+        <Switch>
+          <Route path={`${path}${ROUTES.DASHBOARD}`}>
+            <Alert
+              isOpen={welcomeMessages.dashboard}
+              toggle={() => onDismissWelcome('dashboard')}
+              color="secondary"
+              className="welcomeMessage"
+            >
+              This page allows you to monitor your job status, get help with
+              tickets, and view the status of the High Performance Computing
+              (HPC) systems.
+            </Alert>
+          </Route>
+          <Route path={`${path}${ROUTES.DATA}`}>
+            <Alert
+              isOpen={welcomeMessages.datafiles}
+              toggle={() => onDismissWelcome('datafiles')}
+              color="secondary"
+              className="welcomeMessage"
+            >
+              This page allows you to upload and manage your files.
+            </Alert>
+          </Route>
+          <Route path={`${path}${ROUTES.APPLICATIONS}`}>
+            <Alert
+              isOpen={welcomeMessages.applications}
+              toggle={() => onDismissWelcome('applications')}
+              color="secondary"
+              className="welcomeMessage"
+            >
+              This page allows you to submit jobs to the HPC systems or access
+              Cloud services using a variety of applications.
+            </Alert>
+          </Route>
+          <Route path={`${path}${ROUTES.ALLOCATIONS}`}>
+            <Alert
+              isOpen={welcomeMessages.allocations}
+              toggle={() => onDismissWelcome('allocations')}
+              color="secondary"
+              className="welcomeMessage"
+            >
+              This page allows you to monitor the status of allocations on the
+              HPC systems and view a breakdown of team usage.
+            </Alert>
+          </Route>
+          <Route path={`${path}${ROUTES.HISTORY}`}>
+            <Alert
+              isOpen={welcomeMessages.history}
+              toggle={() => onDismissWelcome('history')}
+              color="secondary"
+              className="welcomeMessage"
+            >
+              This page allows you to monitor a log of all previous job
+              submissions.
+            </Alert>
+          </Route>
+        </Switch>
         <Switch>
           <Route path={`${path}${ROUTES.DASHBOARD}`}>
             <Dashboard />
