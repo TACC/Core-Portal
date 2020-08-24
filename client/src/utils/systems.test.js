1ce5e7a99f8c950dbffe7da9a0cf01d11d44e9a2
Nathan Franklin <nathan.franklin@gmail.com>
2020-08-24 10:48:31 -0500

    Add test for finding display name of system

--- client/src/utils/systems.test.js
+++ client/src/utils/systems.test.js
@@ -1,10 +1,17 @@
 import {
   getSystemName,
-  findSystemName
+  findSystemDisplayName
 } from './systems';
+import systemsFixture from '../components/DataFiles/fixtures/DataFiles.systems.fixture'
+
 
 describe('systems utility functions', () => {
   it('get system name from host', () => {
     expect(getSystemName('stampede2.tacc.utexas.edu')).toEqual('Stampede2');
+  });
+  it('get system name from host', () => {
+    const systemList = systemsFixture.systemList;
+    expect(findSystemDisplayName(systemList, 'frontera.home.username')).toEqual('My Data (Frontera)');
+    expect(findSystemDisplayName(systemList, 'frontera.foo.bar')).toEqual('Frontera');
   });
 });
