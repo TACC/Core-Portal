251816d292272805bf67d2e334b7c89c14905c9f
Nathan Franklin <nathan.franklin@gmail.com>
2020-09-21 14:59:24 -0500

    Refactor ticket creation to check if user is authed

--- client/src/components/Tickets/TicketUnauthenticatedCreate.js
+++ client/src/components/Tickets/TicketUnauthenticatedCreate.js
@@ -1,17 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Navbar } from 'reactstrap';
+import { useDispatch, useSelector } from 'react-redux';
+
+import BrowserChecker from '../_common/BrowserChecker';
 import TicketCreateForm from './TicketCreateForm';
 import './TicketUnauthenticatedCreate.scss';
-import BrowserChecker from '../_common/BrowserChecker';
 
 function TicketUnauthenticatedCreate() {
+  const dispatch = useDispatch();
+  const authenticatedUser = useSelector(state => state.authenticatedUser.user);
+  useEffect(() => {
+    dispatch({ type: 'FETCH_AUTHENTICATED_USER' });
+  }, []);
   return (
     <>
       <Navbar className="ticket-unauthenticated-title">Add Ticket</Navbar>
 
       <div className="ticket-unauthenticated-create-form">
         <BrowserChecker />
-        <TicketCreateForm />
+        <TicketCreateForm authenticatedUser={authenticatedUser} />
       </div>
     </>
   );
