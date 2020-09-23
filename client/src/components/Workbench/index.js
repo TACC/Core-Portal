33eaca9c39e7f259721a7e4974ea3138399dc1b8
Owais Jamil <47395902+owaisj@users.noreply.github.com>
2020-09-23 16:57:38 -0500

    Task/FP-424: Remove unused Manage Account code (#200)
    * Removed unused templates and views
    
    Moved form field data to lib
    
    * Fixed AppRouter test
    
    Moved form_fields back to apps.accounts package
    
    * Remove login view
    
    * Remove captcha comments
    
    Removed logging
    
    * Message test warning fix
    
    * Remove call to messages.error
    
    * Hide empty fields

--- client/src/components/Workbench/index.js
+++ client/src/components/Workbench/index.js
@@ -1,6 +1,6 @@
 import { hot } from 'react-hot-loader/root';
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
 import Workbench from './Workbench';
 import ManageAccount from '../ManageAccount';
 import * as ROUTES from '../../constants/routes';
@@ -12,6 +12,7 @@ function AppRouter() {
       <Route path="/accounts/profile" component={ManageAccount} />
       <Route path={ROUTES.WORKBENCH} component={Workbench} />
       <Route path="/tickets/new" component={TicketUnauthenticatedCreate} />
+      <Redirect from="/accounts" to="/accounts/profile" />
     </Router>
   );
 }
