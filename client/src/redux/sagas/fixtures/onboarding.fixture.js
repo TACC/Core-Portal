f0ec5dc90a05267e57129abef6297ea76c3de0aa
Nathan Franklin <nathan.franklin@gmail.com>
2020-09-23 11:56:26 -0500

    Add missing onboarding api fixtures

---
+++ client/src/redux/sagas/fixtures/onboarding.fixture.js
@@ -0,0 +1,104 @@
+// Admin listing of all users (i.e. result of `api/onboarding/admin/`)
+export const onboardingAdminList = {
+  users: [
+    {
+      username: 'username',
+      setupComplete: false,
+      firstName: 'First',
+      lastName: 'Last',
+      dateJoined: '2020-09-23T16:42:37.623Z',
+      lastEvent: {
+        username: 'username',
+        state: 'failed',
+        step: 'portal.apps.onboarding.steps.test_steps.MockProcessingFailStep',
+        time: '2020-09-23 16:43:01.103968+00:00',
+        message: 'Failure',
+        data: null
+      },
+      email: 'username@tacc.utexas.edu'
+    }
+  ]
+};
+
+// Admin list of a single user (i.e. result of `api/onboarding/user/username`)
+export const onboardingAdminIndividualUser = {
+  username: 'username',
+  first_name: 'First',
+  last_name: 'Last',
+  setupComplete: false,
+  steps: [
+    {
+      data: null,
+      step:
+        'portal.apps.onboarding.steps.test_steps.MockProcessingCompleteStep',
+      displayName: 'Mock Processing Complete Step',
+      state: 'completed',
+      events: [
+        {
+          username: 'username',
+          state: 'completed',
+          step:
+            'portal.apps.onboarding.steps.test_steps.MockProcessingCompleteStep',
+          time: '2020-09-23 16:42:51.069506+00:00',
+          message: 'Completed',
+          data: null
+        },
+        {
+          username: 'username',
+          state: 'processing',
+          step:
+            'portal.apps.onboarding.steps.test_steps.MockProcessingCompleteStep',
+          time: '2020-09-23 16:42:41.029434+00:00',
+          message: 'Beginning automated processing',
+          data: null
+        },
+        {
+          username: 'username',
+          state: 'pending',
+          step:
+            'portal.apps.onboarding.steps.test_steps.MockProcessingCompleteStep',
+          time: '2020-09-23 16:42:37.848371+00:00',
+          message: 'Pending',
+          data: null
+        }
+      ]
+    },
+    {
+      data: null,
+      step: 'portal.apps.onboarding.steps.test_steps.MockProcessingFailStep',
+      displayName: 'Mock Processing Fail Step',
+      state: 'failed',
+      events: [
+        {
+          username: 'username',
+          state: 'failed',
+          step:
+            'portal.apps.onboarding.steps.test_steps.MockProcessingFailStep',
+          time: '2020-09-23 16:43:01.103968+00:00',
+          message: 'Failure',
+          data: null
+        },
+        {
+          username: 'username',
+          state: 'processing',
+          step:
+            'portal.apps.onboarding.steps.test_steps.MockProcessingFailStep',
+          time: '2020-09-23 16:42:51.110869+00:00',
+          message: 'Beginning automated processing',
+          data: null
+        },
+        {
+          username: 'username',
+          state: 'pending',
+          step:
+            'portal.apps.onboarding.steps.test_steps.MockProcessingFailStep',
+          time: '2020-09-23 16:42:37.884030+00:00',
+          message: 'Pending',
+          data: null
+        }
+      ]
+    }
+  ],
+  isStaff: true,
+  email: 'username@tacc.utexas.edu'
+};
