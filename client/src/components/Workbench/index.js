b665be8b140b138b41eeea9744114afa318f9ef6
Nathan Franklin <nathan.franklin@gmail.com>
2020-10-01 19:35:35 -0500

    Fix linting

--- client/src/components/Workbench/index.js
+++ client/src/components/Workbench/index.js
@@ -1,6 +1,6 @@
 import { hot } from 'react-hot-loader/root';
 import React from 'react';
-import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Workbench from './Workbench';
 import ManageAccount from '../ManageAccount';
 import * as ROUTES from '../../constants/routes';
