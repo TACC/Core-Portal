66bb1de2f4897ea14fb1b65fc4342dcf109b2290
Wesley Bomar <wbomar@tacc.utexas.edu>
2020-10-09 00:45:21 -0500

    FP-662: Use `Section` for `ManageAccount`

--- client/src/components/ManageAccount/ManageAccount.js
+++ client/src/components/ManageAccount/ManageAccount.js
@@ -1,9 +1,10 @@
 import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-import { Alert, Col, Row, Container } from 'reactstrap';
+import { Alert, Col, Row } from 'reactstrap';
 import { isEmpty } from 'lodash';
-import { LoadingSpinner } from '_common';
+
+import { LoadingSpinner, Section } from '_common';
 import Sidebar from '../Sidebar';
 import {
   RequiredInformation,
@@ -30,49 +31,59 @@ const ManageAccountView = () => {
     dispatch({ type: 'GET_PROFILE_DATA' });
   }, [dispatch, isLoading]);
   return (
-    <Container fluid className="manage-account-wrapper">
+    <div className="workbench-wrapper">
       <Sidebar />
-      <Container fluid className="manage-account-content">
-        <Row className="manage-account-header">
-          <h5>Manage Account</h5>
-          <Link to="/workbench/dashboard" style={{ fontWeight: '500' }}>
-            Back to Dashboard
-          </Link>
-        </Row>
-        <Row className="user-profile">
-          <Col lg="8" className="user-profile-main">
-            {isLoading ? (
-              <LoadingSpinner />
-            ) : (
-              <>
-                {errors.data && (
-                  <Alert color="danger">Unable to get your profile data</Alert>
-                )}
-                {errors.fields && (
-                  <Alert color="danger">Unable to get form fields</Alert>
-                )}
-                <RequiredInformation />
-                <OptionalInformation />
-                <ChangePasswordModal />
-                <EditOptionalInformationModal />
-                <EditRequiredInformationModal />
-              </>
-            )}
-          </Col>
-          <Col lg="4">
-            {isLoading ? (
-              <LoadingSpinner />
-            ) : (
-              <>
-                {!isEmpty(licenses) && <Licenses />}
-                {!isEmpty(integrations) && <ThirdPartyApps />}
-                <ChangePassword />
-              </>
-            )}
-          </Col>
-        </Row>
-      </Container>
-    </Container>
+      <div className="workbench-content">
+        <Section
+          header="Manage Account"
+          actions={
+            <Link to="/workbench/dashboard" className="wb-link">
+              Back to Dashboard
+            </Link>
+          }
+          content={
+            <>
+              <Row className="user-profile">
+                <Col lg="8" className="user-profile-main">
+                  {isLoading ? (
+                    <LoadingSpinner />
+                  ) : (
+                    <>
+                      {errors.data && (
+                        <Alert color="danger">
+                          Unable to get your profile data
+                        </Alert>
+                      )}
+                      {errors.fields && (
+                        <Alert color="danger">Unable to get form fields</Alert>
+                      )}
+                      <RequiredInformation />
+                      <OptionalInformation />
+                      <ChangePasswordModal />
+                      <EditOptionalInformationModal />
+                      <EditRequiredInformationModal />
+                    </>
+                  )}
+                </Col>
+                <Col lg="4">
+                  {isLoading ? (
+                    <LoadingSpinner />
+                  ) : (
+                    <>
+                      {!isEmpty(licenses) && <Licenses />}
+                      {!isEmpty(integrations) && <ThirdPartyApps />}
+                      <ChangePassword />
+                    </>
+                  )}
+                </Col>
+              </Row>
+            </>
+          }
+          contentClassName="container manage-account-content"
+          contentShouldScroll
+        />
+      </div>
+    </div>
   );
 };
 
