4b7e0a88e65a0b459483e03befb25cfb8ea016d2
Wesley Bomar <wbomar@tacc.utexas.edu>
2020-10-08 23:23:55 -0500

    FP-662: Use `Section` for `Dashboard`

--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Button, Container, Row, Col } from 'reactstrap';
+import { Button, Row, Col } from 'reactstrap';
 import { useDispatch } from 'react-redux';
 import { Link, Route, Switch } from 'react-router-dom';
+
+import { BrowserChecker, Section } from '_common';
 import JobsView from '../Jobs';
 import Tickets, { TicketModal, TicketCreateModal } from '../Tickets';
 import Sysmon from '../SystemMonitor';
-import BrowserChecker from '../_common/BrowserChecker';
 import * as ROUTES from '../../constants/routes';
 import './Dashboard.scss';
 
@@ -13,79 +14,85 @@ function Dashboard() {
   const dispatch = useDispatch();
 
   return (
-    <div className="dashboard-wrapper">
-      <BrowserChecker />
-      <div className="dashboard-header">
-        <h5>Dashboard</h5>
+    <Section
+      messages={<BrowserChecker />}
+      header="Dashboard"
+      headerClassName="dashboard-header"
+      actions={
         <Link to="/accounts/profile" className="wb-link">
-          <h6>Manage Account</h6>
+          Manage Account
         </Link>
-      </div>
-      <Container className="dashboard-items">
-        <Row>
-          <Col lg="7" className="border-right">
-            <div className="jobs-wrapper dash-grid-item">
-              <div className="dashboard-item-header">
-                <h6>My Recent Jobs</h6>
-                <Link to={`${ROUTES.WORKBENCH}${ROUTES.HISTORY}/jobs`}>
-                  <Button color="link">
-                    <h6>View History</h6>
-                  </Button>
-                </Link>
+      }
+      content={
+        <>
+          <Row>
+            <Col lg="7" className="border-right">
+              <div className="jobs-wrapper dash-grid-item">
+                <div className="dashboard-item-header">
+                  <h6>My Recent Jobs</h6>
+                  <Link to={`${ROUTES.WORKBENCH}${ROUTES.HISTORY}/jobs`}>
+                    <Button color="link">
+                      <h6>View History</h6>
+                    </Button>
+                  </Link>
+                </div>
+                <JobsView />
               </div>
-              <JobsView />
-            </div>
-          </Col>
-          <Col lg="5">
-            <div className="sysmon-wrapper dash-grid-item">
-              <div className="dashboard-item-header">
-                <h6>System Status</h6>
+            </Col>
+            <Col lg="5">
+              <div className="sysmon-wrapper dash-grid-item">
+                <div className="dashboard-item-header">
+                  <h6>System Status</h6>
+                </div>
+                <Sysmon />
               </div>
-              <Sysmon />
-            </div>
-          </Col>
-        </Row>
-        <Row>
-          <Col lg="7" className="border-right">
-            <div className="tickets-wrapper dash-grid-item">
-              <div className="dashboard-item-header">
-                <h6>My Tickets</h6>
-                <Link
-                  to={`${ROUTES.WORKBENCH}${ROUTES.DASHBOARD}${ROUTES.TICKETS}/create`}
-                >
-                  <Button color="link">
-                    <h6>Add Ticket</h6>
-                  </Button>
-                </Link>
+            </Col>
+          </Row>
+          <Row>
+            <Col lg="7" className="border-right">
+              <div className="tickets-wrapper dash-grid-item">
+                <div className="dashboard-item-header">
+                  <h6>My Tickets</h6>
+                  <Link
+                    to={`${ROUTES.WORKBENCH}${ROUTES.DASHBOARD}${ROUTES.TICKETS}/create`}
+                  >
+                    <Button color="link">
+                      <h6>Add Ticket</h6>
+                    </Button>
+                  </Link>
+                </div>
+                <Tickets />
               </div>
-              <Tickets />
-            </div>
-          </Col>
-        </Row>
-      </Container>
-      <Switch>
-        <Route
-          exact
-          path={`${ROUTES.WORKBENCH}${ROUTES.DASHBOARD}${ROUTES.TICKETS}/create`}
-          render={() => {
-            dispatch({
-              type: 'TICKETS_CREATE_INIT'
-            });
-            return <TicketCreateModal />;
-          }}
-        />
-        <Route
-          path={`${ROUTES.WORKBENCH}${ROUTES.DASHBOARD}${ROUTES.TICKETS}/:ticketId`}
-          render={({ match: { params } }) => {
-            dispatch({
-              type: 'TICKET_DETAILED_VIEW_OPEN',
-              payload: { ticketId: Number(params.ticketId) }
-            });
-            return <TicketModal />;
-          }}
-        />
-      </Switch>
-    </div>
+            </Col>
+          </Row>
+        </>
+      }
+      contentClassName="dashboard-items container"
+      externals={
+        <Switch>
+          <Route
+            exact
+            path={`${ROUTES.WORKBENCH}${ROUTES.DASHBOARD}${ROUTES.TICKETS}/create`}
+            render={() => {
+              dispatch({
+                type: 'TICKETS_CREATE_INIT'
+              });
+              return <TicketCreateModal />;
+            }}
+          />
+          <Route
+            path={`${ROUTES.WORKBENCH}${ROUTES.DASHBOARD}${ROUTES.TICKETS}/:ticketId`}
+            render={({ match: { params } }) => {
+              dispatch({
+                type: 'TICKET_DETAILED_VIEW_OPEN',
+                payload: { ticketId: Number(params.ticketId) }
+              });
+              return <TicketModal />;
+            }}
+          />
+        </Switch>
+      }
+    />
   );
 }
 
