7ddd807532c1b18df33b41995e79ae8bf99dd438
jchuahtacc <jchuah@tacc.utexas.edu>
2020-10-16 13:20:36 -0500

    Fix unit test template path

--- server/portal/settings/unit_test_settings.py
+++ server/portal/settings/unit_test_settings.py
@@ -141,7 +141,10 @@ MIDDLEWARE = [
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'apps/workbench/templates/portal/apps/workbench')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
