661a35031f4af0d1cf1040fc3e658de2b74d6b33
jchuahtacc <jchuah@tacc.utexas.edu>
2020-10-16 13:26:14 -0500

    Revert "Fix unit test template path"
    This reverts commit 7ddd807532c1b18df33b41995e79ae8bf99dd438.

--- server/portal/settings/unit_test_settings.py
+++ server/portal/settings/unit_test_settings.py
@@ -141,10 +141,7 @@ MIDDLEWARE = [
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-            os.path.join(BASE_DIR, 'templates'),
-            os.path.join(BASE_DIR, 'apps/workbench/templates/portal/apps/workbench')
-        ],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
