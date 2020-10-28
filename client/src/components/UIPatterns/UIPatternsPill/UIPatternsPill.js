64e9b815a1617b75115649e74488eff836c05b5c
jchuahtacc <jchuah@tacc.utexas.edu>
2020-10-28 09:05:52 -0500

    Pill initial stub

---
+++ client/src/components/UIPatterns/UIPatternsPill/UIPatternsPill.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Pill } from '_common';
+
+function UIPatternsPill() {
+  return (
+    <dl>
+      <dt>
+        Pill
+      </dt>
+      <dd>
+        <Pill>Example Pill</Pill>
+      </dd>
+    </dl>
+  );
+}
+
+export default UIPatternsPill;
---
+++ client/src/components/_common/Pill/Pill.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Pill({ children, type }) {
+  return (
+    <div>{children}</div>
+  )
+}
+
+export default Pill;
