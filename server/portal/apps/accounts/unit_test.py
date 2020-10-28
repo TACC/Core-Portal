981f7649a428a27e5ff858e9fc24037070506ed8
Nathan Franklin <nathan.franklin@gmail.com>
2020-10-28 10:23:04 -0500

    Fix test

--- server/portal/apps/accounts/unit_test.py
+++ server/portal/apps/accounts/unit_test.py
@@ -1,3 +1,7 @@
+import pytest
+
+
+@pytest.mark.django_db(transaction=True, reset_sequences=True)
 def test_account_redirect(client, authenticated_user):
     response = client.get('/accounts/profile/')
     assert response.status_code == 302
