166500f43a811652944af94938a23f6ac3c31f4c
Nathan Franklin <nathan.franklin@gmail.com>
2020-10-30 20:40:51 -0500

    Add sidebar to onboarding view

--- client/src/components/Onboarding/Onboarding.js
+++ client/src/components/Onboarding/Onboarding.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { Route, Switch, Redirect, useRouteMatch } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { BrowserChecker } from '_common';
+import Sidebar from '../Sidebar';
 import OnboardingAdmin from './OnboardingAdmin';
 import OnboardingUser from './OnboardingUser';
 
@@ -15,13 +16,16 @@ function Onboarding() {
   }, []);
 
   return (
-    <div>
+    <div className="workbench-wrapper">
       <BrowserChecker />
-      <Switch>
-        <Route path={`${path}/setup/:username?`} component={OnboardingUser} />
-        <Route path={`${path}/admin`} component={OnboardingAdmin} />
-        <Redirect from={`${path}`} to={`${path}/setup`} />
-      </Switch>
+      <Sidebar disabled />
+      <div className="workbench-content">
+        <Switch>
+          <Route path={`${path}/setup/:username?`} component={OnboardingUser} />
+          <Route path={`${path}/admin`} component={OnboardingAdmin} />
+          <Redirect from={`${path}`} to={`${path}/setup`} />
+        </Switch>
+      </div>
     </div>
   );
 }
