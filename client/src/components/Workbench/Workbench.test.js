04f7b98e578bf65c1227b37f98b355eb9c9505bf
Nathan Franklin <nathan.franklin@gmail.com>
2020-11-04 19:17:14 -0600

    Add workbench unit test

---
+++ client/src/components/Workbench/Workbench.test.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import renderComponent from 'utils/testing';
+import configureStore from 'redux-mock-store';
+import { createMemoryHistory } from 'history';
+import Workbench from './Workbench';
+import { initialState as onboarding } from '../../redux/reducers/onboarding.reducers';
+import { initialState as workbench } from '../../redux/reducers/workbench.reducers';
+import { initialState as notifications } from '../../redux/reducers/notifications.reducers';
+import { initialState as jobs } from '../../redux/reducers/jobs.reducers';
+import { initialTicketList as ticketList } from '../../redux/reducers/tickets.reducers';
+import { initialState as authenticatedUser } from '../../redux/reducers/authenticated_user.reducer';
+import { initialState as systemMonitor } from '../../redux/reducers/systemMonitor.reducers';
+import { initialWelcomeMessages as welcomeMessages } from '../../redux/reducers/welcome.reducers';
+
+/* state required to render workbench/dashboard */
+const state = {
+  authenticatedUser,
+  workbench,
+  onboarding,
+  notifications,
+  welcomeMessages,
+  jobs,
+  systemMonitor,
+  ticketList
+};
+
+describe('workbench', () => {
+  const mockStore = configureStore();
+  it('renders workbench for onboarding)', () => {
+    const history = createMemoryHistory();
+    const store = mockStore(state);
+    const { getByText } = renderComponent(<Workbench />, store, history);
+    expect(
+      getByText(
+        /The following steps must be completed before accessing the portal/
+      )
+    ).toBeDefined();
+  });
+  it('renders workbench for user who has already)', () => {
+    const history = createMemoryHistory();
+    const store = mockStore({
+      ...state,
+      workbench: {
+        ...workbench,
+        setupComplete: true
+      }
+    });
+
+    const { getByText } = renderComponent(<Workbench />, store, history);
+    expect(
+      getByText(
+        /This page allows you to monitor your job status, get help with/
+      )
+    ).toBeDefined();
+  });
+});
