974ee2c58a876dc07a58d0336377b44a839c21f0
jchuahtacc <jchuah@tacc.utexas.edu>
2020-11-17 11:23:58 -0600

    App Tray models

--- server/portal/apps/workspace/models_unit_test.py
+++ server/portal/apps/workspace/models_unit_test.py
@@ -1,25 +1,43 @@
 
-from portal.apps.workspace.models import JobSubmission
-from django.contrib.auth import get_user_model
-from django.test import TestCase
+from portal.apps.workspace.models import (
+    JobSubmission,
+    AppTrayCategory,
+    AppTrayEntry
+)
 import pytest
 
 
-@pytest.mark.django_db(transaction=True)
-class TestJobSubmissionModel(TestCase):
-    def setUp(self):
-        super(TestJobSubmissionModel, self).setUp()
-        # Create a test user
-        self.user = get_user_model().objects.create_user("test", "test", "test")
+def test_job_submission_model(django_db_reset_sequences, regular_user):
+    event = JobSubmission.objects.create(
+        user=regular_user,
+        jobId="1234"
+    )
+    event = JobSubmission.objects.all()[0]
+    assert event.user == regular_user
+    assert event.jobId == "1234"
 
-    def tearDown(self):
-        super(TestJobSubmissionModel, self).tearDown()
 
-    def test_model(self):
-        event = JobSubmission.objects.create(
-            user=self.user,
-            jobId="1234"
-        )
-        event = JobSubmission.objects.all()[0]
-        self.assertEqual(event.user, self.user)
-        self.assertEqual(event.jobId, "1234")
+def test_app_tray_models(django_db_reset_sequences):
+    category = AppTrayCategory.objects.create(
+        category="test_category"
+    )
+    category.save()
+    assert str(AppTrayCategory.objects.all()[0]) == "test_category"
+    app = AppTrayEntry.objects.create(
+        category=category,
+        name="matlab",
+        label="Matlab Latest",
+        appType="agave",
+        icon="matlab",
+        version="latest",
+        revision="latest"
+    )
+    app.save()
+    assert str(AppTrayEntry.objects.all()[0]) == "Matlab Latest: matlab-latestulatest"  
+    htmlApp = AppTrayEntry.objects.create(
+        category=category,
+        appType="html",
+        label="Jupyter",
+        htmlId="jupyterhub"
+    )
+    assert str(htmlApp) == "Jupyter: jupyterhub (HTML)" 
