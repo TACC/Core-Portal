3f32e48fda02bcc939151721e99acc4635ced989
Wesley Bomar <wbomar@tacc.utexas.edu>
2020-11-18 15:11:05 -0600

    FP-639: Illustrate Concept (Incomplete Design)

--- client/src/components/UIPatterns/UIPatternsMessage/UIPatternsMessage.js
+++ client/src/components/UIPatterns/UIPatternsMessage/UIPatternsMessage.js
@@ -1,48 +1,153 @@
 import React from 'react';
 import { Message } from '_common';
 
+import './UIPatternsMessage.module.scss';
+
 const EXAMPLE_LINK = (
   <a href="#" className="wb-link">
     Read more.
   </a>
 );
 
+const NOTIFICATION_TEXT = (
+  <em>
+    Can not render in isolation. See{' '}
+    <a
+      href="https://xd.adobe.com/view/db2660cc-1011-4f26-5d31-019ce87c1fe8-ad17/screen/3821fc3e-bda1-40d4-9e50-a514e90aa088/"
+      target="_blank"
+      rel="noreferrer"
+    >
+      Adobe Design.
+    </a>
+  </em>
+);
+
 function UIPatternsMessages() {
   return (
-    <dl>
-      <dt>
-        Information (<code>info</code>)
-      </dt>
-      <dd>
-        <Message type="info">
-          All your information, are belong to us. {EXAMPLE_LINK}
-        </Message>
-      </dd>
-      <dt>
-        Success (<code>success</code>)
-      </dt>
-      <dd>
-        <Message type="success">
-          All your success, are belong to us. {EXAMPLE_LINK}
-        </Message>
-      </dd>
-      <dt>
-        Warning (<code>warn</code>)
-      </dt>
-      <dd>
-        <Message type="warn">
-          All your warning, are come from us. {EXAMPLE_LINK}
-        </Message>
-      </dd>
-      <dt>
-        Error (<code>error</code>)
-      </dt>
-      <dd>
-        <Message type="error">
-          All your error, are belong to you. {EXAMPLE_LINK}
-        </Message>
-      </dd>
-    </dl>
+    <table styleName="container">
+      <thead>
+        <tr>
+          <th scope="row" styleName="secondary">
+            component
+          </th>
+          <th scope="col" colSpan="2">
+            <code>&lt;Message&gt;</code>
+          </th>
+          <th scope="col">
+            <code>&lt;NotifcationToast&gt;</code>
+          </th>
+        </tr>
+        <tr>
+          <th scope="row" styleName="secondary">
+            <code>scope</code>
+          </th>
+          <th scope="col">
+            <code>inline</code>
+          </th>
+          <th scope="col">
+            <code>section</code>
+          </th>
+          <th scope="col">
+            <code>app</code>
+          </th>
+        </tr>
+        <tr>
+          <th scope="col" styleName="secondary">
+            <code>type</code>
+          </th>
+          <td>
+            Example
+            <ul>
+              <li>action result statement in table row</li>
+              <li>(?) message when table can not load data</li>
+            </ul>
+          </td>
+          <td>
+            Example
+            <ul>
+              <li>warning at the top of a form</li>
+              <li>message after successful submission of form</li>
+              <li>(?) message when table can not load data</li>
+            </ul>
+          </td>
+          <td>
+            Example
+            <ul>
+              <li>user action is required</li>
+              <li>important user-initiated action is completed</li>
+              <li>security concern</li>
+              <li>milestone for time-sensitive activity</li>
+            </ul>
+          </td>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row">
+            <code>info</code>
+          </th>
+          <td>
+            <Message type="info" scope="inline">
+              All your information, are belong to us. {EXAMPLE_LINK}
+            </Message>
+          </td>
+          <td>
+            <Message type="info" scope="section">
+              All your information, are belong to us. {EXAMPLE_LINK}
+            </Message>
+          </td>
+          <td rowSpan="2">{NOTIFICATION_TEXT}</td>
+        </tr>
+        <tr>
+          <th scope="row">
+            <code>success</code>
+          </th>
+          <td>
+            <Message type="success" scope="inline">
+              All your success, are belong to us. {EXAMPLE_LINK}
+            </Message>
+          </td>
+          <td>
+            <Message type="success" scope="section">
+              All your success, are belong to us. {EXAMPLE_LINK}
+            </Message>
+          </td>
+          {/* <td /> */}
+        </tr>
+        <tr>
+          <th scope="row">
+            <code>warn</code>
+          </th>
+          <td>
+            <Message type="warn" scope="inline">
+              All your warning, are come from us. {EXAMPLE_LINK}
+            </Message>
+          </td>
+          <td>
+            <Message type="warn" scope="section">
+              All your warning, are come from us. {EXAMPLE_LINK}
+            </Message>
+          </td>
+          <td rowSpan="2" styleName="is-row-end">{NOTIFICATION_TEXT}</td>
+        </tr>
+        <tr>
+          <th scope="row">
+            <code>error</code>
+          </th>
+          <td>
+            <Message type="error" scope="inline">
+              All your error, are belong to you. {EXAMPLE_LINK}
+            </Message>
+          </td>
+          <td>
+            <Message type="error" scope="section">
+              All your error, are belong to you. {EXAMPLE_LINK}
+            </Message>
+          </td>
+          {/* <td /> */}
+        </tr>
+      </tbody>
+    </table>
   );
 }
 
