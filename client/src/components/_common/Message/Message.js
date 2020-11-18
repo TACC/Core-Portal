3bc7c5510f50f1ab655125692fdbf1f9a83747c4
Wesley Bomar <wbomar@tacc.utexas.edu>
2020-11-18 12:02:55 -0600

    FP-639: Support `scope`. Improve `type` logic

--- client/src/components/_common/Message/Message.js
+++ client/src/components/_common/Message/Message.js
@@ -3,35 +3,49 @@ import PropTypes from 'prop-types';
 import Icon from '_common/Icon';
 import './Message.module.scss';
 
-export const TYPE_ICON_MAP = {
+export const TYPE_MAP = {
   info: {
-    name: 'conversation',
+    iconName: 'conversation',
+    className: 'is-info',
     text: 'Notice'
   },
   success: {
-    name: 'approved-reverse',
+    iconName: 'approved-reverse',
+    className: 'is-success',
     text: 'Notice'
   },
   warn: {
-    name: 'alert',
+    iconName: 'alert',
+    className: 'is-warn',
     text: 'Warning'
   },
   error: {
-    name: 'alert',
+    iconName: 'alert',
+    className: 'is-error',
     text: 'Error'
   }
 };
-const TYPES = PropTypes.oneOf(Object.keys(TYPE_ICON_MAP));
+export const TYPES = Object.keys(TYPE_MAP);
+
+export const SCOPE_CLASS_MAP = {
+  inline: 'is-scope-inline',
+  section: 'is-scope-section'
+  // app: 'is-scope-app' // FAQ: Do not use. Use a Notification Toast instead
+};
+export const SCOPES = ['', ...Object.keys(SCOPE_CLASS_MAP)];
+export const DEFAULT_SCOPE = 'inline';
 
 /** Show an event-based message to the user */
-const Message = ({ children, className, hidden, type }) => {
-  const iconName = TYPE_ICON_MAP[type].name;
-  const iconText = TYPE_ICON_MAP[type].text;
-  const containerStyleName = `container is-${type}`;
+const Message = ({ children, className, hidden, scope, type }) => {
+  const { iconName, iconText } = TYPE_MAP[type];
+  const modifierClassNames = [];
+  modifierClassNames.push(TYPE_MAP[type].className);
+  modifierClassNames.push(SCOPE_CLASS_MAP[scope || DEFAULT_SCOPE]);
+  const containerStyleNames = ['container', ...modifierClassNames].join(' ');
 
   return (
     <span
-      styleName={containerStyleName}
+      styleName={containerStyleNames}
       className={className}
       hidden={hidden}
       role="status"
@@ -53,12 +67,15 @@ Message.propTypes = {
   className: PropTypes.string,
   /** Allow external management of visibility */
   hidden: PropTypes.bool,
+  /** How to place the message within the layout */
+  scope: PropTypes.oneOf(SCOPES), // RFE: Require scope. Change all instances.
   /** Message type or severity */
-  type: TYPES.isRequired
+  type: PropTypes.oneOf(TYPES).isRequired
 };
 Message.defaultProps = {
   className: '',
-  hidden: false
+  hidden: false,
+  scope: DEFAULT_SCOPE
 };
 
 export default Message;
--- client/src/components/_common/Message/Message.test.js
+++ client/src/components/_common/Message/Message.test.js
@@ -1,43 +1,62 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import Message, { TYPE_ICON_MAP } from './Message';
+import Message, * as MSG from './Message';
 
 const TEST_CONTENT = '…';
-const TYPES = Object.keys(TYPE_ICON_MAP);
+const TEST_TYPE = 'info';
 
 function testClassnamesByType(type, getByRole, getByTestId) {
   const root = getByRole('status');
   const icon = getByRole('img'); // WARNING: Relies on `Icon`
   const text = getByTestId('text');
-  const modifierClassnamePattern = new RegExp(`is-${type}`);
-  const iconName = TYPE_ICON_MAP[type].name;
+  const iconName = MSG.TYPE_MAP[type].iconName;
+  const modifierClassName = MSG.TYPE_MAP[type].className;
   expect(root.className).toMatch('container');
-  expect(root.className).toMatch(modifierClassnamePattern);
+  expect(root.className).toMatch(new RegExp(modifierClassName));
   expect(icon.className).toMatch(iconName);
   expect(text.className).toMatch('text');
 }
 
 describe('Message', () => {
-  it.each(TYPES)('has correct text for type %s', type => {
-    const { getByTestId } = render(<Message type={type}>{TEST_CONTENT}</Message>);
+  it.each(MSG.TYPES)('has correct text for type %s', type => {
+    const { getByTestId } = render(
+      <Message type={type}>{TEST_CONTENT}</Message>
+    );
     expect(getByTestId('text').textContent).toEqual(TEST_CONTENT);
   });
 
   describe('elements', () => {
-    it.each(TYPES)('include icon when type is %s', type => {
-      const { getByRole } = render(<Message type={type}>{TEST_CONTENT}</Message>);
+    it.each(MSG.TYPES)('include icon when type is %s', type => {
+      const { getByRole } = render(
+        <Message type={type}>{TEST_CONTENT}</Message>
+      );
       expect(getByRole('img')).toBeDefined(); // WARNING: Relies on `Icon`
     });
-    it.each(TYPES)('include text when type is %s', type => {
-      const { getByTestId } = render(<Message type={type}>{TEST_CONTENT}</Message>);
+    it.each(MSG.TYPES)('include text when type is %s', type => {
+      const { getByTestId } = render(
+        <Message type={type}>{TEST_CONTENT}</Message>
+      );
       expect(getByTestId('text')).toBeDefined();
     });
   });
 
   describe('className', () => {
-    it.each(TYPES)('is accurate when type is %s', type => {
-      const { getByRole, getByTestId } = render(<Message type={type}>{TEST_CONTENT}</Message>);
+    it.each(MSG.TYPES)('is accurate when type is %s', type => {
+      const { getByRole, getByTestId } = render(
+        <Message type={type}>{TEST_CONTENT}</Message>
+      );
+
       testClassnamesByType(type, getByRole, getByTestId);
+    });
+    it.each(MSG.SCOPES)('has accurate className when scope is "%s"', scope => {
+      const { getByRole, getByTestId } = render(
+        <Message type={TEST_TYPE} scope={scope}>{TEST_CONTENT}</Message>
+      );
+      const root = getByRole('status');
+      const modifierClassName = MSG.SCOPE_CLASS_MAP[scope || MSG.DEFAULT_SCOPE];
+
+      testClassnamesByType(TEST_TYPE, getByRole, getByTestId);
+      expect(root.className).toMatch(new RegExp(modifierClassName));
     });
   });
 });
