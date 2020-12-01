c36a13784a0e725b9609ee43e5072fcfdd2dc601
Sal Tijerina <r.sal.tijerina@gmail.com>
2020-12-01 16:58:25 -0600

    fix unit test

--- client/src/components/DataFiles/DataFilesStatus/DataFilesStatus.test.js
+++ client/src/components/DataFiles/DataFilesStatus/DataFilesStatus.test.js
@@ -5,9 +5,10 @@ describe('getOperationText', () => {
   it('converts operations to proper UI strings', () => {
     expect(getOperationText('mkdir')).toEqual('added');
     expect(getOperationText('rename')).toEqual('renamed');
-    expect(getOperationText('upload')).toEqual('moved');
+    expect(getOperationText('upload')).toEqual('uploaded');
     expect(getOperationText('copy')).toEqual('copied');
     expect(getOperationText('trash')).toEqual('moved');
+    expect(getOperationText('move')).toEqual('moved');
     expect(getOperationText('random_op')).toEqual('Unknown');
   });
 });
