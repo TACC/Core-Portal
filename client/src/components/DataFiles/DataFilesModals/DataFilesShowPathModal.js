7e93a9b03ebba333c2960a115c38a2d1b8a0e7fd
jchuahtacc <jchuah@tacc.utexas.edu>
2020-12-03 10:56:34 -0600

    Code cleanup

--- client/src/components/DataFiles/DataFilesModals/DataFilesShowPathModal.js
+++ client/src/components/DataFiles/DataFilesModals/DataFilesShowPathModal.js
@@ -1,52 +1,35 @@
-import React, { useCallback, useMemo, useState } from 'react';
+import React from 'react';
 import { useSelector, useDispatch, shallowEqual } from 'react-redux';
 import { Modal, ModalHeader, ModalBody } from 'reactstrap';
-import { useHistory, useLocation } from 'react-router-dom';
-import { v4 as uuidv4 } from 'uuid';
 
 import DataFilesBreadcrumbs from '../DataFilesBreadcrumbs/DataFilesBreadcrumbs';
 
 const DataFilesShowPathModal = React.memo(() => {
-  const history = useHistory();
-  const location = useLocation();
-
   const dispatch = useDispatch();
   const params = useSelector(
     state => state.files.params.FilesListing,
     shallowEqual
   );
-  const modalParams = useSelector(
-    state => state.files.params.modal,
-    shallowEqual
-  );
-
-  const files = useSelector(state => state.files.listing.modal, shallowEqual);
-  const isOpen = useSelector(state => state.files.modals.showpath);
-  const status = useSelector(
-    state => state.files.operationStatus.copy,
-    shallowEqual
-  );
-  const [disabled, setDisabled] = useState(false);
-
-  const selectedFiles = useSelector(
-    state =>
-      state.files.selected.FilesListing.map(i => ({
-        ...state.files.listing.FilesListing[i],
-        id: uuidv4()
-      })),
-    () => true
+  const selectedFile = useSelector(
+    state => state.files.listing.FilesListing[0]
   );
 
   const definition = useSelector(state => {
-    if (!selectedFiles[0]) {
+    if (!selectedFile) {
       return null;
     }
-    const matching = state.systems.systemList.find(sys => sys.system === selectedFiles[0].system);
+    const matching = state.systems.systemList.find(
+      sys => sys.system === selectedFile.system
+    );
     if (!matching) {
       return null;
     }
     return matching.definition;
   });
+
+  const isOpen = useSelector(
+    state => state.files.modals.showpath && definition
+  );
 
   const toggle = () =>
     dispatch({
@@ -63,36 +46,37 @@ const DataFilesShowPathModal = React.memo(() => {
 
   const onClosed = () => {
     dispatch({ type: 'DATA_FILES_MODAL_CLOSE' });
-    setDisabled(false);
   };
 
   return definition ? (
     <Modal
-      isOpen={isOpen && definition}
+      isOpen={isOpen}
       onOpened={onOpened}
       onClosed={onClosed}
       toggle={toggle}
-      size="xl"
       className="dataFilesModal"
     >
-      <ModalHeader>Show Path</ModalHeader>
-      <ModalBody style={{ height: '70vh' }}>
+      <ModalHeader toggle={toggle}>Show Path</ModalHeader>
+      <ModalBody>
         <div className="row h-100">
           <div className="col-md-6 d-flex flex-column">
             {/* Table of selected files */}
-            <div className="dataFilesModalColHeader"></div>
+            <div className="dataFilesModalColHeader" />
             <DataFilesBreadcrumbs
               api={params.api}
               scheme={params.scheme}
               system={params.system}
-              path={params.path + selectedFiles[0].path || '/'}
+              path={params.path + selectedFile.path || '/'}
               section=""
             />
             <dl>
               <dt>Storage Host</dt>
               <dd>{definition.storage.host}</dd>
               <dt>Storage Path</dt>
-              <dd>{definition.storage.rootDir}{selectedFiles[0].path}</dd>
+              <dd>
+                {definition.storage.rootDir}
+                {selectedFile.path}
+              </dd>
             </dl>
           </div>
         </div>
