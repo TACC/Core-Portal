a356ae10e2fc69c56621ec944990d3bebf8a2566
jchuahtacc <jchuah@tacc.utexas.edu>
2020-12-10 15:26:45 -0600

    Manage project modal unit test

---
+++ client/src/components/DataFiles/DataFilesModals/tests/DataFilesManageProjectModal.test.js
@@ -0,0 +1,59 @@
+import React from "react";
+import DataFilesManageProjectModal from "../DataFilesManageProjectModal";
+import configureStore from "redux-mock-store";
+import renderComponent from 'utils/testing';
+import {
+  projectsListingFixture,
+  projectMetadataFixture
+} from '../../../../redux/sagas/fixtures/projects.fixture';
+const mockStore = configureStore();
+
+const initialMockState = {
+  files: {
+    modals: {
+      manageproject: {}
+    }
+  },
+  users: {
+    search: {
+      users: []
+    }
+  },
+  projects: {
+    listing: {
+      project: projectsListingFixture,
+      loading: false,
+      error: null
+    },
+    operation: {
+      name: '',
+      loading: false,
+      error: null,
+      result: null
+    },
+    metadata: projectMetadataFixture
+  },
+  authenticatedUser: {
+    user: {
+      username: "username",
+      first_name: "User",
+      last_name: "Name",
+      email: "user@name.com"
+    }
+  }
+};
+
+
+describe("DataFilesManageProjectModal", () => {
+  it("renders the manage project modal", () => {
+    const store = mockStore(initialMockState);
+    const { getAllByText, debug } = renderComponent(
+      <DataFilesManageProjectModal />,
+      store
+    )
+    debug();
+
+    // Check that the authenticated user sees the Change Ownership option
+    expect(getAllByText(/Change Ownership/)).toBeDefined();
+  });
+});
