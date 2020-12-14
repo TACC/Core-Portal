1ee3f48370e538db9c8c6cc9392569cdcf0ce55f
jchuahtacc <jchuah@tacc.utexas.edu>
2020-12-14 11:00:29 -0600

    Error messages for project creation failure

--- client/src/components/DataFiles/DataFilesModals/DataFilesAddProjectModal.js
+++ client/src/components/DataFiles/DataFilesModals/DataFilesAddProjectModal.js
@@ -3,7 +3,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import * as Yup from 'yup';
 import { Formik, Form } from 'formik';
 import FormField from '_common/Form/FormField';
-import { LoadingSpinner } from '_common';
+import { LoadingSpinner, Message } from '_common';
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 import { useHistory, useRouteMatch } from 'react-router-dom';
 import DataFilesProjectMembers from '../DataFilesProjectMembers/DataFilesProjectMembers';
@@ -20,6 +20,14 @@ const DataFilesAddProjectModal = () => {
       state.projects.operation.name === 'create' &&
       state.projects.operation.loading
     );
+  });
+
+  const error = useSelector(state => {
+    return (
+      state.projects.operation &&
+      state.projects.operation.name === 'create' &&
+      state.projects.operation.error
+    )
   });
 
   const toggle = () => {
@@ -97,6 +105,11 @@ const DataFilesAddProjectModal = () => {
                 onAdd={onAdd}
                 onRemove={onRemove}
               />
+              {
+                error 
+                  ? <Message type="warning">Your shared workspace could not be created</Message>
+                  : null
+              }
             </ModalBody>
             <ModalFooter>
               <Button
