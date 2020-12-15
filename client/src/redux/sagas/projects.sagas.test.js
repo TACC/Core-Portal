cbeb6b3cd9f0b950bb5e69f020f92fef27ffe0a2
Nathan Franklin <nfranklin@tacc.utexas.edu>
2020-12-15 16:11:30 -0600

    Task/FP-793 edit projects description modal3 (#280)
    * Add edit project description/title modal
    
    * Add test for modal
    
    * Add additional tests
    
    * Fix state
    
    * Configure redux for editing description
    
    * Fix tests
    
    * Fix linting errors
    
    * Add redux test
    
    * Fix test imports
    
    * Fix linting in project router test
    
    * Remove duplicate addproject
    
    * Fix linting error

--- client/src/redux/sagas/projects.sagas.test.js
+++ client/src/redux/sagas/projects.sagas.test.js
@@ -1,67 +1,61 @@
-import { expectSaga } from "redux-saga-test-plan";
-import * as matchers from "redux-saga-test-plan/matchers";
+import { expectSaga } from 'redux-saga-test-plan';
+import * as matchers from 'redux-saga-test-plan/matchers';
 import {
   getProjectsListing,
   fetchProjectsListing,
   getMetadata,
   fetchMetadata,
   setMember,
-  setMemberUtil
-} from "./projects.sagas";
+  setMemberUtil,
+  setTitleDescription,
+  setTitleDescriptionUtil
+} from './projects.sagas';
 import projectsReducer, { initialState } from '../reducers/projects.reducers';
-import { 
+import {
   projectsFixture,
   projectMetadataFixture,
   projectMetadataResponse,
   projectsListingFixture
 } from './fixtures/projects.fixture';
 
-jest.mock("cross-fetch");
+jest.mock('cross-fetch');
 
-describe("Projects Sagas", () => {
-  it("should get a listing of projects", () => {
+describe('Projects Sagas', () => {
+  it('should get a listing of projects', () => {
     return expectSaga(getProjectsListing)
       .withReducer(projectsReducer)
       .provide([
-        [
-          matchers.call.fn(fetchProjectsListing),
-          projectsListingFixture
-        ],
+        [matchers.call.fn(fetchProjectsListing), projectsListingFixture]
       ])
-      .put({ type: "PROJECTS_GET_LISTING_STARTED" })
+      .put({ type: 'PROJECTS_GET_LISTING_STARTED' })
       .call(fetchProjectsListing)
       .put({
-        type: "PROJECTS_GET_LISTING_SUCCESS",
-        payload: projectsListingFixture,
+        type: 'PROJECTS_GET_LISTING_SUCCESS',
+        payload: projectsListingFixture
       })
       .hasFinalState({
         ...initialState,
-        ...projectsFixture,
+        ...projectsFixture
       })
       .run();
   });
-  it("should get project metadata", () => {
+  it('should get project metadata', () => {
     return expectSaga(getMetadata, { payload: 'system' })
       .withReducer(projectsReducer)
-      .provide([
-        [
-          matchers.call.fn(fetchMetadata),
-          projectMetadataResponse
-        ]
-      ])
-      .put({ type: "PROJECTS_GET_METADATA_STARTED" })
-      .call(fetchMetadata, "system")
+      .provide([[matchers.call.fn(fetchMetadata), projectMetadataResponse]])
+      .put({ type: 'PROJECTS_GET_METADATA_STARTED' })
+      .call(fetchMetadata, 'system')
       .put({
-        type: "PROJECTS_GET_METADATA_SUCCESS",
+        type: 'PROJECTS_GET_METADATA_SUCCESS',
         payload: projectMetadataResponse
       })
       .hasFinalState({
         ...initialState,
-        metadata: projectMetadataFixture 
+        metadata: projectMetadataFixture
       })
       .run();
   });
-  it("should manage membership on a project", () => {
+  it('should manage membership on a project', () => {
     const action = {
       type: 'PROJECTS_SET_MEMBER',
       payload: {
@@ -71,19 +65,17 @@ describe("Projects Sagas", () => {
           username: 'username'
         }
       }
-    }
+    };
     return expectSaga(setMember, action)
       .withReducer(projectsReducer)
-      .provide([
-        [
-          matchers.call.fn(setMemberUtil),
-          projectMetadataResponse  
-        ]
-      ])
+      .provide([[matchers.call.fn(setMemberUtil), projectMetadataResponse]])
       .put({ type: 'PROJECTS_SET_MEMBER_STARTED' })
-      .call(setMemberUtil, "PRJ-123", { action: 'add_member', username: 'username' })
+      .call(setMemberUtil, 'PRJ-123', {
+        action: 'add_member',
+        username: 'username'
+      })
       .put({
-        type: "PROJECTS_SET_MEMBER_SUCCESS",
+        type: 'PROJECTS_SET_MEMBER_SUCCESS',
         payload: projectMetadataResponse
       })
       .hasFinalState({
@@ -94,6 +86,52 @@ describe("Projects Sagas", () => {
           loading: false,
           error: null,
           result: projectMetadataResponse
+        }
+      })
+      .run();
+  });
+  it('should allow change to project title description', () => {
+    const action = {
+      type: 'PROJECTS_SET_TITLE_DESCRIPTION',
+      payload: {
+        projectId: 'PRJ-123',
+        data: {
+          title: 'new title',
+          description: 'new description'
+        }
+      }
+    };
+    const updatedProjectMetadataResponse = {
+      ...projectMetadataResponse,
+      title: action.payload.data.title,
+      description: action.payload.data.description
+    };
+    return expectSaga(setTitleDescription, action)
+      .withReducer(projectsReducer)
+      .provide([
+        [
+          matchers.call.fn(setTitleDescriptionUtil),
+          updatedProjectMetadataResponse
+        ]
+      ])
+      .put({ type: 'PROJECTS_SET_TITLE_DESCRIPTION_STARTED' })
+      .call(setTitleDescriptionUtil, 'PRJ-123', action.payload.data)
+      .put({
+        type: 'PROJECTS_SET_TITLE_DESCRIPTION_SUCCESS',
+        payload: updatedProjectMetadataResponse
+      })
+      .hasFinalState({
+        ...initialState,
+        metadata: {
+          ...projectMetadataFixture,
+          title: 'new title',
+          description: 'new description'
+        },
+        operation: {
+          name: 'titleDescription',
+          loading: false,
+          error: null,
+          result: updatedProjectMetadataResponse
         }
       })
       .run();
