a40fa67e89bdf9f2dfba27d386fdaa45c8f5b310
Sal Tijerina <sal@sal-mbp20.local>
2020-12-15 17:58:27 -0600

    add missing settings

--- server/conf/cms/secrets.sample.py
+++ server/conf/cms/secrets.sample.py
@@ -248,3 +248,77 @@ _PORTAL_LOGO =  [
 ]
 
 _LOGO = _PORTAL_LOGO                # Default Portal Logo.
+
+########################
+# PORTAL
+########################
+
+_PORTAL = True     # True for any CMS that is part of a Portal.
+
+"""
+Portal Links
+
+Usage:
+
+- For each link used in the templating, add new links values (see example below).
+- New links must be added to the _PORTAL_AUTH_LINKS and _PORTAL_UNAUTH_LINKS lists.
+- The order of the _PORTAL_[…]_LINKS lists determine the rendering order of the elements.
+
+Values to populate:
+
+_NAMED_LINK = {                    # The name of the link object.
+    "name": "…",                       # The name of the link (to distinguish it, as if for ID).
+    "url": "…",                        # The URL path to which the link should navigate the user.
+    "text": "…",                       # The text of the link.
+    "icon": "…",                       # The icon of the link.
+}
+
+Links Configuration Example.
+
+_ANY_AUTH_LINK = {
+    "name": "section-1",
+    "url": "/some/section/",
+    "text": "Visit Section",
+    "icon": "some-section",
+}
+
+_ANY_UNAUTH_LINK = {
+    "name": "action-1",
+    "url": "/some-action/",
+    "text": "Do Action",
+    "icon": "some-action",
+}
+
+"""
+
+########################
+# LINKS (for Portal).
+
+_DASH_AUTH_LINK = {
+    "name": "dash",
+    "url": "/workbench/dashboard/",
+    "text": "My Dashboard",
+    "icon": "desktop",
+}
+_PROFILE_AUTH_LINK = {
+    "name": "profile",
+    "url": "/accounts/profile/",
+    "text": "My Account",
+    "icon": "user-circle",
+}
+_LOGOUT_AUTH_LINK = {
+    "name": "logout",
+    "url": "/accounts/logout/",
+    "text": "Log Out",
+    "icon": "sign-out-alt",
+}
+
+_LOGIN_UNAUTH_LINK = {
+    "name": "login",
+    "url": "/login/",
+    "text": "Log In",
+    "icon": "sign-in-alt",
+}
+
+_PORTAL_AUTH_LINKS = [_DASH_AUTH_LINK, _PROFILE_AUTH_LINK, _LOGOUT_AUTH_LINK]       # Default TACC Portal.
+_PORTAL_UNAUTH_LINKS = [_LOGIN_UNAUTH_LINK]                                         # Default TACC Portal.
