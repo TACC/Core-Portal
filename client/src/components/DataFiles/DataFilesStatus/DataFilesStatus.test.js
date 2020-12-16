2b5b906f7e5b8787a59c207327488a14a1481a6b
Sal Tijerina <r.sal.tijerina@gmail.com>
2020-12-16 16:20:17 -0600

    task/FP-695: Data Files Events and Notification Toasts (#258)
    * add data files event toasts
    
    * fix post toasts; improve messages
    
    * finalize toasts
    
    * add error color
    
    * fix unit tests
    
    * refactor Toast.js
    
    * add tests
    
    * add unit tests
    
    * add tests
    
    * fix unit test
    
    * add notif generation unit test
    
    * don't show notification counter for data_files events
    
    * data files events do not cause an unread notif to appear
    
    Co-authored-by: Sal Tijerina <sal@sal-mbp20.lan>
    Co-authored-by: Sal Tijerina <sal@sal-mbp20.local>

---
+++ client/src/components/DataFiles/DataFilesStatus/DataFilesStatus.test.js
@@ -0,0 +1,14 @@
+import { getOperationText } from './DataFilesStatus';
+
+
+describe('getOperationText', () => {
+  it('converts operations to proper UI strings', () => {
+    expect(getOperationText('mkdir')).toEqual('added');
+    expect(getOperationText('rename')).toEqual('renamed');
+    expect(getOperationText('upload')).toEqual('uploaded');
+    expect(getOperationText('copy')).toEqual('copied');
+    expect(getOperationText('trash')).toEqual('moved');
+    expect(getOperationText('move')).toEqual('moved');
+    expect(getOperationText('random_op')).toEqual('Unknown');
+  });
+});
