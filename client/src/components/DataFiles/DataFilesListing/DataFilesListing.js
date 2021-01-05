eecd75bf42c8acca6bf10566df984dcc6ce567fb
jchuahtacc <jchuah@tacc.utexas.edu>
2021-01-05 11:39:30 -0600

    View Path is only available for TAPIS api

--- client/src/components/DataFiles/DataFilesListing/DataFilesListing.js
+++ client/src/components/DataFiles/DataFilesListing/DataFilesListing.js
@@ -26,7 +26,7 @@ const DataFilesListing = ({ api, scheme, system, path }) => {
   );
 
   const showViewPath = useSelector(
-    state => state.workbench && state.workbench.config.viewPath
+    state => api === 'tapis' && state.workbench && state.workbench.config.viewPath
   );
 
   const scrollBottomCallback = useCallback(() => {
