a04f7a5afbb4c273b7742fe468aa1f5c58335bea
Sal Tijerina <r.sal.tijerina@gmail.com>
2021-02-05 12:53:14 -0600

    bug/FP-896: Fix add definition to list (#342)
    * fix add definition to list
    
    * add system def list unit test
    
    Co-authored-by: Owais Jamil <47395902+owaisj@users.noreply.github.com>

---
+++ client/src/redux/reducers/tests/datafiles.reducers.test.js
@@ -0,0 +1,26 @@
+import {systems as datafilesReducer,  initialSystemState } from "../datafiles.reducers";
+import systemDefinitionFixture from '../../sagas/fixtures/systemDefinition.fixture';
+
+describe("Datafiles Reducer", () => {
+  test("Load initial state", () => {
+    expect(datafilesReducer(initialSystemState, { type: undefined })).toEqual(
+      initialSystemState
+    );
+  });
+  test("Add system definition to state", () => {
+    const addSystemDefinitionAction = {
+      type: "FETCH_SYSTEM_DEFINITION_SUCCESS",
+      payload: systemDefinitionFixture
+    };
+    expect(datafilesReducer(initialSystemState, addSystemDefinitionAction)).toEqual({
+      ...initialSystemState,
+        definitions: {
+          ...initialSystemState.definitions,
+          list: [systemDefinitionFixture],
+          error: false,
+          errorMessage: null,
+          loading: false
+        }
+    });
+  });
+});
