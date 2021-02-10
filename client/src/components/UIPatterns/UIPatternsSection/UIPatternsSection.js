27e1579a3870adec5c9bce2a0eab076460ab02cd
Wesley Bomar <wbomar@tacc.utexas.edu>
2021-02-10 13:04:17 -0600

    FP-385: WIP: Show SectionTable Support

--- client/src/components/UIPatterns/UIPatternsSection/UIPatternsSection.js
+++ client/src/components/UIPatterns/UIPatternsSection/UIPatternsSection.js
@@ -1,9 +1,17 @@
 import React from 'react';
-import { Section, DescriptionList } from '_common';
+import PropTypes from 'prop-types';
+import {
+  Section,
+  SectionTable,
+  DescriptionList,
+  InfiniteScrollTable
+} from '_common';
 import { UncontrolledAlert } from 'reactstrap';
 import { useDispatch } from 'react-redux';
 import { Link, Route, Switch } from 'react-router-dom';
+
 import * as ROUTES from '../../../constants/routes';
+
 import { TicketCreateModal } from '../../Tickets';
 
 import './UIPatternsSection.module.css';
@@ -12,6 +20,13 @@ const modalPath = `${ROUTES.WORKBENCH}${ROUTES.UI}/modal`;
 
 function UIPatternsSection() {
   const dispatch = useDispatch();
+
+  function performAction() {
+    // eslint-disable-next-line no-alert
+    window.alert(
+      'Sample action (no actual action has occurred beyond this alert).'
+    );
+  }
 
   return (
     <dl>
@@ -62,6 +77,49 @@ function UIPatternsSection() {
         />
       </dd>
       <dt>
+        With a Complex Table
+        <DescriptionList
+          className="small"
+          density="compact"
+          direction="horizontal"
+          data={{
+            header: 'Section with <SectionTable> and <InfiniteScrollTable>',
+            content: '(paragraph, table, paragraph)',
+            headerActions: '(a button to trigger sample action)'
+          }}
+        />
+      </dt>
+      <dd>
+        <Section
+          contentStyleName="has-infinite-scroll-table"
+          header={
+            <>
+              Section with <code>&lt;SectionTable&gt;</code> and
+              &nbsp;<code>&lt;InfiniteScrollTable&gt;</code>
+            </>
+          }
+          content={
+            <SectionTable styleName="table" manualContent>
+              <p>
+                A UI element can appear <em>above</em> the table.
+              </p>
+              <div className="o-flex-item-table-wrap">
+                <UIPatternsSectionTableInfinite />
+              </div>
+              <p>
+                A UI element can appear <em>below</em> the table.
+              </p>
+            </SectionTable>
+          }
+          headerActions={
+            <button type="button" onClick={performAction}>
+              Click Me
+            </button>
+          }
+          contentLayoutName="oneColumn"
+        />
+      </dd>
+      <dt>
         All Properties
         <DescriptionList
           className="small"
@@ -70,7 +128,7 @@ function UIPatternsSection() {
           data={{
             header: 'Header',
             content: '(instruction list and a paragraph)',
-            contentLayoutName: 'oneColumn (default value)',
+            contentLayoutName: 'oneColumn',
             contentShouldScroll: (
               <>
                 <code>true</code> (must resize to scroll)
@@ -105,6 +163,9 @@ function UIPatternsSection() {
                   properly add a modal.
                 </li>
               </ol>
+              <SectionTable styleName="table" manualContent>
+                <UIPatternsSectionTablePlain />
+              </SectionTable>
               <p>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
                 eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
@@ -141,3 +202,69 @@ function UIPatternsSection() {
 }
 
 export default UIPatternsSection;
+
+function UIPatternsSectionTableInfinite({ hasInfiniteScroll }) {
+  const tableData = [
+    {
+      col1: 'Hello',
+      col2: 'World'
+    },
+    {
+      col1: 'react-table',
+      col2: 'rocks'
+    },
+    {
+      col1: 'whatever',
+      col2: 'you want'
+    }
+  ];
+
+  const tableColumns = [
+    {
+      Header: 'Column 1',
+      accessor: 'col1' // accessor is the "key" in the data
+    },
+    {
+      Header: 'Column 2',
+      accessor: 'col2'
+    }
+  ];
+
+  return (
+    <InfiniteScrollTable tableColumns={tableColumns} tableData={tableData} />
+  );
+}
+UIPatternsSectionTableInfinite.propTypes = {
+  /** Whether to use an <InfiniteScrollTable> */
+  hasInfiniteScroll: PropTypes.bool
+};
+UIPatternsSectionTableInfinite.defaultProps = {
+  hasInfiniteScroll: false
+};
+
+function UIPatternsSectionTablePlain() {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Column 1</th>
+          <th>Column 2</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Hello</td>
+          <td>World</td>
+        </tr>
+        <tr>
+          <td>react-table</td>
+          <td>rocks</td>
+        </tr>
+        <tr>
+          <td>whatever</td>
+          <td>you want</td>
+        </tr>
+      </tbody>
+    </table>
+  );
+}
