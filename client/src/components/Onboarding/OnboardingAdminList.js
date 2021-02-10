aa9532ac72f0788485c6158aa9ffb785662ac103
jchuahtacc <jchuah@tacc.utexas.edu>
2021-02-10 14:58:36 -0600

    Switched to custom table DOM

--- client/src/components/Onboarding/OnboardingAdminList.js
+++ client/src/components/Onboarding/OnboardingAdminList.js
@@ -1,46 +1,63 @@
 import React from 'react';
 import { InfiniteScrollTable } from '_common';
+import { Button } from 'reactstrap'
 import { v4 as uuidv4 } from 'uuid';
+import OnboardingActions from './OnboardingActions';
 import './OnboardingAdminList.scss';
 import './OnboardingAdminList.module.scss';
 
-const OnboardingAdminList = ({ users }) => {
-  const columns = [
-    {
-      Header: (
-        <>
-          User
-          <button>Sort</button>
-        </>
-      ),
-      accessor: 'lastName',
-      Cell: el => (
-        <span>
-          {`${el.row.original.firstName} ${el.row.original.lastName}`}
-        </span>
-      )
-    },
-    {
-      Header: 'Step',
-      accessor: 'steps',
-      Cell: el => (
-        <table styleName="cell-table">
-          {el.value.map(step => (
-            <tr>
-              <td>{step.displayName}</td>
-            </tr>
-          ))}
-        </table>
-      )
-    }
-  ];
-
+const OnboardingAdminListUser = ({ user }) => {
   return (
-    <InfiniteScrollTable
-      tableData={users}
-      tableColumns={columns}
-      className="onboarding-admin-list"
-    />
+    <tr>
+      <td>{`${user.firstName} ${user.lastName}`}</td>
+      <td styleName="step-list">
+        {user.steps.map(
+          step => (
+            <div key={step.step}>
+              {step.displayName}
+            </div>
+          )
+        )}
+      </td>
+      <td styleName="step-list">
+        {user.steps.map(
+          step => (
+            <div>
+              <OnboardingActions step={step} key={step.step}/>
+            </div>
+          )
+        )}
+      </td>
+      <td styleName="step-list">
+        {user.steps.map(
+          step => (
+            <div>
+              <Button color="link"><h6>View Log</h6></Button>
+            </div>            
+          )
+        )}
+      </td>
+    </tr>
+  )
+}
+
+const OnboardingAdminList = ({ users }) => {
+  return (
+    <table styleName="root">
+      <thead>
+        <tr>
+          <td>User</td>
+          <td>Step</td>
+          <td>Administrative Actions</td>
+          <td>Log</td>
+        </tr>
+      </thead>
+      <tbody>
+        {users.map(user => (
+          <OnboardingAdminListUser user={user} key={user.username}/>
+        ))}
+      </tbody>
+    </table>
   );
 };
 
