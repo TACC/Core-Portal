e75645d18cddfd73ae6dc2280bb520d66513564f
jchuahtacc <jchuah@tacc.utexas.edu>
2021-02-11 13:19:47 -0600

    Component stubs

---
+++ client/src/components/UIPatterns/UIPatternsPaginator/UIPatternsPaginator.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Paginator } from '_common';
+
+
+function UIPatternsPaginator() {
+  return (
+    <Paginator />
+  );
+}
+
+export default UIPatternsPaginator;
---
+++ client/src/components/_common/Paginator/Paginator.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import PropTypes from 'prop-types';
+import './Paginator.module.scss';
+
+const Paginator = ({ pages, current, callback }) => {
+  return (
+    <>
+      Paginator
+    </>
+  );
+};
+
+export default Paginator;
