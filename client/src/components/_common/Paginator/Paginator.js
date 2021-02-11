d13bc2b4b550a0774ea00bd015f8c5603c7c002e
jchuahtacc <jchuah@tacc.utexas.edu>
2021-02-11 16:05:37 -0600

    Paginator special cases

--- client/src/components/_common/Paginator/Paginator.js
+++ client/src/components/_common/Paginator/Paginator.js
@@ -11,69 +11,89 @@ import './Paginator.module.scss';
 const PaginatorPage = ({ number, callback, current }) => {
   return (
     <Button
-      styleName={`page ${current ? 'current' : ''}`}
+      styleName={`page ${number === current ? 'current' : ''}`}
       onClick={() => callback(number)}
     >
       {number}
     </Button>
-  ) 
-}
+  );
+};
 
 PaginatorPage.propTypes = {
   number: PropTypes.number.isRequired,
   callback: PropTypes.func.isRequired,
-  current: PropTypes.bool
-}
-
-PaginatorPage.defaultProps = {
-  current: false
-}
+  current: PropTypes.number.isRequired
+};
 
 const Paginator = ({ pages, current, callback }) => {
-  const start = Math.max(current - 2, 2);
-  const end = Math.min(current + 2, pages - 1);
-  const middle = Array(end - start + 1).fill().map((_, index) => start + index)
-  console.log(middle);
+  let start, end;
+  if (pages === 1) {
+    end = 0;
+    start = 1;
+  } else if (pages > 2 && pages < 7) {
+    start = 2;
+    end = pages - 1;
+  } else if (pages > 7 && current <= 4) {
+    start = 2;
+    end = 6;
+  } else if (pages > 7 && current > pages - 5) {
+    start = pages - 5;
+    end = pages - 1;
+  } else {
+    start = current - 2;
+    end = current + 2;
+  }
+  const middle = end - start + 1;
+  const middlePages =
+    middle > 0
+      ? Array(middle)
+          .fill()
+          .map((_, index) => start + index)
+      : [];
   return (
     <div>
-      <Button color="link" styleName="endcap">
+      <Button
+        color="link"
+        styleName="endcap"
+        onClick={() => callback(current - 1)} // eslint-disable-line
+        disabled={current === 1}
+      >
         <FontAwesomeIcon icon={faChevronLeft} />
-        <span>
-          Previous
-        </span>
+        <span>Previous</span>
       </Button>
-      <PaginatorPage number={1} callback={callback} />
-      {
-        middle[0] > 2 && (
-          <span>...</span>
-        )
-      }
-      {
-        middle.map(
-          number => {
-          return <PaginatorPage 
-            number={number} 
+      <PaginatorPage number={1} callback={callback} current={current} />
+      {middlePages[0] > 2 && <span>...</span>}
+      {middlePages.map(number => {
+        return (
+          <PaginatorPage
+            number={number}
             key={number}
-            current={number === current}
+            current={current}
             callback={callback}
           />
-          }
-        )
-      }
-      {
-        middle[middle.length - 1] < pages - 1 && (
-          <span>...</span>
-        )
-      }
-      <PaginatorPage number={pages} callback={callback}/>
-      <Button color="link" styleName="endcap">
-        <span>
-          Next
-        </span>
+        );
+      })}
+      {middlePages[middlePages.length - 1] < pages - 1 && <span>...</span>}
+      {pages > 1 && (
+        <PaginatorPage number={pages} callback={callback} current={current} />
+      )}
+      <Button
+        color="link"
+        styleName="endcap"
+        onClick={() => callback(current + 1)} // eslint-disable-line
+        disabled={current === pages}
+      >
+        <span>Next</span>
         <FontAwesomeIcon icon={faChevronRight} />
       </Button>
     </div>
   );
+};
+
+Paginator.propTypes = {
+  pages: PropTypes.number.isRequired,
+  current: PropTypes.number.isRequired,
+  callback: PropTypes.func.isRequired
 };
 
 export default Paginator;
