e959678501358ad0435996c43c1cf7a28828494c
jchuahtacc <jchuah@tacc.utexas.edu>
2021-02-24 16:27:05 -0600

    Fix test

--- client/src/components/_common/Paginator/Paginator.test.js
+++ client/src/components/_common/Paginator/Paginator.test.js
@@ -12,6 +12,6 @@ describe('Paginator', () => {
     expect(getAllByText('12').length).toEqual(1);
     expect(getAllByText('13').length).toEqual(1);
     expect(getAllByText('...').length).toEqual(1);
-    expect(getAllByText('20').length).toEqual(1);
+    expect(getAllByText('20').length).toEqual(2);
   });
 });
