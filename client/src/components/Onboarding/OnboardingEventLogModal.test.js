001a2817500b28ab135ff95a12354f2f5a6eb84a
jchuahtacc <jchuah@tacc.utexas.edu>
2021-02-24 11:49:06 -0600

    Unit tests

--- client/src/components/Onboarding/OnboardingEventLogModal.test.js
+++ client/src/components/Onboarding/OnboardingEventLogModal.test.js
@@ -1,3 +1,20 @@
-describe("Onboarding Event Log Modal", () => {
-  it.todo("Should display the event log")
-})
+import React from 'react';
+import { render } from '@testing-library/react';
+import OnboardingEventLogModal from './OnboardingEventLogModal';
+import { onboardingUserFixture } from '../../redux/sagas/fixtures/onboarding.fixture';
+
+describe('Onboarding Event Log Modal', () => {
+  it('renders onboarding steps', () => {
+    const params = {
+      user: onboardingUserFixture,
+      step: onboardingUserFixture.steps[0]
+    };
+    const { getByText } = render(
+      <OnboardingEventLogModal toggle={() => {}} params={params} />
+    );
+
+    expect(getByText(/First Last/)).toBeDefined();
+    expect(getByText(/Mock User Wait Step/)).toBeDefined();
+    expect(getByText(/11:43/)).toBeDefined();
+  });
+});
