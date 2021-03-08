ff711799259919bbf6601c2fd615d49c3e7cf6de
Strmiska <keith.strmiska@gmail.com>
2021-03-08 15:07:15 -0600

    check for system health only when app id is requested

--- server/portal/apps/workspace/api/views.py
+++ server/portal/apps/workspace/api/views.py
@@ -91,6 +91,16 @@ class AppsView(BaseApiView):
         if app_id:
             METRICS.debug("user:{} is requesting app id:{}".format(request.user.username, app_id))
             data = _get_app(app_id, request.user)
+            
+            # check if default system needs keys pushed
+            default_sys = UserSystemsManager(
+                request.user,
+                settings.PORTAL_DATA_DEPOT_LOCAL_STORAGE_SYSTEM_DEFAULT
+            )
+            storage_sys = StorageSystem(agave, default_sys.get_system_id())
+            success, result = storage_sys.test()
+            data['systemHasKeys'] = success
+            data['pushKeysSystem'] = storage_sys.to_dict()
         else:
             METRICS.debug("user:{} is requesting all public apps".format(request.user.username))
             public_only = request.GET.get('publicOnly')
@@ -105,16 +115,6 @@ class AppsView(BaseApiView):
                     "name": name
                 }
             data = agave.apps.list(**list_kwargs)
-
-        # check if default system needs keys pushed
-        default_sys = UserSystemsManager(
-            request.user,
-            settings.PORTAL_DATA_DEPOT_LOCAL_STORAGE_SYSTEM_DEFAULT
-        )
-        storage_sys = StorageSystem(agave, default_sys.get_system_id())
-        success, result = storage_sys.test()
-        data['systemHasKeys'] = success
-        data['pushKeysSystem'] = storage_sys.to_dict()
 
         return JsonResponse({"response": data})
 
