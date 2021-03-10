2376e928f328a63b7269efc0c472f5ada17319f8
Juan Martinez <jmartinez@tacc.utexas.edu>
2021-03-10 10:44:31 -0600

    Cleaning code.

--- client/src/components/DataFiles/DataFilesModals/DataFilesUploadModalListing/DataFilesUploadModalListingTable.js
+++ client/src/components/DataFiles/DataFilesModals/DataFilesUploadModalListing/DataFilesUploadModalListingTable.js
@@ -3,7 +3,6 @@ import { useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
 import { Table } from 'reactstrap';
 import { LoadingSpinner } from '_common';
-import { findSystemDisplayName } from 'utils/systems';
 import { FileLengthCell } from '../../DataFilesListing/DataFilesListingCells';
 
 const DataFilesUploadStatus = ({ i, removeCallback }) => {
@@ -33,58 +32,48 @@ DataFilesUploadStatus.propTypes = {
 };
 
 function DataFilesUploadModalListingTable({ uploadedFiles, setUploadedFiles }) {
-  const params = useSelector(state => state.files.params.FilesListing);
-  const systemList = useSelector(state => state.systems.storage.configuration);
-  const systemDisplayName = findSystemDisplayName(systemList, params.system);
-
   const removeFile = id => {
     setUploadedFiles(uploadedFiles.filter(f => f.id !== id));
   };
 
   return (
     <div style={{ marginTop: '10px' }}>
-      <span style={{ fontSize: '20px' }}>
-        Uploading to {systemDisplayName}/{params.path}
-      </span>
-
-      <div>
-        <div
-          style={{
-            border: '1px solid black',
-            width: '100%',
-            marginTop: '5px',
-            height: '300px',
-            overflow: 'auto'
-          }}
-        >
-          <Table striped>
-            <thead>
-              <tr>
-                <th>Name</th>
-                <th>Size</th>
-                <th aria-label="null" />
+      <div
+        style={{
+          border: '1px solid black',
+          width: '100%',
+          marginTop: '5px',
+          height: '300px',
+          overflow: 'auto'
+        }}
+      >
+        <Table striped>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Size</th>
+              <th aria-label="null" />
+            </tr>
+          </thead>
+          <tbody>
+            {uploadedFiles.map((file, i) => (
+              <tr key={file.id}>
+                <td style={{ verticalAlign: 'middle' }}>{file.data.name}</td>
+                <td style={{ verticalAlign: 'middle' }}>
+                  <FileLengthCell cell={{ value: file.data.size }} />
+                </td>
+                <td>
+                  <span className="float-right">
+                    <DataFilesUploadStatus
+                      i={file.id}
+                      removeCallback={removeFile}
+                    />
+                  </span>
+                </td>
               </tr>
-            </thead>
-            <tbody>
-              {uploadedFiles.map((file, i) => (
-                <tr key={file.id}>
-                  <td style={{ verticalAlign: 'middle' }}>{file.data.name}</td>
-                  <td style={{ verticalAlign: 'middle' }}>
-                    <FileLengthCell cell={{ value: file.data.size }} />
-                  </td>
-                  <td>
-                    <span className="float-right">
-                      <DataFilesUploadStatus
-                        i={file.id}
-                        removeCallback={removeFile}
-                      />
-                    </span>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </Table>
-        </div>
+            ))}
+          </tbody>
+        </Table>
       </div>
     </div>
   );
