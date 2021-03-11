05ecd90ddd24a105bd99e4972ebdfff2ef5cd410
Juan Martinez <jmartinez@tacc.utexas.edu>
2021-03-11 10:51:14 -0600

    Adding css styles to css module.

--- client/src/components/DataFiles/DataFilesModals/DataFilesUploadModalListing/DataFilesUploadModalListingTable.js
+++ client/src/components/DataFiles/DataFilesModals/DataFilesUploadModalListing/DataFilesUploadModalListingTable.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 import { Table } from 'reactstrap';
 import { LoadingSpinner } from '_common';
 import { FileLengthCell } from '../../DataFilesListing/DataFilesListingCells';
+import './DataFilesUploadModalListingTable.module.scss';
 
 const DataFilesUploadStatus = ({ i, removeCallback }) => {
   const status = useSelector(state => state.files.operationStatus.upload[i]);
@@ -37,44 +38,34 @@ function DataFilesUploadModalListingTable({ uploadedFiles, setUploadedFiles }) {
   };
 
   return (
-    <div style={{ marginTop: '10px' }}>
-      <div
-        style={{
-          border: '1px solid black',
-          width: '100%',
-          marginTop: '5px',
-          height: '300px',
-          overflow: 'auto'
-        }}
-      >
-        <Table striped>
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Size</th>
-              <th aria-label="null" />
+    <div styleName="table-wrapper">
+      <Table striped>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Size</th>
+            <th aria-label="null" />
+          </tr>
+        </thead>
+        <tbody>
+          {uploadedFiles.map((file, i) => (
+            <tr key={file.id}>
+              <td style={{ verticalAlign: 'middle' }}>{file.data.name}</td>
+              <td style={{ verticalAlign: 'middle' }}>
+                <FileLengthCell cell={{ value: file.data.size }} />
+              </td>
+              <td>
+                <span className="float-right">
+                  <DataFilesUploadStatus
+                    i={file.id}
+                    removeCallback={removeFile}
+                  />
+                </span>
+              </td>
             </tr>
-          </thead>
-          <tbody>
-            {uploadedFiles.map((file, i) => (
-              <tr key={file.id}>
-                <td style={{ verticalAlign: 'middle' }}>{file.data.name}</td>
-                <td style={{ verticalAlign: 'middle' }}>
-                  <FileLengthCell cell={{ value: file.data.size }} />
-                </td>
-                <td>
-                  <span className="float-right">
-                    <DataFilesUploadStatus
-                      i={file.id}
-                      removeCallback={removeFile}
-                    />
-                  </span>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </Table>
-      </div>
+          ))}
+        </tbody>
+      </Table>
     </div>
   );
 }
