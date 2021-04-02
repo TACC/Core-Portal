8b8a434a03e35a73d7b9cd217cdd4ef8d2151edf
Nathan Franklin <nathan.franklin@gmail.com>
2021-04-02 15:30:51 -0500

    Update project id management command

--- server/portal/apps/projects/management/commands/projects_id.py
+++ server/portal/apps/projects/management/commands/projects_id.py
@@ -1,96 +1,72 @@
 """Management command."""
 
 from django.conf import settings
+from django.db import transaction
+from django.core.exceptions import ObjectDoesNotExist
 from django.core.management.base import BaseCommand
 from portal.libs.agave.utils import service_account
-from portal.libs.agave.models.files import BaseFile
 from portal.apps.projects.models.base import ProjectId, Project
+from portal.libs.agave.operations import iterate_listing
 
 
 class Command(BaseCommand):
-    """Command class."""
-
     help = (
-        'Manage projects next Id. By default this command will print '
+        'Manage projects latest project i. By default this command will print '
         'the current latest project id, the last project id used in '
-        'storage systems and the last project id used in folders created '
-        'in corral.'
+        'storage systems and the last project id used in folders created.'
     )
 
-    def _get_latest_project_storage(self):  # pylint:disable=no-self-use
-        """Get latest agave project storage."""
+    @staticmethod
+    def get_latest_project_storage(ignore_above_value=None):
+        """Get latest agave project storage.
+
+        :param ignore_above_value: If provided, then ignore projects ids that are greater than or equal to this value.
+        """
         offset = 0
-        limit = 100
-        prjs = Project.listing(
-            service_account(),
-            offset=offset,
-            limit=limit
-        )
-        done = False
+        limit = 1000
         latest = -1
-        while not done:
-            prj_id = -1
+        while True:
+            prjs = Project.listing(
+                service_account(),
+                offset=offset,
+                limit=limit
+            )
+            prjs = [p for p in prjs]
             for prj in prjs:
                 prj_id = prj.storage.id.replace(
-                    settings._PORTAL_DATA_DEPOT_PROJECTS_SYSTEM_PREFIX,
+                    settings.PORTAL_PROJECTS_NAME_PREFIX,
                     ''
                 )
                 if '-' not in prj_id:
                     continue
                 _, prj_id = prj_id.rsplit('-')
-                try:
-                    prj_id = int(prj_id)
-                except ValueError:
-                    # prj_id is not an int
-                    pass
+                prj_id = int(prj_id)
 
-                if prj_id > latest:
+                if prj_id > latest and (ignore_above_value is None or prj_id < ignore_above_value):
                     latest = prj_id
-            if prj_id == -1:
-                done = True
-            else:
-                offset += 100
-                prjs = Project.listing(
-                    service_account(),
-                    offset=offset,
-                    limit=limit
-                )
+
+            offset += limit
+            if len(prjs) < limit:
+                break
         return latest
 
-    def _get_latest_project_directory(self):  # pylint:disable=no-self-use
-        """Get latest agave project directory."""
-        offset = 0
-        limit = 100
-        _file = BaseFile(
-            service_account(),
-            system=settings.PORTAL_PROJECTS_ROOT_SYSTEM_NAME,
-            path='/'
-        )
-        dirs = _file.children(offset=offset, limit=limit)
-        done = False
+    @staticmethod
+    def get_latest_project_directory(ignore_above_value=None):
+        """Get latest agave project directory.
+
+        :param ignore_above_value: If provided, then ignore projects ids that are greater than or equal to this value.
+        """
         latest = -1
-        while not done:
-            dir_id = -1
-            for _dir in dirs:
-                if ('-' not in _dir.name or
-                        not _dir.name.startswith(settings.PORTAL_PROJECTS_ID_PREFIX)):
-                    continue
-                _, dir_id = _dir.name.rsplit('-', 1)
-                try:
-                    dir_id = int(dir_id)
-                except ValueError:
-                    # dir_id is not an int
-                    pass
-
-                if dir_id > latest:
-                    latest = dir_id
-
-            if dir_id == -1:
-                done = True
-            else:
-                offset += 100
-                _file.set_children(None)
-                dirs = _file.children(offset=offset, limit=limit)
+        for f in iterate_listing(service_account(),
+                                 system=settings.PORTAL_PROJECTS_ROOT_SYSTEM_NAME,
+                                 path='/'):
+            name = f["name"]
+            if '-' not in name or not name.startswith(settings.PORTAL_PROJECTS_ID_PREFIX):
+                continue
+            _, dir_id = name.rsplit('-', 1)
+            dir_id = int(dir_id)
+            if dir_id > latest and (ignore_above_value is None or dir_id < ignore_above_value):
+                latest = dir_id
         return latest
 
     def add_arguments(self, parser):
@@ -99,29 +75,50 @@ class Command(BaseCommand):
             '--update',
             action='store',
             type=int,
-            help='Integer to update saved DB value.'
+            help='Update project id DB value.'
+        )
+        parser.add_argument(
+            '--update-using-storage-system-id',
+            action='store_true',
+            help='Update project id DB value using value derived from latest storage system project id.'
+        )
+        parser.add_argument(
+            '--ignore-large-project-ids',
+            action='store',
+            type=int,
+            help='Ignore project ids larger than a certain value'
         )
 
     def handle(self, *args, **options):
         """Handle command."""
+        ignore_large_ids = options["ignore_large_project_ids"]
+        if ignore_large_ids:
+            self.stdout.write('NOTE(!!!!): Ignoring project ids >= {} when '
+                              'processing/updating the storage systems and directories'.format(ignore_large_ids))
+
+        latest_storage_system_id = Command.get_latest_project_storage(ignore_above_value=ignore_large_ids)
+        latest_project_id = Command.get_latest_project_directory(ignore_above_value=ignore_large_ids)
+
+        if latest_storage_system_id == -1:
+            self.stdout.write('There are no project storage systems.')
+        if latest_project_id == -1:
+            self.stdout.write('There are no project directories.')
+
+        self.stdout.write('Latest storage system project id: {}'.format(latest_storage_system_id))
+        self.stdout.write('Latest directory project id: {}'.format(latest_project_id))
+
+        try:
+            with transaction.atomic():
+                model_project_id = ProjectId.objects.select_for_update().latest('last_updated').value
+            self.stdout.write('Latest project id in ProjectId model: {}'.format(model_project_id))
+        except ObjectDoesNotExist:
+            self.stdout.write('Latest project id in ProjectId mode: None')
+            if (options.get('update') or options.get('find-and-update')):
+                ProjectId.objects.create(value=1).save()
+
         if options.get('update'):
+            self.stdout.write('Updating to user provided value of: {}'.format(options.get('update')))
             ProjectId.update(options.get('update'))
-        else:
-            latest_storage_prj_id = self._get_latest_project_storage()
-            if latest_storage_prj_id == -1:
-                self.stdout.write('There are no project storage systems.')
-            else:
-                self.stdout.write(
-                    'Latest Storage Project Id: {prj_id}'.format(
-                        prj_id=latest_storage_prj_id
-                    )
-                )
-            latest_project_dir_id = self._get_latest_project_directory()
-            if latest_project_dir_id == -1:
-                self.stdout.write('There are no project directories.')
-            else:
-                self.stdout.write(
-                    'Latest Directory Project Id: {dir_id}'.format(
-                        dir_id=latest_project_dir_id
-                    )
-                )
+        elif options["update_using_storage_system_id"]:
+            self.stdout.write('Updating to user provided value of: {}'.format(latest_storage_system_id))
+            ProjectId.update(latest_storage_system_id)
