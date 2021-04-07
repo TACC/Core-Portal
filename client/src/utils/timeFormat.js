635e20643e8f1a3d2960a3b0b865856fa6bad0d5
Wesley Bomar <wbomar@tacc.utexas.edu>
2021-04-07 17:25:41 -0500

    Quick: Have Data List Cells use Util Functions

--- client/src/utils/timeFormat.js
+++ client/src/utils/timeFormat.js
@@ -1,3 +1,8 @@
+/**
+ * Create a string representation of date using internal standard
+ * @param {Date} timeDateValue - A date object
+ * @returns {string}
+ */
 export function formatDate(timeDateValue) {
   return timeDateValue.toLocaleDateString('en-US', {
     day: '2-digit',
@@ -6,6 +11,11 @@ export function formatDate(timeDateValue) {
   });
 }
 
+/**
+ * Create a string representation of time using internal standard
+ * @param {Date} timeDateValue - A date object
+ * @returns {string}
+ */
 export function formatTime(timeDateValue) {
   return timeDateValue.toLocaleTimeString('en-US', {
     hour12: false,
@@ -14,6 +24,27 @@ export function formatTime(timeDateValue) {
   });
 }
 
+/**
+ * Create a string representation of date/time using internal standard
+ * @param {Date} dateTime - A date object
+ * @returns {string}
+ */
 export function formatDateTime(timeDateValue) {
   return `${formatDate(timeDateValue)} ${formatTime(timeDateValue)}`;
+}
+
+/**
+ * A standard-format date string or UNIX timestamp
+ * @typedef {string|number} dateTime
+ * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/Date
+ */
+/**
+ * Create a string representation of date/time using internal standard
+ * @param {dateTime} dateTimeValue - A single value date-time representation
+ * @returns {string}
+ */
+export function formatDateTimeFromValue(dateTimeValue) {
+  const date = new Date(dateTimeValue);
+
+  return formatDateTime(date);
 }
