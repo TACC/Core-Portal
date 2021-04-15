2196c4ad551bda6fab0941d23a46bfb9cacaa17b
Wesley Bomar <wbomar@tacc.utexas.edu>
2021-04-15 13:36:49 -0500

    GH-191: Use CMS script to add theme class

--- client/src/components/Workbench/index.js
+++ client/src/components/Workbench/index.js
@@ -2,9 +2,6 @@ import { hot } from 'react-hot-loader/root';
 import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-
-import getCMSSettings from 'utils/cmsSettings';
-
 import Workbench from './Workbench';
 import * as ROUTES from '../../constants/routes';
 import TicketStandaloneCreate from '../Tickets/TicketStandaloneCreate';
@@ -18,18 +15,6 @@ function AppRouter() {
     dispatch({ type: 'FETCH_WELCOME' });
     dispatch({ type: 'FETCH_AUTHENTICATED_USER' });
   }, []);
-
-  const cmsSettings = getCMSSettings();
-  const { themeClassName } = cmsSettings;
-  useEffect(() => {
-    const root = document.documentElement;
-
-    if (themeClassName) root.classList.add(themeClassName);
-
-    return function cleanup() {
-      if (themeClassName) root.documentElement.classList.remove(themeClassName);
-    };
-  }, [themeClassName]);
 
   return (
     <Router>
