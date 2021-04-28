1c6ab7778503fdcaaba510a37956b1e890405f09
Juan <jmartinez@tacc.utexas.edu>
2021-04-28 14:08:08 -0500

    Update server/portal/apps/workbench/urls.py
    Co-authored-by: Sal Tijerina <r.sal.tijerina@gmail.com>

--- server/portal/apps/workbench/urls.py
+++ server/portal/apps/workbench/urls.py
@@ -9,6 +9,6 @@ app_name = 'workbench'
 urlpatterns = [
     re_path('account', IndexView.as_view(), name='account'),
     re_path('dashboard', IndexView.as_view(), name='dashboard'),
-    re_path('onboarding/admin', IndexView.as_view(), name='onboardingadmin'),
+    re_path('onboarding/admin', IndexView.as_view(), name='onboarding_admin'),
     re_path('', IndexView.as_view(), name='index'),
 ]
