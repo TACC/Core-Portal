8a0aecc966c310ccba02942d7ad0f8ddf986b404
Alex Rocha <rochaa@tacc.utexas.edu>
2021-06-02 11:24:37 -0500

    matching indentations

--- client/src/redux/reducers/datafiles.reducers.js
+++ client/src/redux/reducers/datafiles.reducers.js
@@ -370,12 +370,12 @@ export function files(state = initialFilesState, action) {
         }
       };
     case 'STORE_SELECTOR_REF':
-        return {
-          ...state,
-          refs: {
-            ...state.refs,
-            FileSelector: action.payload
-          }
+      return {
+        ...state,
+        refs: {
+          ...state.refs,
+          FileSelector: action.payload
+        }
       };
     case 'CLEAR_REFS':
       return { ...state, refs: {} };
