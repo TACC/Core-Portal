695b69ae8b62fe3cb742db43a010cb3ceea00538
Alex Rocha <rochaa@tacc.utexas.edu>
2021-06-02 11:37:36 -0500

    removing unused import

--- client/src/components/DataFiles/DataFilesModals/DataFilesSelectModal.js
+++ client/src/components/DataFiles/DataFilesModals/DataFilesSelectModal.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Modal, ModalHeader, ModalBody } from 'reactstrap';
 import { shallowEqual, useDispatch, useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
