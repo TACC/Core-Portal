fe1d5009346c7236b05e3686b06eed61a2ca4d68
Sal Tijerina <r.sal.tijerina@gmail.com>
2021-06-15 10:19:34 -0500

    fix missing .py extention on import (#427)

--- server/portal/settings/settings.py
+++ server/portal/settings/settings.py
@@ -25,7 +25,7 @@ def gettext(s): return s  # noqa:E731
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-if os.path.isfile(os.path.join(BASE_DIR, 'settings', 'settings_custom')):
+if os.path.isfile(os.path.join(BASE_DIR, 'settings', 'settings_custom.py')):
     from portal.settings import settings_custom
 else:
     from portal.settings import settings_default as settings_custom
