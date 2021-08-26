24554356d1ad61ff3dac9d91a886e348aa83ede4
W. Bomar <62723358+tacc-wbomar@users.noreply.github.com>
2021-08-26 13:09:31 -0500

    FP-223: Add Custom Close Icon to Modals (#465)
    * FP-223: some darker, custom modal close buttons
    
    Only modals using `charCode` have been updated.
    
    * FP-223: all modal close buttons use custom icon
    
    - All `<ModalHeader>`s use `charCode="&#xe912;"`.
    - Any lint fixes needed are made.
    
    * FP-223: less modal close button CSS comments
    
    * FP-223: remove comment that is now inaccurate
    
    * FP-223: Fix comment typo
    
    * FP-223: CSS Lint ignore custom font family Cortal

--- client/src/components/DataFiles/DataFilesModals/DataFilesCopyModal.js
+++ client/src/components/DataFiles/DataFilesModals/DataFilesCopyModal.js
@@ -137,7 +137,9 @@ const DataFilesCopyModal = React.memo(() => {
       size="xl"
       className="dataFilesModal"
     >
-      <ModalHeader toggle={toggle}>Copy</ModalHeader>
+      <ModalHeader toggle={toggle} charCode="&#xe912;">
+        Copy
+      </ModalHeader>
       <ModalBody style={{ height: '70vh' }}>
         <div className="row h-100">
           <div className="col-md-6 d-flex flex-column">
