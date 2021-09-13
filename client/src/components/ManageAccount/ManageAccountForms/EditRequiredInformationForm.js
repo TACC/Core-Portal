f13db46887dc6b1cb6282909de1205c8af7cde26
williamkwon97 <uk669@utexas.edu>
2021-09-13 14:53:33 -0500

    add a new css module

--- client/src/components/ManageAccount/ManageAccountForms/EditRequiredInformationForm.js
+++ client/src/components/ManageAccount/ManageAccountForms/EditRequiredInformationForm.js
@@ -93,7 +93,7 @@ export default function() {
     phone: str()
       .matches(
         /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/,
-        'Phone number is not valid' + str(phone)
+        'Phone number is not valid'
       )
       .required('Please enter your phone number'),
     // Schema for 'select' fields
