d6520a1a0c7f290076e582205f390e22ca3b45b1
Juan Martinez <jmartinez@tacc.utexas.edu>
2021-09-15 16:39:00 -0500

    Returning function with no action.

--- server/portal/apps/search/tasks.py
+++ server/portal/apps/search/tasks.py
@@ -29,7 +29,7 @@ def agave_indexer(self, systemId, filePath='/', recurse=True, update_pems=False,
     if next(sys for sys in settings.PORTAL_DATAFILES_STORAGE_SYSTEMS
         if sys['name'] == 'Shared Workspaces' and sys['hideSearchBar'] == True
         and systemId.startswith(settings.PORTAL_PROJECTS_SYSTEM_PREFIX)):
-            return {}
+            return
 
     try:
         filePath, folders, files = walk_levels(client, systemId, filePath, ignore_hidden=ignore_hidden).__next__()
