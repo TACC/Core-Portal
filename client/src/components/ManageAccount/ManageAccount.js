4f6b460fb5d0fba96b90f2ecb93c07bafc279904
Scott Willrich <swillrich@gmail.com>
2021-09-15 15:53:54 -0500

    Update Manage.js - add shallowEqual
    Added 'shallowEqual' not needed for my changes, but needed in merged code.

--- client/src/components/ManageAccount/ManageAccount.js
+++ client/src/components/ManageAccount/ManageAccount.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch, useSelector, shallowEqual } from 'react-redux';
 import { isEmpty } from 'lodash';
 import { LoadingSpinner, Section, SectionMessage } from '_common';
 import {
