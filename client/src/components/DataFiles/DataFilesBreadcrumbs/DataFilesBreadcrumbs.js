96b4e0c102110e636b769dcc6ccd3768fdc30e24
williamkwon97 <uk669@utexas.edu>
2021-09-22 11:50:55 -0500

    add a new action to setTtitleDescrition function on DataFilesPrjectList.js

--- client/src/components/DataFiles/DataFilesBreadcrumbs/DataFilesBreadcrumbs.js
+++ client/src/components/DataFiles/DataFilesBreadcrumbs/DataFilesBreadcrumbs.js
@@ -103,7 +103,6 @@ const DataFilesBreadcrumbs = ({
     system,
     projectTitle
   );
-
   return (
     <div className={`breadcrumbs ${className}`}>
       {scheme === 'projects' && (
--- client/src/redux/reducers/projects.reducers.js
+++ client/src/redux/reducers/projects.reducers.js
@@ -184,6 +184,7 @@ export default function projects(state = initialState, action) {
           result: null
         }
       };
+
     case 'PROJECTS_SET_TITLE_DESCRIPTION_STARTED':
       return {
         ...state,
