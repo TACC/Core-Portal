f21c088d009c64119bd69f4c8976e23d2a6d970c
William Kwon <34716439+williamkwon97@users.noreply.github.com>
2021-10-01 19:46:16 -0500

    Update client/src/components/Applications/AppLayout/AppLayout.test.js
    Co-authored-by: Sal Tijerina <r.sal.tijerina@gmail.com>

--- client/src/components/Applications/AppLayout/AppLayout.test.js
+++ client/src/components/Applications/AppLayout/AppLayout.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import configureStore from "redux-mock-store";
-import  AppsLayout from "./AppLayout";
+import AppsLayout from "./AppLayout";
 import { MemoryRouter, Route } from 'react-router-dom';
 import systemsFixture from '../../DataFiles/fixtures/DataFiles.systems.fixture';
 import { projectsFixture } from '../../../redux/sagas/fixtures/projects.fixture';
