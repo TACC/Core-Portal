ed9c13a74cef18d732bb85445dcbbf30dd5f1d2e
Jarosenb <jrosenberg@tacc.utexas.edu>
2021-10-05 16:50:04 -0500

    serve icon fonts correctly in both dev/prod configs

--- server/portal/settings/settings.py
+++ server/portal/settings/settings.py
@@ -215,7 +215,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, '../static')
 MEDIA_ROOT = os.path.join(BASE_DIR, '../media')
 
 STATICFILES_DIRS = [
-    ('build', os.path.join(BASE_DIR, '../../client/build')),
+    os.path.join(BASE_DIR, '../../client/dist'),
+    # Serve fonts using the cep.dev hostname in debug mode
+    ('src/fonts', os.path.join(BASE_DIR, '../../client/src/fonts'))
 ]
 
 STATICFILES_FINDERS = [
