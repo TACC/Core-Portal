571e7890ea64716bceed725365a2323674b37272
swillrich5 <swillrich@gmail.com>
2021-10-13 08:21:26 -0500

    added multiple files fixture and test

--- client/src/utils/filePermissions.test.js
+++ client/src/utils/filePermissions.test.js
@@ -26,6 +26,26 @@ const publicFixture = {
   scheme: 'public',
   api: 'tapis',
 };
+const multipleFilesFixture = {
+  files: [
+    {
+      name: 'text.txt',
+      path: '/test/text.txt',
+      lastModified: '2020-08-01T00:00:00-05:00',
+      system: 'frontera.home.test',
+      type: 'file',
+    },
+    {
+      name: 'text2.txt',
+      path: '/test/text2.txt',
+      lastModified: '2020-08-01T00:00:00-05:00',
+      system: 'frontera.home.test',
+      type: 'file',
+    },
+  ],
+  scheme: 'private',
+  api: 'tapis',
+}
 describe('getFilePermissions utility function', () => {
   it.each([
     'rename',
@@ -37,7 +57,6 @@ describe('getFilePermissions utility function', () => {
     'compress',
     'areMultipleFilesOrFolderSelected'
   ])('Correctly evaluate %s permission', (pem) => {
-    // if (pem !== 'compress') {
     if (!['compress', 'areMultipleFilesOrFolderSelected'].includes(pem)) {
       expect(getFilePermissions(pem, privateFixture)).toEqual(true);
     } else {
@@ -47,6 +66,11 @@ describe('getFilePermissions utility function', () => {
       expect(getFilePermissions(pem, publicFixture)).toEqual(true);
     } else {
       expect(getFilePermissions(pem, publicFixture)).toEqual(false);
+    }
+    if (['areMultipleFilesOrFolderSelected', 'compress', 'copy', 'move', 'trash'].includes(pem)) {
+      expect(getFilePermissions(pem, multipleFilesFixture)).toEqual(true);
+    } else {
+      expect(getFilePermissions(pem, multipleFilesFixture)).toEqual(false);
     }
   });
 });
