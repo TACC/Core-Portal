956fb3190806c4a4211a9864ad6c526cbc295e79
Jarosenb <jrosenberg@tacc.utexas.edu>
2021-10-20 16:53:33 -0500

    Fallback to spinner while JS loads

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,13 +1,14 @@
 import React, {Suspense} from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import LoadingSpinner from '_common/LoadingSpinner';
 const AppRouter = React.lazy(() => import('./components/Workbench'));
 import './index.css';
 import store from './redux/store';
 
 ReactDOM.render(
   <Provider store={store}>
-    <Suspense fallback={<div>Lazy loading the app...</div>}>
+    <Suspense fallback={<LoadingSpinner/>}>
       <AppRouter/>
     </Suspense>
   </Provider>,
