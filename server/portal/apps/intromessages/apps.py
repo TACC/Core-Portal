23487343f1c1bb88a46cfadd579bd0aca83d2608
swillrich5 <swillrich@gmail.com>
2021-11-01 08:36:03 -0500

    stub unit_test.py added

--- server/portal/apps/intromessages/apps.py
+++ server/portal/apps/intromessages/apps.py
@@ -3,3 +3,4 @@ from django.apps import AppConfig
 
 class IntromessagesConfig(AppConfig):
     name = 'intromessages'
+    app_label = 'intromessages'
--- server/portal/apps/intromessages/unit_test.py
+++ server/portal/apps/intromessages/unit_test.py
@@ -3,3 +3,9 @@ from portal.apps.intromessages.views import IntroMessagesView
 import json
 import pytest
 import logging
+
+
+from portal.apps.intromessages.models import IntroMessages
+
+def test_example():
+    assert 1 == 1
