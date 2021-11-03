4637caed851d28b3d27d79d2090419044dadbf48
swillrich5 <swillrich@gmail.com>
2021-11-03 12:54:40 -0500

    added unauthenticated user get test

--- server/portal/apps/intromessages/intro_unit_test.py
+++ server/portal/apps/intromessages/intro_unit_test.py
@@ -39,12 +39,20 @@ def mock_intromessage(scope="module"):
     ]
 
 
+# test get of "read" (not unread) IntroMessages for an authenticated user
 @pytest.mark.django_db(transaction=True, reset_sequences=True)
 def test_intromessages_get(client, authenticated_user, mock_intromessages):
     response = client.get('/api/intromessages/msg/')
     assert response.status_code == 200
 
+# test get of "read" IntroMessages for an unauthenticated user
+# user should be redirected to login?
+@pytest.mark.django_db(transaction=True, reset_sequences=True)
+def test_intromessages_get(client, regular_user, mock_intromessages):
+    response = client.get('/api/intromessages/msg/')
+    assert response.status_code == 302
 
+# test the marking of an IntroMessage as "read" by writing to the database
 @pytest.mark.django_db(transaction=True, reset_sequences=True)
 def test_intromessages_put(client, authenticated_user):
     body = {
