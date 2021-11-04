a96c6d939947493d214392531c050251c1ee4855
Carrie Arnold <cmarnold@tacc.utexas.edu>
2021-11-04 12:34:31 -0500

    Trying to add a failure test

--- server/portal/apps/onboarding/steps/allocation_unit_test.py
+++ server/portal/apps/onboarding/steps/allocation_unit_test.py
@@ -11,11 +11,23 @@ def get_allocations_mock(mocker):
             'inactive': [{'allocation'}] }
     yield get_allocations
 
+@pytest.fixture
+def get_allocations_failure_mock(mocker):
+    get_allocations = mocker.patch('portal.apps.onboarding.steps.allocation.get_allocations')
+    get_allocations.return_value = {'hosts': {},
+            'portal_alloc': None,
+            'active': [],
+            'inactive': [] }
+    yield get_allocations
+
 
 @pytest.fixture
 def allocation_step_complete_mock(mocker):
     yield mocker.patch.object(AllocationStep, 'complete')
 
+@pytest.fixture
+def allocation_step_userwait_mock(mocker):
+    yield mocker.patch.object(AllocationStep, 'userwait')
 
 def test_get_allocations(regular_user, get_allocations_mock, allocation_step_complete_mock):
     step = AllocationStep(regular_user)
@@ -25,3 +37,9 @@ def test_get_allocations(regular_user, get_allocations_mock, allocation_step_com
             'portal_alloc': None,
             'active': [{'allocation'}],
             'inactive': [{'allocation'}] })
+
+def test_get_allocations_failure(regular_user, get_allocations_mock, allocation_step_complete_mock):
+    step = AllocationStep(regular_user)
+    step.process()
+    get_allocations_failure_mock.assert_called_with("username", force=True)
+    allocation_step_userwait_mock.assert_called_with()
