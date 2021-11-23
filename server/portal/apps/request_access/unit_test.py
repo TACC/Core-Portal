59135bc7fd5a1118bf59ec5c167b5febc2a29101
Juan <jmartinez@tacc.utexas.edu>
2021-11-23 14:44:20 -0600

    Update server/portal/apps/request_access/unit_test.py
    Co-authored-by: Nathan Franklin <nfranklin@tacc.utexas.edu>

--- server/portal/apps/request_access/unit_test.py
+++ server/portal/apps/request_access/unit_test.py
@@ -1,11 +1,7 @@
 
 def test_request_access_get(client, regular_user):
-    """Users who are authenticated are redirected to workbench/dashboard
-    """
-    client.force_login(regular_user)
     response = client.get('/request-access/')
-    assert response.status_code == 302
-    assert response.url == '/workbench/dashboard/'
+    assert response.status_code == 200
 
 
 def test_request_access_authenticated(client, authenticated_user):
