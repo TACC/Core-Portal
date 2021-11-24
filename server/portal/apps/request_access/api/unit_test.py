0fefb628afe7c9bf8c216ad92a7c38c5419faabb
Juan Martinez <jmartinez@tacc.utexas.edu>
2021-11-24 14:25:52 -0600

    Unit tests update.

--- server/portal/apps/request_access/api/unit_test.py
+++ server/portal/apps/request_access/api/unit_test.py
@@ -6,15 +6,7 @@ from django.conf import settings
 
 @pytest.fixture
 def mock_rtutil(mocker, mock_rt):
-    mocker.patch('portal.apps.request_access.api.views.rtUtil.DjangoRt',
-                 return_value=mock_rt)
-    yield mock_rt
-
-
-@pytest.fixture
-def mock_rtutil_no_access(mocker, mock_rt):
-    mock_rt.hasAccess.return_value = False
-    mocker.patch('portal.apps.request_access.api.views.rtUtil.DjangoRt',
+    mocker.patch('portal.apps.tickets.utils.rtUtil.DjangoRt',
                  return_value=mock_rt)
     yield mock_rt
 
@@ -29,15 +21,26 @@ def mock_rt(mocker):
 
 @pytest.fixture
 def get_authenticate(mocker):
-    mock = mocker.patch('portal.apps.request_access.api.views.TASClient.authenticate')
+    mock = mocker.patch(
+            'portal.apps.request_access.api.views.TASClient.authenticate')
     mock.return_value = {}
     yield mock
 
 
 @pytest.fixture
+def get_authenticate_error(mocker):
+    mock = mocker.patch(
+            'portal.apps.request_access.api.views.TASClient.authenticate')
+    mock.return_value = None
+    yield mock
+
+
+@pytest.fixture
 def get_user(mocker):
-    mock = mocker.patch('portal.apps.request_access.api.views.TASClient.get_user')
-    with open(os.path.join(settings.BASE_DIR, 'fixtures/tas/tas_user.json')) as f:
+    mock = mocker.patch(
+                'portal.apps.request_access.api.views.TASClient.get_user')
+    with open(os.path.join(settings.BASE_DIR,
+              'fixtures/tas/tas_user.json')) as f:
         tas_user = json.load(f)
     mock.return_value = tas_user
     yield mock
@@ -46,11 +49,12 @@ def get_user(mocker):
 @pytest.mark.django_db(transaction=True, reset_sequences=True)
 def test_request_access_wrong_user_password(client,
                                             regular_user,
-                                            mock_rtutil):
+                                            get_authenticate_error):
     response = client.post('/api/request-access/',
-                           data={"problem_description": "This is the problem description",
-                                 "username": "wrongUsername",
-                                 "password": "wrongPassword"})
+                           data={"problem_description":
+                                 "This is the problem description",
+                                 "username": "testUsername",
+                                 "password": "testPassword"})
     assert response.status_code == 401
 
 
