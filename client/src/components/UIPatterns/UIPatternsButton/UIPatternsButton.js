542f63fde8f4a646562552bc9c174e17bac5ad64
swillrich5 <swillrich@gmail.com>
2021-12-01 11:33:54 -0600

    button icon functionality in progress

--- client/src/components/UIPatterns/UIPatternsButton/UIPatternsButton.js
+++ client/src/components/UIPatterns/UIPatternsButton/UIPatternsButton.js
@@ -11,17 +11,17 @@ const UIPatternsButton = () => {
   return (
     <div>
       <p>Buttons using the predefined Bootstrap button styles</p>
-      <Button className="m-1" iconBeforeName="\e957" type="primary" onClick={() => clickButton('Primary')}>Primary Button</Button>
-      <Button className="m-1" type="secondary" onClick={() => clickButton('Secondary')}>Secondary Button</Button>
-      <Button className="m-1" type="success" onClick={() => clickButton('Success')}>Success Button</Button>
-      <Button className="m-1" type="info" onClick={() => clickButton('Info')}>Info Button</Button>
-      <Button className="m-1" type="warning" onClick={() => clickButton('Warning')}>Warning Button</Button>
-      <Button className="m-1" type="danger" onClick={() => clickButton('Deeper')}>Danger Button</Button>
-      <Button className="m-1"type="link" onClick={() => clickButton('Link')}>Link Button</Button>
+      <Button iconBeforeName="alert" className="m-1" iconBeforeName="\e957" type="primary" onClick={() => clickButton('Primary')}>Primary Button</Button>
+      <Button iconBeforeName="nav-left" className="m-1" type="secondary" onClick={() => clickButton('Secondary')}>Secondary Button</Button>
+      <Button iconBeforeName="nav-left" className="m-1" type="success" onClick={() => clickButton('Success')}>Success Button</Button>
+      <Button iconBeforeName="nav-left" className="m-1" type="info" onClick={() => clickButton('Info')}>Info Button</Button>
+      <Button iconBeforeName="nav-left" className="m-1" type="warning" onClick={() => clickButton('Warning')}>Warning Button</Button>
+      <Button iconBeforeName="nav-left" className="m-1" type="danger" onClick={() => clickButton('Deeper')}>Danger Button</Button>
+      <Button iconBeforeName="nav-left" className="m-1"type="link" onClick={() => clickButton('Link')}>Link Button</Button>
       <hr />
       <p>Buttons using the predefined Bootstrap button styles</p>
-      <Button className="m-1" size="sm" color="primary" outline onClick={() => clickButton('Primary')}>Primary Button</Button>
-      <Button className="m-1" color="secondary" onClick={() => clickButton('Secondary')}>Secondary Button</Button>
+      <Button iconBeforeName="nav-left" className="m-1" size="sm" color="primary" outline onClick={() => clickButton('Primary')}>Primary Button</Button>
+      <Button iconBeforeName="nav-left" className="m-1" color="secondary" onClick={() => clickButton('Secondary')}>Secondary Button</Button>
       <Button className="m-1" color="success" onClick={() => clickButton('Success')}>Success Button</Button>
       <Button className="m-1" color="info" onClick={() => clickButton('Info')}>Info Button</Button>
       <Button className="m-1" color="warning" onClick={() => clickButton('Warning')}>Warning Button</Button>
