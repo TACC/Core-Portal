a3052a5d28d28f87f0c80af20ac2a42ccc99fa51
Juan Martinez <jmartinez@tacc.utexas.edu>
2021-12-09 09:54:50 -0600

    Request Access module unit test.

---
+++ client/src/components/RequestAccess/RequestAccess.test.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import renderComponent from 'utils/testing';
+import configureStore from 'redux-mock-store';
+import RequestAccess from './RequestAccess';
+import { initialRequestAccessState as requestAccess } from '../../redux/reducers/requestAccess.reducers';
+import { initialState as workbench } from '../../redux/reducers/workbench.reducers';
+
+import '@testing-library/jest-dom/extend-expect';
+
+const mockStore = configureStore();
+
+describe('RequestAccess', () => {
+  it('renders portal name withing the module', () => {
+    const store = mockStore({
+      requestAccess,
+      workbench: {
+        ...workbench,
+        portalName: 'Test Portal'
+      },
+    });
+
+    const { getByText } = renderComponent(<RequestAccess />, store);
+    expect(getByText(/Request Access to the Test Portal/)).toBeInTheDocument();
+  });
+});
