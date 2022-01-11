889da9919064c6c231e83c15e15199254b4304c7
rstijerina <r.sal.tijerina@gmail.com>
2022-01-11 16:49:34 -0600

    remove setup_complete from public data context

--- server/portal/apps/public_data/views.py
+++ server/portal/apps/public_data/views.py
@@ -10,7 +10,6 @@ class IndexView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super(IndexView, self).get_context_data(**kwargs)
-        context['setup_complete'] = self.request.user.profile.setup_complete
         context['DEBUG'] = settings.DEBUG
         return context
 
