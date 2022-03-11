db3c08b2baaac651d33578860f5bdef49cbc0f21
rstijerina <r.sal.tijerina@gmail.com>
2022-03-11 10:00:29 -0600

    fix linting

--- client/src/components/DataFiles/DataFilesModals/DataFilesSelectModal.jsx
+++ client/src/components/DataFiles/DataFilesModals/DataFilesSelectModal.jsx
@@ -64,9 +64,13 @@ const DataFilesSelectModal = ({ isOpen, toggle, onSelect }) => {
               Select Input
               <DataFilesSystemSelector
                 operation="select"
-                systemId={(systems.filter((s) => !s.hidden)[0] || modalParams).system}
+                systemId={
+                  (systems.filter((s) => !s.hidden)[0] || modalParams).system
+                }
                 section="modal"
-                excludedSystems={systems.filter((s) => s.api !== 'tapis' || s.hidden).map((s) => s.system)}
+                excludedSystems={systems
+                  .filter((s) => s.api !== 'tapis' || s.hidden)
+                  .map((s) => s.system)}
                 showProjects={showProjects}
               />
             </div>
