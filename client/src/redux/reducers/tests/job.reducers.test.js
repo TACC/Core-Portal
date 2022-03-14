b3b4888623cdd81860705302744fd3196522bda8
Juan Martinez <jmartinez@tacc.utexas.edu>
2022-03-14 15:53:15 -0500

    Job Reducers Unit Test.

---
+++ client/src/redux/reducers/tests/job.reducers.test.js
@@ -0,0 +1,24 @@
+import { jobs, initialState } from "../jobs.reducers";
+import jobDetailFixture from "../../sagas/fixtures/jobdetail.fixture";
+
+describe("Job Detail Reducer", () => {
+  test("Load initial state", () => {
+    expect(jobs(initialState, { type: undefined })).toEqual(initialState);
+  });
+  test("Jobs list update job", () => {
+    const jobOutputLocationPopulated = {
+      ...jobDetailFixture,
+      outputLocation: "//data/files/example",
+    };
+    const jobsListUpdateJobAction = {
+      type: "JOBS_LIST_UPDATE_JOB",
+      payload: jobOutputLocationPopulated,
+    };
+    expect(jobs(initialState, jobsListUpdateJobAction)).toEqual(
+      { ...initialState },
+      {
+        jobOutputLocationPopulated,
+      }
+    );
+  });
+});
