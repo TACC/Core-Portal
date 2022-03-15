a2e88f40d11d90d4f78413e956f76bf7350635f8
Garrett Edmonds <edmondsgarrett@gmail.com>
2022-03-15 12:56:10 -0500

    _common Sidebar UI Patterns example

---
+++ client/src/components/UIPatterns/UIPatternsSidebar/UIPatternsSidebar.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Sidebar } from '_common';
+
+function UIPatternsSidebar() {
+    const sidebarItems = [
+        {to: 'history', iconName: 'trash', children: 'History'},
+        {to: 'allocations', iconName: 'alert', children: 'Allocations'},
+        {to: 'applications', iconName: 'alert', children: 'Disabled', disabled: true},
+        {to: 'ui-patterns', iconName: 'file', children: 'Here'}
+    ]
+    return (
+        <dl>
+            <dd>
+                <Sidebar sidebarItems={sidebarItems}/>
+            </dd>
+        </dl>
+    );
+}
+
+export default UIPatternsSidebar;
