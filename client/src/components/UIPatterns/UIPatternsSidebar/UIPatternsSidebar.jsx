b3902adfe853fc43ff00c99757085560f6339aaf
Garrett Edmonds <edmondsgarrett@gmail.com>
2022-03-15 12:58:11 -0500

    Fixed linting

--- client/src/components/UIPatterns/UIPatternsSidebar/UIPatternsSidebar.jsx
+++ client/src/components/UIPatterns/UIPatternsSidebar/UIPatternsSidebar.jsx
@@ -2,19 +2,24 @@ import React from 'react';
 import { Sidebar } from '_common';
 
 function UIPatternsSidebar() {
-    const sidebarItems = [
-        {to: 'history', iconName: 'trash', children: 'History'},
-        {to: 'allocations', iconName: 'alert', children: 'Allocations'},
-        {to: 'applications', iconName: 'alert', children: 'Disabled', disabled: true},
-        {to: 'ui-patterns', iconName: 'file', children: 'Here'}
-    ]
-    return (
-        <dl>
-            <dd>
-                <Sidebar sidebarItems={sidebarItems}/>
-            </dd>
-        </dl>
-    );
+  const sidebarItems = [
+    { to: 'history', iconName: 'trash', children: 'History' },
+    { to: 'allocations', iconName: 'alert', children: 'Allocations' },
+    {
+      to: 'applications',
+      iconName: 'alert',
+      children: 'Disabled',
+      disabled: true,
+    },
+    { to: 'ui-patterns', iconName: 'file', children: 'Here' },
+  ];
+  return (
+    <dl>
+      <dd>
+        <Sidebar sidebarItems={sidebarItems} />
+      </dd>
+    </dl>
+  );
 }
 
 export default UIPatternsSidebar;
--- client/src/components/_common/Sidebar/Sidebar.jsx
+++ client/src/components/_common/Sidebar/Sidebar.jsx
@@ -6,55 +6,58 @@ import Icon from '_common/Icon';
 import styles from './Sidebar.module.css';
 
 function isNotEmptyString(props, propName, componentName) {
-    if (!props[propName] || props[propName].replace(/ /g, '') === '') {
-      return new Error(`No text passed to ${componentName}. Validation failed.`);
-    }
-    return null;
+  if (!props[propName] || props[propName].replace(/ /g, '') === '') {
+    return new Error(`No text passed to ${componentName}. Validation failed.`);
   }
+  return null;
+}
 
 const SidebarItem = ({ to, iconName, children, disabled }) => {
-    return (
-        <NavItem>
-            <NavLink
-                tag={RRNavLink}
-                to={to}
-                disabled={disabled}
-                className={styles['link']}
-                activeClassName={styles['link--active']}
-            >
-                <div className={`${disabled && styles['disabled']}`}>
-                    <Icon name={iconName} />
-                    <span className={styles['text']}>
-                        {children}
-                    </span>
-                </div>
-            </NavLink>
-        </NavItem>
-    )
-}
+  return (
+    <NavItem>
+      <NavLink
+        tag={RRNavLink}
+        to={to}
+        disabled={disabled}
+        className={styles['link']}
+        activeClassName={styles['link--active']}
+      >
+        <div className={`${disabled && styles['disabled']}`}>
+          <Icon name={iconName} />
+          <span className={styles['text']}>{children}</span>
+        </div>
+      </NavLink>
+    </NavItem>
+  );
+};
 SidebarItem.propTypes = {
-    to: PropTypes.string.isRequired,
-    iconName: PropTypes.string.isRequired,
-    children: isNotEmptyString,
-    disabled: PropTypes.bool
-}
+  to: PropTypes.string.isRequired,
+  iconName: PropTypes.string.isRequired,
+  children: isNotEmptyString,
+  disabled: PropTypes.bool,
+};
 SidebarItem.defaultProps = {
-    disabled: false
-}
+  disabled: false,
+};
 
 const Sidebar = ({ sidebarItems }) => {
-    return (
-        <Nav className={styles['root']} vertical>
-            { sidebarItems.map((item) => (
-            <SidebarItem to={item.to} iconName={item.iconName} disabled={item.disabled} key={item.children}>
-                {item.children}
-            </SidebarItem>
-            ))}
-        </Nav>
-    )
-}
+  return (
+    <Nav className={styles['root']} vertical>
+      {sidebarItems.map((item) => (
+        <SidebarItem
+          to={item.to}
+          iconName={item.iconName}
+          disabled={item.disabled}
+          key={item.children}
+        >
+          {item.children}
+        </SidebarItem>
+      ))}
+    </Nav>
+  );
+};
 Sidebar.propTypes = {
-    sidebarItems: PropTypes.arrayOf(PropTypes.object).isRequired
-}
+  sidebarItems: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
 
 export default Sidebar;
