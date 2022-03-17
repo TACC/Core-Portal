a350f675365e2e2ea5ea41c0d705135954bd4264
Juan Martinez <jmartinez@tacc.utexas.edu>
2022-03-17 11:03:19 -0500

    Linting.

--- client/src/redux/reducers/tests/job.reducers.test.js
+++ client/src/redux/reducers/tests/job.reducers.test.js
@@ -1,18 +1,18 @@
-import { jobs, initialState } from "../jobs.reducers";
-import jobDetailFixture from "../../sagas/fixtures/jobdetail.fixture";
+import { jobs, initialState } from '../jobs.reducers';
+import jobDetailFixture from '../../sagas/fixtures/jobdetail.fixture';
 
-describe("Job Detail Reducer", () => {
-  test("Load initial state", () => {
+describe('Job Detail Reducer', () => {
+  test('Load initial state', () => {
     expect(jobs(initialState, { type: undefined })).toEqual(initialState);
   });
 
-  test("Jobs list update job", () => {
+  test('Jobs list update job', () => {
     const jobOutputLocationPopulated = {
       ...jobDetailFixture,
-      outputLocation: "//data/files/example",
+      outputLocation: '//data/files/example',
     };
     const jobsListUpdateJobAction = {
-      type: "JOBS_LIST_UPDATE_JOB",
+      type: 'JOBS_LIST_UPDATE_JOB',
       payload: { job: jobOutputLocationPopulated },
     };
     const jobsInitialState = { ...initialState, list: [jobDetailFixture] };
