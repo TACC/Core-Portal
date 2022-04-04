55d02bdcd7dfacf4ae2aa49aa9e7546ebcdf0457
Garrett Edmonds <edmondsgarrett@gmail.com>
2022-04-04 14:42:36 -0500

    Updated main Sidebar to use _common Sidebar

--- client/src/components/Sidebar/Sidebar.jsx
+++ client/src/components/Sidebar/Sidebar.jsx
@@ -1,52 +1,12 @@
 import React from 'react';
-import { Nav, NavItem, NavLink } from 'reactstrap';
-import { NavLink as RRNavLink, useRouteMatch } from 'react-router-dom';
+import { useRouteMatch } from 'react-router-dom';
 import { useSelector } from 'react-redux';
-import PropTypes from 'prop-types';
-import { Icon } from '_common';
 import { FeedbackButton } from '../FeedbackForm';
 import * as ROUTES from '../../constants/routes';
 import HistoryBadge from '../History/HistoryBadge';
 import './Sidebar.global.scss'; // XXX: Global stylesheet imported in component
 import styles from './Sidebar.module.css';
-
-/** Navigation list item **/
-const SidebarItem = ({ to, label, iconName, children, disabled }) => {
-  return (
-    <NavItem>
-      <NavLink
-        tag={RRNavLink}
-        to={to}
-        className={styles['link']}
-        activeClassName={styles['link--active']}
-        disabled={disabled}
-      >
-        <div
-          className={`${disabled && styles['disabled']} ${
-            styles['content']
-          } nav-content`}
-        >
-          <Icon name={iconName} />
-          <span className={styles['text']}>{label}</span>
-          {children}
-        </div>
-      </NavLink>
-    </NavItem>
-  );
-};
-
-SidebarItem.propTypes = {
-  to: PropTypes.string.isRequired,
-  label: PropTypes.string.isRequired,
-  iconName: PropTypes.string.isRequired,
-  children: PropTypes.node,
-  disabled: PropTypes.bool,
-};
-
-SidebarItem.defaultProps = {
-  children: null,
-  disabled: false,
-};
+import { Sidebar as CommonSidebar } from '_common';
 
 /** A navigation list for the application */
 const Sidebar = ({ disabled, showUIPatterns, loading }) => {
@@ -58,76 +18,62 @@ const Sidebar = ({ disabled, showUIPatterns, loading }) => {
   const hideDataFiles = useSelector(
     (state) => state.workbench.config.hideDataFiles
   );
-
+  const sidebarItems = [
+    {
+      to: path + ROUTES.DASHBOARD,
+      label: 'Dashboard',
+      iconName: 'dashboard',
+      disabled: disabled,
+    },
+    {
+      to: path + ROUTES.DATA,
+      label: 'Data Files',
+      iconName: 'folder',
+      disabled: disabled,
+      hidden: hideDataFiles,
+    },
+    {
+      to: path + ROUTES.APPLICATIONS,
+      label: 'Applications',
+      iconName: 'applications',
+      disabled: disabled,
+      hidden: hideApps,
+    },
+    {
+      to: path + ROUTES.ALLOCATIONS,
+      label: 'Allocations',
+      iconName: 'allocations',
+      disabled: disabled,
+    },
+    {
+      to: path + ROUTES.HISTORY,
+      label: 'History',
+      iconName: 'history',
+      disabled: disabled,
+      hidden: hideApps,
+      children: <HistoryBadge unread={unread} disabled={disabled} />,
+    },
+    {
+      to: path + ROUTES.UI,
+      label: 'UI Patterns',
+      iconName: 'copy',
+      disabled: disabled,
+      hidden: !showUIPatterns,
+    },
+  ];
+  const addItems = [
+    {
+      className: styles['feedback-nav-item'],
+      children: <FeedbackButton />,
+    },
+  ];
   return (
-    <Nav className={styles['root']} vertical>
-      {!loading && (
-        <>
-          <SidebarItem
-            to={`${path}${ROUTES.DASHBOARD}`}
-            label="Dashboard"
-            iconName="dashboard"
-            disabled={disabled}
-          />
-          {!hideDataFiles && (
-            <SidebarItem
-              to={`${path}${ROUTES.DATA}`}
-              label="Data Files"
-              iconName="folder"
-              disabled={disabled}
-            />
-          )}
-          {!hideApps && (
-            <SidebarItem
-              to={`${path}${ROUTES.APPLICATIONS}`}
-              label="Applications"
-              iconName="applications"
-              disabled={disabled}
-            />
-          )}
-          <SidebarItem
-            to={`${path}${ROUTES.ALLOCATIONS}`}
-            label="Allocations"
-            iconName="allocations"
-            disabled={disabled}
-          />
-          {!hideApps && (
-            <SidebarItem
-              to={`${path}${ROUTES.HISTORY}`}
-              label="History"
-              iconName="history"
-              disabled={disabled}
-            >
-              <HistoryBadge unread={unread} disabled={disabled} />
-            </SidebarItem>
-          )}
-          {showUIPatterns && (
-            <SidebarItem
-              to={`${path}${ROUTES.UI}`}
-              label="UI Patterns"
-              iconName="copy"
-              disabled={disabled}
-            />
-          )}
-        </>
-      )}
-      <NavItem className={styles['feedback-nav-item']}>
-        <FeedbackButton />
-      </NavItem>
-    </Nav>
+    <CommonSidebar
+      sidebarItems={sidebarItems}
+      addItems={addItems}
+      loading={loading}
+    />
   );
-};
-
-Sidebar.propTypes = {
-  disabled: PropTypes.bool,
-  showUIPatterns: PropTypes.bool,
-  loading: PropTypes.bool,
-};
-
-Sidebar.defaultProps = {
-  disabled: false,
-  showUIPatterns: false,
-  loading: true,
 };
 
 export default Sidebar;
