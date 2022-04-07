39cfc0f4612eb10ef1029aedf2af923fb2b2b454
Garrett Edmonds <edmondsgarrett@gmail.com>
2022-04-07 13:57:16 -0500

    Added successful render unit test for _common Sidebar

--- client/src/components/_common/Sidebar/Sidebar.test.js
+++ client/src/components/_common/Sidebar/Sidebar.test.js
@@ -4,11 +4,24 @@ import Sidebar from './Sidebar';
 import { BrowserRouter } from 'react-router-dom';
 
 describe('Sidebar', () => {
+  it('renders sidebar successfully', () => {
+    const sidebarItems = [
+      { to: 'allocations', iconName: 'allocations', label: 'Allocations' },
+      { to: 'history', iconName: 'file', label: 'History' },
+    ];
+    const { queryByTestId } = render(
+      <BrowserRouter>
+        <Sidebar data-testid="sidebar here" sidebarItems={sidebarItems} />
+      </BrowserRouter>
+    );
+    const el = queryByTestId('sidebar here');
+    expect(el).toBeDefined;
+  });
   it('does not render sidebar where one item has no text', () => {
     const sidebarItems = [
-      { to: 'history', iconName: 'file', children: 'History' },
+      { to: 'history', iconName: 'file', label: 'History' },
       { to: 'applications', iconName: 'alert' },
-      { to: 'ui-patterns', iconName: 'trash', children: 'UI Patterns' },
+      { to: 'ui-patterns', iconName: 'trash', label: 'UI Patterns' },
     ];
     const { queryByTestId } = render(
       <BrowserRouter>
