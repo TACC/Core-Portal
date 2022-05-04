bb4e2d51d21032a78d0eed83a0887b87457a4ec8
Wesley Bomar <wbomar@tacc.utexas.edu>
2022-05-04 15:36:57 -0500

    fix(fp-1194): add haystack so it uses ES vars

--- server/conf/cms/secrets.sample.py
+++ server/conf/cms/secrets.sample.py
@@ -39,3 +39,12 @@ ES_AUTH = 'username:password'
 ES_HOSTS = 'http://elasticsearch:9200'
 ES_INDEX_PREFIX = 'cms-dev-{}'
 ES_DOMAIN = 'http://localhost:8000'
+
+HAYSTACK_CONNECTIONS = {
+    'default': {
+        'ENGINE': 'haystack.backends.elasticsearch_backend.ElasticsearchSearchEngine',
+        'URL': ES_HOSTS,
+        'INDEX_NAME': ES_INDEX_PREFIX.format('cms'),
+        'KWARGS': {'http_auth': ES_AUTH}
+    }
+}
