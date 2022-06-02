af2187669781eb0f35b66e10a82fc7a7ce95e2bb
Sal Tijerina <r.sal.tijerina@gmail.com>
2022-06-02 12:32:04 -0500

    737: Use `_common` `<Button>` component in `<UserSearchBar>` component (#647)
    * Use common button component in UserSearchBar
    
    * fix linting

--- client/src/components/Allocations/AllocationsModals/AllocationsTeamViewModal/AllocationsTeamViewModal.jsx
+++ client/src/components/Allocations/AllocationsModals/AllocationsTeamViewModal/AllocationsTeamViewModal.jsx
@@ -132,8 +132,7 @@ const AllocationsTeamViewModal = ({
               <UserSearchbar
                 members={teams[projectId]}
                 onAdd={onAdd}
-                addDisabled={isLoading}
-                searchDisable={isLoading}
+                isLoading={isLoading}
                 onChange={onChange}
                 searchResults={search.results}
                 placeholder=""
