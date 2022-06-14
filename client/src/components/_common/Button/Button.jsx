28aa67d665c0ac98c4ea339848d948a7ded24b5a
Wesley B <62723358+wesleyboar@users.noreply.github.com>
2022-06-14 11:50:28 -0500

    FP-1650: Test Cases for _common/Button (#640)
    * test(fp-1650): add test cases, add prop value maps
    
    * chore(fp-1650): prettier fix
    
    * fix(fp-1650): don't fail on empty className
    
    Button does not need to have a class. This msitake came form me in #631.
    
    * fix(fp-1650): dataTestid default, '' → undefined
    
    Also changed instances in two other components.
    
    * fix(fp-1650): remove unused function
    
    * fix(fp-1650): fixes from tup-ui PR
    
    Source: https://github.com/TACC/tup-ui/pull/7/files#diff-7c7d6d82c4485c00f402e951eb76d77bfad3a2f3b567fa9f2330eb47a85f814a
    
    * Revert "fix(fp-1650): fixes from tup-ui PR"
    
    This reverts commit e80f9de1af9160ff8f73bfe906271957b0908d69.

--- client/src/components/_common/Button/Button.jsx
+++ client/src/components/_common/Button/Button.jsx
@@ -5,9 +5,22 @@ import Icon from '../Icon';
 import styles from './Button.module.css';
 import LoadingSpinner from '_common/LoadingSpinner';
 
-export const TYPES = ['', 'primary', 'secondary', 'tertiary', 'active', 'link'];
+export const TYPE_MAP = {
+  primary: 'primary',
+  secondary: 'secondary',
+  tertiary: 'tertiary',
+  active: 'is-active',
+  link: 'as-link',
+};
+export const TYPES = [''].concat(Object.keys(TYPE_MAP));
 
-export const SIZES = ['', 'short', 'medium', 'long', 'small'];
+export const SIZE_MAP = {
+  short: 'width-short',
+  medium: 'width-medium',
+  long: 'width-long',
+  small: 'size-small',
+};
+export const SIZES = [''].concat(Object.keys(SIZE_MAP));
 
 export const ATTRIBUTES = ['button', 'submit', 'reset'];
 
@@ -25,6 +38,7 @@ const Button = ({
   iconNameAfter,
   type,
   size,
+  dataTestid,
   disabled,
   onClick,
   attr,
@@ -65,38 +79,18 @@ const Button = ({
   }
   /* eslint-enable no-console */
 
-  const buttonRootClass = styles['root'];
-
-  let buttonTypeClass;
-  if (type === 'link') {
-    buttonTypeClass = styles['as-link'];
-  } else if (['primary', 'secondary', 'tertiary', 'active'].includes(type)) {
-    buttonTypeClass = styles[`${type}`];
-  } else if (type === '') {
-    buttonTypeClass = type;
-  }
-
-  let buttonSizeClass;
-  if (size === 'small') {
-    buttonSizeClass = styles['size-small'];
-  } else {
-    buttonSizeClass = styles[`width-${size}`];
-  }
-
-  const buttonLoadingClass = isLoading ? styles['loading'] : '';
-
   return (
     <button
       className={`
-        ${buttonRootClass}
-        ${buttonTypeClass}
-        ${buttonSizeClass}
-        ${buttonLoadingClass}
-        ${className}
+        ${styles['root']}
+        ${TYPE_MAP[type] ? styles[TYPE_MAP[type]] : ''}
+        ${SIZE_MAP[size] ? styles[SIZE_MAP[size]] : ''}
+        ${isLoading ? styles['loading'] : ''}
       `}
       disabled={disabled || isLoading}
       type={attr}
       onClick={onclick}
+      data-testid={dataTestid}
     >
       {isLoading && (
         <LoadingSpinner
@@ -107,15 +101,19 @@ const Button = ({
       {iconNameBefore ? (
         <Icon
           name={iconNameBefore}
+          dataTestid="icon-before"
           className={iconNameBefore ? styles['icon--before'] : ''}
         />
       ) : (
         ''
       )}
-      <span className={styles['text']}>{children}</span>
+      <span className={styles['text']} data-testid="text">
+        {children}
+      </span>
       {iconNameAfter && (
         <Icon
           name={iconNameAfter}
+          dataTestid="icon-after"
           className={iconNameAfter ? styles['icon--after'] : ''}
         />
       )}
@@ -124,11 +122,12 @@ const Button = ({
 };
 Button.propTypes = {
   children: isNotEmptyString,
-  className: isNotEmptyString,
+  className: PropTypes.string,
   iconNameBefore: PropTypes.string,
   iconNameAfter: PropTypes.string,
   type: PropTypes.oneOf(TYPES),
   size: PropTypes.oneOf(SIZES),
+  dataTestid: PropTypes.string,
   disabled: PropTypes.bool,
   onClick: PropTypes.func,
   attr: PropTypes.oneOf(ATTRIBUTES),
@@ -140,6 +139,7 @@ Button.defaultProps = {
   iconNameAfter: '',
   type: 'secondary',
   size: '', // unless `type="link", defaults to `short` after `propTypes`
+  dataTestid: undefined,
   disabled: false,
   onClick: null,
   attr: 'button',
