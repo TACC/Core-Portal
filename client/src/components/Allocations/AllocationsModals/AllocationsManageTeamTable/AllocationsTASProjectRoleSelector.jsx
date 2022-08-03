19df37cac107950c0f561cd625cacbb5a653633f
rstijerina <r.sal.tijerina@gmail.com>
2022-08-03 13:03:46 -0500

    use common button

--- client/src/components/Allocations/AllocationsModals/AllocationsManageTeamTable/AllocationsTASProjectRoleSelector.jsx
+++ client/src/components/Allocations/AllocationsModals/AllocationsManageTeamTable/AllocationsTASProjectRoleSelector.jsx
@@ -1,22 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import { useQuery, useMutation } from 'react-query';
-import { useDispatch, useSelector } from 'react-redux';
+import { useMutation } from 'react-query';
+import { useDispatch } from 'react-redux';
 import DropdownSelector from '_common/DropdownSelector';
-import { Button } from 'reactstrap';
-import styles from '../../../DataFiles/DataFilesProjectMembers/DataFilesProjectMembers.module.scss';
-import LoadingSpinner from '_common/LoadingSpinner';
 import {fetchUtil} from 'utils/fetchUtil';
+import { Button } from '_common';
+import styles from './AllocationsManageTeamTable.module.scss';
 
-// const getProjectRole = async (projectId, username) => {
-//   if (!projectId || !username) return {};
-//   const url = `/api/projects/${projectId}/system-role/${username}/`;
-//   const request = await fetch(url, {
-//     headers: { 'X-CSRFToken': Cookies.get('csrftoken') },
-//     credentials: 'same-origin',
-//   });
-//   const data = await request.json();
-//   return data;
-// };
 
 const setProjectRole = async (projectId, userId, role) => {
   const roleMap = {
@@ -55,8 +44,6 @@ const TASProjectRoleSelector = ({ projectId, userId, role }) => {
   const [selectedRole, setSelectedRole] = useState(role);
   useEffect(() => setSelectedRole(role), [role]);
 
-  if (isMutating)
-    return <LoadingSpinner placement="inline" />;
   if (error) return <span>Error</span>;
 
   return (
@@ -64,15 +51,18 @@ const TASProjectRoleSelector = ({ projectId, userId, role }) => {
       <DropdownSelector
         value={selectedRole}
         onChange={(e) => setSelectedRole(e.target.value)}
+        disabled={isMutating}
       >
         <option value="Standard">Member</option>
         <option value="Delegate">Allocation Manager</option>
       </DropdownSelector>
       {role !== selectedRole && (
         <Button
-          style={{ marginLeft: '5px' }}
-          className={styles['ownership-button']}
+          type="secondary"
           onClick={() => setProjectRole(selectedRole)}
+          size="small"
+          isLoading={isMutating}
+          className={styles['action-button']}
         >
           Update
         </Button>
