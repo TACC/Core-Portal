a072f475d1ed921cc55cdcab769ed08052c566bd
Sal Tijerina <r.sal.tijerina@gmail.com>
2022-10-10 16:23:24 -0500

    task/TV3-50: Tapis v3 apps operations (#711)
    * Tapis v3 auth
    
    * rename agave_oauth to tapis_oauth; remove token_type and scope
    
    * add migration
    
    * fix casing
    
    * fix param
    
    * fix max_length, request param, given_name
    
    * add update method to token model
    
    * upgrade tapipy
    
    * fix tests
    
    * fix linting
    
    * add longer test token
    
    * fix tests
    
    * stop patcher; fix setting name
    
    * fix oauth test url
    
    * f strings for auth
    
    * add apps hook and handler
    
    * pin image tag for v3 development
    
    * update poetry.lock
    
    * parity with tv3 feat
    
    * add tapis result serializer
    
    * remove unused app functions; add license type processing
    
    * Task/TV3-50-add-some-testing-fixes (#715)
    
    * Comment out unused url
    
    * Rename tests to clarify that they are for the tray
    
    * Skip two tests that are failing due to refactor
    
    * Add get app auth test
    
    * Add placeholder for use of license in job post
    
    Job posting has a call to refactored function where it wants to get license info from appId.  breaks job submission so adding a dummy function for the time being.
    
    * Improve reason for skipping tests
    
    * Fix mock
    
    * Fix linting errors
    
    * Remove unused path
    
    * linting
    
    Co-authored-by: Nathan Franklin <nathan.franklin@gmail.com>

--- server/portal/apps/workspace/api/urls.py
+++ server/portal/apps/workspace/api/urls.py
@@ -6,15 +6,12 @@ from portal.apps.workspace.api import views
 
 app_name = 'workspace_api'
 urlpatterns = [
-    # url(r'^(?P<service>[a-z]+?)/$', views.call_api, name='call_api'),
     url(r'^apps/?', views.AppsView.as_view()),
-    url(r'^meta/?', views.MetadataView.as_view()),
     url(
         r'^jobs/(?P<job_uuid>[0-9a-fA-F]{8}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{12}\-[0-9a-fA-F]{3})/history/?$',
         views.JobHistoryView.as_view()
     ),
     url(r'^jobs/?', views.JobsView.as_view()),
-    url(r'^monitors/?', views.MonitorsView.as_view()),
     url(r'^systems/?', views.SystemsView.as_view()),
     url(r'^tray/?', views.AppsTrayView.as_view())
 
