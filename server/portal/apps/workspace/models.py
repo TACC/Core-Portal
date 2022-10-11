bd6863e780f321acb6cc2b93c31219e06b6dfd6a
Ian Park <contact@duckonomy.com>
2022-10-11 14:43:53 -0500

    Job submission backend test.

--- server/portal/apps/workspace/models.py
+++ server/portal/apps/workspace/models.py
@@ -4,22 +4,22 @@ from django.utils import timezone
 
 
 # NOTE: Unneeded for v3
-# class JobSubmission(models.Model):
-#     """Job Submission
+class JobSubmission(models.Model):
+    """Job Submission
 
-#     Used for tracking jobs that originate from this portal for filtering purposes
-#     """
-#     user = models.ForeignKey(
-#         settings.AUTH_USER_MODEL,
-#         related_name="+",
-#         on_delete=models.CASCADE
-#     )
+    Used for tracking jobs that originate from this portal for filtering purposes
+    """
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        related_name="+",
+        on_delete=models.CASCADE
+    )
 
-#     # Timestamp for event
-#     time = models.DateTimeField(default=timezone.now)
+    # Timestamp for event
+    time = models.DateTimeField(default=timezone.now)
 
-#     # ID of job returned from Agave
-#     jobId = models.CharField(max_length=300)
+    # ID of job returned from Agave
+    jobId = models.CharField(max_length=300)
 
 
 class AppTrayCategory(models.Model):
