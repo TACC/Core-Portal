afe18df375aa433291ee068cb8f42b35ec44851a
Ian Park <contact@duckonomy.com>
2022-11-30 14:44:02 -0600

    Task/TV3-55: Job (re)submission backend (#734)
    * Resumit job in v3.
    
    * Handle cancel job explicitly.
    
    * Update server/portal/apps/workspace/api/views.py
    
    Co-authored-by: Nathan Franklin <nathan.franklin@gmail.com>
    
    * Revert dot uuid.
    
    * Fix test for tapisresult.
    
    Co-authored-by: Nathan Franklin <nathan.franklin@gmail.com>

--- server/portal/apps/workspace/api/views.py
+++ server/portal/apps/workspace/api/views.py
@@ -145,30 +145,38 @@ class JobsView(BaseApiView):
         )
 
     def post(self, request, *args, **kwargs):
-        agave = request.user.tapis_oauth.client
+        tapis = request.user.tapis_oauth.client
         job_post = json.loads(request.body)
-        job_id = job_post.get('job_id')
+        job_uuid = job_post.get('job_uuid')
         job_action = job_post.get('action')
 
-        if job_id and job_action:
-            # resubmit job
+        if job_uuid and job_action:
             if job_action == 'resubmit':
-                METRICS.info("user:{} is resubmitting job id:{}".format(request.user.username, job_id))
-            # cancel job / stop job
+                METRICS.info("user:{} is resubmitting job uuid:{}".format(request.user.username, job_uuid))
+                data = tapis.jobs.resubmitJob(jobUuid=job_uuid)
+
+                job = JobSubmission.objects.create(
+                    user=request.user,
+                    jobId=data.uuid
+                )
+                job.save()
+            elif job_action == 'cancel':
+                METRICS.info("user:{} is canceling/stopping job uuid:{}".format(request.user.username, job_uuid))
+                data = tapis.jobs.cancelJob(jobUuid=job_uuid)
             else:
-                METRICS.info("user:{} is canceling/stopping job id:{}".format(request.user.username, job_id))
+                raise ApiException("user:{} is trying to run an unsupported job action: {} for job uuid: {}".format(
+                    request.user.username,
+                    job_action,
+                    job_uuid
+                ), status=400)
 
-            data = agave.jobs.manage(jobId=job_id, body={"action": job_action})
-
-            if job_action == 'resubmit':
-                if "id" in data:
-                    job = JobSubmission.objects.create(
-                        user=request.user,
-                        jobId=data["id"]
-                    )
-                    job.save()
-
-            return JsonResponse({"response": data})
+            return JsonResponse(
+                {
+                    'status': 200,
+                    'response': data,
+                },
+                encoder=BaseTapisResultSerializer
+            )
         # submit job
         elif job_post:
             METRICS.info("user:{} is submitting job:{}".format(request.user.username, job_post))
@@ -249,7 +257,7 @@ class JobsView(BaseApiView):
                                       for param in job_post['parameters']
                                       if param in [p['id'] for p in app.parameters]}
 
-            response = agave.jobs.submit(body=job_post)
+            response = tapis.jobs.submit(body=job_post)
 
             if "id" in response:
                 job = JobSubmission.objects.create(
