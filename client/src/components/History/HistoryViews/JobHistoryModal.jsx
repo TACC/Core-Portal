b86a27b49a03e8508febaf6a87df5c3552c4cc8b
Ian Park <contact@duckonomy.com>
2023-01-06 11:37:33 -0600

    Refactor JobHistoryModal.

--- client/src/components/History/HistoryViews/JobHistoryModal.jsx
+++ client/src/components/History/HistoryViews/JobHistoryModal.jsx
@@ -62,213 +62,147 @@ function JobHistoryContent({
   toggle,
   version,
 }) {
-  if (version === 'v3') {
-    const dispatch = useDispatch();
-    const outputLocation = useSelector((state) => {
-      return state.jobs.list.find((job) => job.uuid === jobDetails.uuid)
-        .outputLocation;
-    });
-    const created = formatDateTime(new Date(jobDetails.created));
-    const lastUpdated = formatDateTime(new Date(jobDetails.lastUpdated));
-    const hasFailedStatus = jobDetails.status === 'FAILED';
-    const hideDataFiles = useSelector(
-      (state) => state.workbench.config.hideDataFiles
-    );
+  const dispatch = useDispatch();
+  const outputLocation =
+    version === 'v3'
+      ? useSelector((state) => {
+          return state.jobs.list.find((job) => job.uuid === jobDetails.uuid)
+            .outputLocation;
+        })
+      : `${jobDetails.archiveSystem}/${jobDetails.archivePath}`;
+  const created = formatDateTime(new Date(jobDetails.created));
+  const lastUpdated = formatDateTime(new Date(jobDetails.lastUpdated));
+  const hasFailedStatus = jobDetails.status === 'FAILED';
+  const hideDataFiles = useSelector(
+    (state) => state.workbench.config.hideDataFiles
+  );
 
-    const statusDataObj = {
-      Submitted: created,
-      [`${getStatusText(jobDetails.status)}`]: lastUpdated,
-    };
-    const inputAndParamsDataObj = {
-      ...reduceInputParameters(jobDisplay.inputs),
-      ...reduceInputParameters(jobDisplay.parameters),
-    };
-    const configDataObj = {};
-    const outputDataObj = {
-      'Job Name': jobName,
-      'Output Location': outputLocation,
-    };
+  const statusDataObj = {
+    Submitted: created,
+    [`${getStatusText(jobDetails.status)}`]: lastUpdated,
+  };
 
-    statusDataObj[hasFailedStatus ? 'Failure Report' : 'Last Status Message'] =
-      (
-        <Expand
-          detail={hasFailedStatus ? 'Last Status Message' : 'System Output'}
-          message={<pre>${jobDetails.lastMessage}</pre>}
-        />
-      );
+  const inputAndParamsDataObj = {
+    ...reduceInputParameters(jobDisplay.inputs),
+    ...reduceInputParameters(jobDisplay.parameters),
+  };
+  const configDataObj = {};
+  const outputDataObj = {
+    'Job Name': jobName,
+    'Output Location': outputLocation,
+  };
 
-    const resubmitJob = () => {
-      dispatch({
-        type: 'SUBMIT_JOB',
-        payload: {
-          job_uuid: jobDetails.uuid,
-          action: 'resubmit',
-          onSuccess: {
-            type: 'GET_JOBS',
-            params: {
-              offset: 0,
-            },
+  statusDataObj[hasFailedStatus ? 'Failure Report' : 'Last Status Message'] = (
+    <Expand
+      detail={hasFailedStatus ? 'Last Status Message' : 'System Output'}
+      message={
+        <pre>
+          $
+          {version === 'v3'
+            ? jobDetails.lastMessage
+            : jobDetails.lastStatusMessage}
+        </pre>
+      }
+    />
+  );
+
+  const resubmitJob = () => {
+    dispatch({
+      type: 'SUBMIT_JOB',
+      payload: {
+        job_uuid: jobDetails.uuid,
+        action: 'resubmit',
+        onSuccess: {
+          type: 'GET_JOBS',
+          params: {
+            offset: 0,
           },
         },
-      });
-      toggle();
-    };
+      },
+    });
+    toggle();
+  };
 
-    if ('queue' in jobDisplay) {
-      configDataObj.Queue = jobDisplay.queue;
-    }
+  if ('queue' in jobDisplay) {
+    configDataObj.Queue = jobDisplay.queue;
+  }
 
+  if (version === 'v3') {
     configDataObj['Max Minutes'] = jobDetails.maxMinutes;
+  } else {
+    configDataObj['Max Hours'] = jobDetails.maxHours;
+  }
 
-    if ('coresPerNode' in jobDisplay) {
-      configDataObj['Cores On Each Node'] = jobDisplay.coresPerNode;
-    }
-    if ('nodeCount' in jobDisplay) {
-      configDataObj['Node Count'] = jobDisplay.nodeCount;
-    }
-    if ('allocation' in jobDisplay) {
-      configDataObj.Allocation = jobDisplay.allocation;
-    }
+  if ('coresPerNode' in jobDisplay) {
+    configDataObj['Cores On Each Node'] = jobDisplay.coresPerNode;
+  }
 
-    if (jobDetails.status !== 'FINISHED') {
+  if ('processorsPerNode' in jobDisplay && version === 'v2') {
+    configDataObj['Processors On Each Node'] = jobDisplay.processorsPerNode;
+  }
+
+  if ('nodeCount' in jobDisplay) {
+    configDataObj['Node Count'] = jobDisplay.nodeCount;
+  }
+  if ('allocation' in jobDisplay) {
+    configDataObj.Allocation = jobDisplay.allocation;
+  }
+
+  if (jobDetails.status !== 'FINISHED') {
+    if (version === 'v3') {
       configDataObj['Execution Directory'] = jobDetails.execSystemExecDir;
       configDataObj['Input Directory'] = jobDetails.execSystemInputDir;
       configDataObj['Output Directory'] = jobDetails.execSystemOutputDir;
-    }
-
-    const isTerminalState =
-      jobDetails.status === 'FINISHED' ||
-      jobDetails.status === 'FAILED' ||
-      jobDetails.status === 'STOPPED';
-
-    const data = {
-      Status: <DescriptionList data={statusDataObj} />,
-      Inputs: <DescriptionList data={inputAndParamsDataObj} />,
-      Configuration: <DescriptionList data={configDataObj} />,
-      Output: <DescriptionList data={outputDataObj} />,
-    };
-
-    return (
-      <>
-        <div className={`${styles['left-panel']} ${styles['panel-content']}`}>
-          <DescriptionList
-            density="compact"
-            data={{
-              Output: !hideDataFiles && (
-                <DataFilesLink
-                  path={outputLocation}
-                  disabled={!isOutputState(jobDetails.status)}
-                >
-                  View in Data Files
-                </DataFilesLink>
-              ),
-            }}
-          />
-          {isTerminalState && (
-            <Button
-              type="primary"
-              attr="submit"
-              className={styles['submit-button']}
-              onClick={resubmitJob}
-            >
-              Resubmit Job
-            </Button>
-          )}
-        </div>
-        <DescriptionList
-          className={`${styles['right-panel']} ${styles['panel-content']}`}
-          data={data}
-        />
-      </>
-    );
-  } else {
-    const dispatch = useDispatch();
-    const outputPath = `${jobDetails.archiveSystem}/${jobDetails.archivePath}`;
-    const created = formatDateTime(new Date(jobDetails.created));
-    const lastUpdated = formatDateTime(new Date(jobDetails.lastUpdated));
-    const hasFailedStatus = jobDetails.status === 'FAILED';
-    const hideDataFiles = useSelector(
-      (state) => state.workbench.config.hideDataFiles
-    );
-
-    const statusDataObj = {
-      Submitted: created,
-      [`${getStatusText(jobDetails.status)}`]: lastUpdated,
-    };
-
-    const inputAndParamsDataObj = {
-      ...reduceInputParameters(jobDisplay.inputs),
-      ...reduceInputParameters(jobDisplay.parameters),
-    };
-    const configDataObj = {};
-    const outputDataObj = {
-      'Job Name': jobName,
-      'Output Location': outputPath,
-    };
-
-    statusDataObj[hasFailedStatus ? 'Failure Report' : 'Last Status Message'] =
-      (
-        <Expand
-          detail={hasFailedStatus ? 'Last Status Message' : 'System Output'}
-          message={<pre>${jobDetails.lastStatusMessage}</pre>}
-        />
-      );
-
-    if ('queue' in jobDisplay) {
-      configDataObj.Queue = jobDisplay.queue;
-    }
-
-    configDataObj['Max Hours'] = jobDetails.maxHours;
-
-    if ('processorsPerNode' in jobDisplay) {
-      configDataObj['Processors On Each Node'] = jobDisplay.processorsPerNode;
-    }
-    if ('nodeCount' in jobDisplay) {
-      configDataObj['Node Count'] = jobDisplay.nodeCount;
-    }
-    if ('allocation' in jobDisplay) {
-      configDataObj.Allocation = jobDisplay.allocation;
-    }
-
-    if (jobDetails.status !== 'FINISHED')
+    } else {
       configDataObj['Temporary Working Directory'] = jobDetails.workPath;
-
-    const isTerminalState =
-      jobDetails.status === 'FINISHED' ||
-      jobDetails.status === 'FAILED' ||
-      jobDetails.status === 'STOPPED';
-
-    const data = {
-      Status: <DescriptionList data={statusDataObj} />,
-      Inputs: <DescriptionList data={inputAndParamsDataObj} />,
-      Configuration: <DescriptionList data={configDataObj} />,
-      Output: <DescriptionList data={outputDataObj} />,
-    };
-
-    return (
-      <>
-        <div className={`${styles['left-panel']} ${styles['panel-content']}`}>
-          <DescriptionList
-            density="compact"
-            data={{
-              Output: !hideDataFiles && (
-                <DataFilesLink
-                  path={outputPath}
-                  disabled={!isOutputState(jobDetails.status)}
-                >
-                  View in Data Files
-                </DataFilesLink>
-              ),
-            }}
-          />
-        </div>
-        <DescriptionList
-          className={`${styles['right-panel']} ${styles['panel-content']}`}
-          data={data}
-        />
-      </>
-    );
+    }
   }
+
+  const isTerminalState =
+    jobDetails.status === 'FINISHED' ||
+    jobDetails.status === 'FAILED' ||
+    jobDetails.status === 'STOPPED';
+
+  const data = {
+    Status: <DescriptionList data={statusDataObj} />,
+    Inputs: <DescriptionList data={inputAndParamsDataObj} />,
+    Configuration: <DescriptionList data={configDataObj} />,
+    Output: <DescriptionList data={outputDataObj} />,
+  };
+
+  return (
+    <>
+      <div className={`${styles['left-panel']} ${styles['panel-content']}`}>
+        <DescriptionList
+          density="compact"
+          data={{
+            Output: !hideDataFiles && (
+              <DataFilesLink
+                path={outputLocation}
+                disabled={!isOutputState(jobDetails.status)}
+              >
+                View in Data Files
+              </DataFilesLink>
+            ),
+          }}
+        />
+        {isTerminalState && version === 'v3' && (
+          <Button
+            type="primary"
+            attr="submit"
+            className={styles['submit-button']}
+            onClick={resubmitJob}
+          >
+            Resubmit Job
+          </Button>
+        )}
+      </div>
+      <DescriptionList
+        className={`${styles['right-panel']} ${styles['panel-content']}`}
+        data={data}
+      />
+    </>
+  );
 }
 
 JobHistoryContent.propTypes = {
@@ -285,148 +219,84 @@ JobHistoryContent.defaultProps = {
   jobDisplay: {},
 };
 
-// TODOV3: Duplicate code, but will be removed once we drop support for V2 jobs
 function JobHistoryModal({ uuid, version }) {
-  if (version === 'v3') {
-    loading = useSelector((state) => state.jobDetail.loading);
-    loadingError = useSelector((state) => state.jobDetail.loadingError);
-    const { job, display } = useSelector((state) => state.jobDetail);
-    const { state } = useLocation();
-
-    let jobName = job ? job.name : placeHolder;
-
-    if (jobName === placeHolder) {
-      if (state && state.jobName) {
-        jobName = state.jobName;
-      }
+  const { loading, loadingError, job, display } = useSelector((state) => {
+    if (version === 'v3') {
+      const jobv2 = state.jobsv2.list.find((job) => job.id === uuid);
+      return {
+        loading: false,
+        loadingError: null,
+        job: jobv2,
+        display: jobv2.display,
+      };
+    } else {
+      const jobv3 = state.jobDetail;
+      return {
+        loading: jobv3.loading,
+        loadingError: jobv3.loadingError,
+        job: jobv3.job,
+        display: jobv3.display,
+      };
     }
+  });
+  let jobName = job ? job.name : placeHolder;
 
-    const history = useHistory();
-    const close = () => {
-      history.push(`${ROUTES.WORKBENCH}${ROUTES.HISTORY}${ROUTES.JOBS}`, {
-        fromJobHistoryModal: true,
-      });
-    };
-
-    const headerData = {
-      'Job UUID': uuid,
-      Application: display ? display.applicationName : placeHolder,
-      System: display ? display.systemName : placeHolder,
-    };
-
-    return (
-      <Modal
-        isOpen
-        className={`${styles.root} job-history-modal`}
-        toggle={close}
-        size="lg"
-      >
-        <ModalHeader
-          className={styles.header}
-          toggle={close}
-          charCode="&#xe912;"
-        >
-          <div className="d-inline-block text-truncate">{jobName}</div>
-          <DescriptionList
-            data={headerData}
-            direction="horizontal"
-            className={styles['header-details']}
-          />
-        </ModalHeader>
-
-        <ModalBody>
-          <div className={styles['modal-body-container']}>
-            {loading && <LoadingSpinner />}
-            {loadingError && (
-              <Message type="warn" className={styles.error}>
-                Unable to retrieve job information.
-              </Message>
-            )}
-            {!loading && !loadingError && job && (
-              <JobHistoryContent
-                jobName={jobName}
-                jobDetails={job}
-                jobDisplay={display}
-                toggle={close}
-                version={version}
-              />
-            )}
-          </div>
-        </ModalBody>
-      </Modal>
-    );
-  } else {
-    const loading = false;
-    const loadingError = null;
-    const job = useSelector((state) => {
-      return state.jobsv2.list.find((job) => job.id === uuid);
-    });
-    const display = job.display;
-
-    const { state } = useLocation();
-
-    let jobName = job ? job.name : placeHolder;
-
-    if (jobName === placeHolder) {
-      if (state && state.jobName) {
-        jobName = state.jobName;
-      }
+  if (jobName === placeHolder) {
+    if (state && state.jobName) {
+      jobName = state.jobName;
     }
-
-    const history = useHistory();
-    const close = () => {
-      history.push(`${ROUTES.WORKBENCH}${ROUTES.HISTORY}${ROUTES.JOBSV2}`, {
-        fromJobHistoryModal: true,
-      });
-    };
-
-    const headerData = {
-      'Job ID': uuid,
-      Application: display ? display.applicationName : placeHolder,
-      System: display ? display.systemName : placeHolder,
-    };
-
-    return (
-      <Modal
-        isOpen
-        className={`${styles.root} job-history-modal`}
-        toggle={close}
-        size="lg"
-      >
-        <ModalHeader
-          className={styles.header}
-          toggle={close}
-          charCode="&#xe912;"
-        >
-          <div className="d-inline-block text-truncate">{jobName}</div>
-          <DescriptionList
-            data={headerData}
-            direction="horizontal"
-            className={styles['header-details']}
-          />
-        </ModalHeader>
-
-        <ModalBody>
-          <div className={styles['modal-body-container']}>
-            {loading && <LoadingSpinner />}
-            {loadingError && (
-              <Message type="warn" className={styles.error}>
-                Unable to retrieve job information.
-              </Message>
-            )}
-            {!loading && !loadingError && job && (
-              <JobHistoryContent
-                jobName={jobName}
-                jobDetails={job}
-                jobDisplay={display}
-                toggle={close}
-              />
-            )}
-          </div>
-        </ModalBody>
-      </Modal>
-    );
   }
+
+  const history = useHistory();
+  const close = () => {
+    history.push(`${ROUTES.WORKBENCH}${ROUTES.HISTORY}${ROUTES.JOBS}`, {
+      fromJobHistoryModal: true,
+    });
+  };
+
+  const headerData = {
+    ...(version === 'v3' ? { 'Job UUID': uuid } : { 'Job ID': uuid }),
+    Application: display ? display.applicationName : placeHolder,
+    System: display ? display.systemName : placeHolder,
+  };
+
+  return (
+    <Modal
+      isOpen
+      className={`${styles.root} job-history-modal`}
+      toggle={close}
+      size="lg"
+    >
+      <ModalHeader className={styles.header} toggle={close} charCode="&#xe912;">
+        <div className="d-inline-block text-truncate">{jobName}</div>
+        <DescriptionList
+          data={headerData}
+          direction="horizontal"
+          className={styles['header-details']}
+        />
+      </ModalHeader>
+
+      <ModalBody>
+        <div className={styles['modal-body-container']}>
+          {loading && <LoadingSpinner />}
+          {loadingError && (
+            <Message type="warn" className={styles.error}>
+              Unable to retrieve job information.
+            </Message>
+          )}
+          {!loading && !loadingError && job && (
+            <JobHistoryContent
+              jobName={jobName}
+              jobDetails={job}
+              jobDisplay={display}
+              toggle={close}
+              version={version}
+            />
+          )}
+        </div>
+      </ModalBody>
+    </Modal>
+  );
 }
 
 JobHistoryModal.propTypes = {
