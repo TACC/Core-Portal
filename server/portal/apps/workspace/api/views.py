02f54ccec1932e23c422f2e6154583efbb8325f2
Shayan Khan <shayanaijaz@gmail.com>
2023-01-17 15:40:46 -0600

    update comment

--- server/portal/apps/workspace/api/views.py
+++ server/portal/apps/workspace/api/views.py
@@ -257,7 +257,7 @@ class JobsView(BaseApiView):
             portal_name = settings.PORTAL_NAMESPACE
             job_post['tags'] = job_post.get('tags', []) + [portal_name]
 
-            # ttlMinutes of 0 corresponds to max default (1 month)
+            # ttlMinutes of 0 corresponds to max default (1 week)
             job_post["subscriptions"] = [
                {
                     "description": "Portal job status notification",
