ef8d74d098882b776accf8756ee5371b3a871dec
Nathan Franklin <nathan.franklin@gmail.com>
2023-03-08 22:12:30 -0600

    Update fixture

--- client/src/components/Applications/AppForm/fixtures/AppForm.allocations.fixture.js
+++ client/src/components/Applications/AppForm/fixtures/AppForm.allocations.fixture.js
@@ -1,15 +1,10 @@
 const allocationsFixture = {
   hosts: {
-    'ls5.tacc.utexas.edu': ['TACC-ACI'],
     'ls6.tacc.utexas.edu': ['TACC-ACI'],
-    'wrangler.tacc.utexas.edu': ['TACC-ACI'],
     'data.tacc.utexas.edu': ['TACC-ACI'],
     'ranch.tacc.utexas.edu': ['TACC-ACI'],
-    'rodeo.tacc.utexas.edu': ['TACC-ACI'],
-    'hikari.tacc.utexas.edu': ['TACC-ACI'],
     'stampede2.tacc.utexas.edu': ['TACC-ACI'],
     'maverick2.tacc.utexas.edu': ['TACC-ACI'],
-    'longhorn.tacc.utexas.edu': ['TACC-ACI'],
     'frontera.tacc.utexas.edu': ['TACC-ACI'],
   },
   portal_alloc: 'TACC-ACI',
