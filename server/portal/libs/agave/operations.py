dc9b383d37e4864e992ec56322ac57bd7beb4357
Jake Rosenberg <jrosenberg@MacBook-Pro.lan>
2023-03-13 16:06:14 -0500

    defer to settings for SUPPPORTED_BRAINMAP_PREVIEW_EXTS

--- server/portal/libs/agave/operations.py
+++ server/portal/libs/agave/operations.py
@@ -493,7 +493,7 @@ def preview(client, system, path, href, max_uses=3, lifetime=600, **kwargs):
         file_type = 'text'
     elif file_ext in settings.SUPPORTED_IMAGE_PREVIEW_EXTS:
         file_type = 'image'
-    elif (file_name.endswith('.nii') or file_name.endswith('.nii.gz')):
+    elif any([ext for ext in settings.SUPPORTED_BRAINMAP_PREVIEW_EXTS if file_name.endswith(ext)]):
         file_type = 'brainmap'
     elif file_ext in settings.SUPPORTED_OBJECT_PREVIEW_EXTS:
         file_type = 'object'
