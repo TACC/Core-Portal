ff4593d68f617893bedadeb23872cb93867e95e3
Nathan Franklin <nathan.franklin@gmail.com>
2023-04-05 15:03:50 -0500

    task/TV3-130: manage account changes (#782)
    * Update layout/text of manage account
    
    * Remove unused classes
    
    * Update link for Change Password
    
    * Update what profile fields to show
    
    * Update link for editing profile
    
    * Add noreffer
    
    * Fix linting
    
    * Remove test and update fields in tests
    
    * Remove no longer used components/redux and related tests
    
    * Fix client-side linting errors
    
    * Fix server-side linting/pep errors
    
    * Add back needed styling used in 3rd party apps modal
    
    * Revert some unused changes
    
    ---------
    
    Co-authored-by: Sal Tijerina <r.sal.tijerina@gmail.com>

--- server/portal/apps/accounts/unit_test.py
+++ server/portal/apps/accounts/unit_test.py
@@ -23,15 +23,6 @@ def tas_client(mocker):
     yield tas_client_mock
 
 
-@pytest.fixture
-def tas_form_fields(mocker):
-    tas = mocker.patch('portal.apps.accounts.form_fields.tas', autospec=True)
-    tas.institutions.return_value = [{"id": "uniId", "name": "My University"}]
-    tas.get_departments.return_value = [{"id": "departmentId", "name": "My Department"}]
-    tas.countries.return_value = [{"id": "countryId", "name": "My Country"}]
-    yield tas
-
-
 def test_profile_data(client, authenticated_user, tas_client, tas_user_history_request):
     response = client.get('/accounts/api/profile/data/')
     assert response.status_code == 200
@@ -47,84 +38,3 @@ def test_profile_data_unexpected(client, authenticated_user, tas_client, tas_use
     response = client.get('/accounts/api/profile/data/')
     assert response.status_code == 500
     assert response.json() == {'message': 'Unable to get profile.'}
-
-
-def test_profile_fields(client, authenticated_user, tas_form_fields):
-    response = client.get('/accounts/api/profile/fields/')
-    assert response.status_code == 200
-
-
-def test_profile_fields_unauthenticated(client):
-    response = client.get('/accounts/api/profile/fields/')
-    assert response.status_code == 302  # redirect to login
-
-
-def test_profile_fields_unexpected_error(client, authenticated_user, tas_form_fields):
-    tas_form_fields.institutions.side_effect = Exception()
-    response = client.get('/accounts/api/profile/fields/')
-    assert response.status_code == 500
-    assert response.json() == {'message': 'Unable to get form fields.'}
-
-
-def test_change_password(client, authenticated_user, tas_client):
-    response = client.put('/accounts/api/profile/change-password/',
-                          content_type="application/json",
-                          data={"newPW": "1234", "currentPW": "abcd"})
-    assert response.status_code == 200
-    assert response.json() == {'completed': True}
-
-
-def test_change_password_unauthenticated(client):
-    response = client.put('/accounts/api/profile/change-password/',
-                          content_type="application/json",
-                          data={"newPW": "1234", "currentPW": "abcd"})
-    assert response.status_code == 302  # redirect to login
-
-
-def test_change_password_incorrect_password(client, authenticated_user, tas_client):
-    tas_client.authenticate.return_value = False
-    response = client.put('/accounts/api/profile/change-password/',
-                          content_type="application/json",
-                          data={"newPW": "1234", "currentPW": "abcd"})
-    assert response.status_code == 401
-    assert response.json() == {'message': 'Incorrect Password'}
-
-
-def test_change_password_but_new_password_is_poor(client, authenticated_user, tas_client):
-    e = Exception(f'Failed password change for user={authenticated_user.username}',
-                  'This password does not meet the password complexity requirements.')
-    tas_client.change_password.side_effect = e
-
-    response = client.put('/accounts/api/profile/change-password/',
-                          content_type="application/json",
-                          data={"newPW": "1234", "currentPW": "abcd"})
-    assert response.status_code == 422
-    assert response.json() == {'message': 'This password does not meet the password complexity requirements.'}
-
-
-def test_change_password_unexpected_error(client, authenticated_user, tas_client):
-    e = Exception(f'Failed password change for user={authenticated_user.username}',
-                  'This password does not meet the password complexity requirements.')
-    tas_client.change_password.side_effect = e
-
-    response = client.put('/accounts/api/profile/change-password/',
-                          content_type="application/json",
-                          data={"newPW": "1234"})  # Note that its missing current password
-    assert response.status_code == 500
-    assert response.json() == {'message': 'Unable to change password.'}
-
-
-def test_edit_profile_failure(client, authenticated_user, tas_client):
-    # Only failed editing of profile tested.  Need additional unit test: https://jira.tacc.utexas.edu/browse/FP-1034
-    response = client.put('/accounts/api/profile/edit-profile/',
-                          content_type="application/json",
-                          data={})  # Missing required fields
-    assert response.status_code == 500
-    assert response.json() == {'message': 'Unable to update profile.'}
-
-
-def test_edit_profile_unauthenticated(client, tas_client):
-    response = client.put('/accounts/api/profile/edit-profile/',
-                          content_type="application/json",
-                          data={})
-    assert response.status_code == 302  # redirect to login
