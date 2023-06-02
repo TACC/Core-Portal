092ae5b64ddda832940319d8b5786b2d7a4e9cbd
shayanaijaz <shayanaijaz@gmail.com>
2023-06-02 13:35:43 -0500

    update

--- server/portal/apps/auth/models_unit_test.py
+++ server/portal/apps/auth/models_unit_test.py
@@ -1,6 +1,3 @@
-from portal.apps.onboarding.state import SetupState
-from portal.apps.onboarding.models import SetupEvent
-from tapipy.errors import BaseTapyException
 import pytest
 import time
 from datetime import timedelta
@@ -8,7 +5,6 @@ from portal.apps.auth.models import TapisOAuthToken
 
 pytestmark = pytest.mark.django_db
 
-ROUTE = '/workbench/account/'
 
 @pytest.fixture
 def authenticated_user_with_expired_token(authenticated_user):
@@ -33,20 +29,10 @@ def tapis_client_mock(mocker):
 
 
 def test_valid_user(client, authenticated_user_with_valid_token, tapis_client_mock):
-    response = client.get(ROUTE)
-    assert response.status_code == 200
-    assert not tapis_client_mock.client.refresh_tokens.called, 'method should not be called'
+    tapis_oauth = TapisOAuthToken.objects.filter(user=authenticated_user_with_valid_token).select_for_update().get()
+    assert not tapis_oauth.expired
 
 
 def test_expired_user(client, authenticated_user_with_expired_token, tapis_client_mock):
-    response = client.get(ROUTE)
-    assert response.status_code == 200
-    assert tapis_client_mock.refresh_tokens.called, 'method should be called'
-
-    # check that token is updated in model
     tapis_oauth = TapisOAuthToken.objects.filter(user=authenticated_user_with_expired_token).select_for_update().get()
-    assert not tapis_oauth.expired
-    assert authenticated_user_with_expired_token.tapis_oauth.created != tapis_oauth.created
-    assert authenticated_user_with_expired_token.tapis_oauth.access_token != tapis_oauth.access_token
-    assert authenticated_user_with_expired_token.tapis_oauth.refresh_token == tapis_oauth.refresh_token
-
+    assert tapis_oauth.expired
