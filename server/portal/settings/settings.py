4d57595e74cd70bb7546f488b0fc04621abe4a6f
Carrie Arnold <cmarnold@tacc.utexas.edu>
2023-07-10 15:11:17 -0500

    added cache settings and pymemecache dependency

--- server/portal/settings/settings.py
+++ server/portal/settings/settings.py
@@ -251,6 +251,16 @@ DATABASES = {
     }
 }
 
+# Cache
+# https://docs.djangoproject.com/en/3.2/topics/cache/ 
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.memcached.PyMemcacheCache',
+        'LOCATION': '127.0.0.1:11211',
+    }
+}
+
 WEBSOCKET_URL = '/ws/'
 
 # TAS Authentication.
