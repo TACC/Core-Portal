c8049b06116a18daf1c3afc4a9a3c62359f54997
Carrie Arnold <cmarnold@tacc.utexas.edu>
2023-07-13 14:57:43 -0500

    needed a few tweaks to get this working properly

--- server/portal/settings/settings.py
+++ server/portal/settings/settings.py
@@ -257,7 +257,7 @@ DATABASES = {
 CACHES = {
     'default': {
         'BACKEND': 'django.core.cache.backends.memcached.PyMemcacheCache',
-        'LOCATION': '127.0.0.1:11211',
+        'LOCATION': 'memcached:11211',
     }
 }
 
