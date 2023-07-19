6cec3e5f588b448f4f481e3da63f74346e7faa7d
Shayan Khan <shayanaijaz@gmail.com>
2023-07-19 16:24:26 -0500

    task/TV3-139 Cleanup Old Systems Code (#817)
    * removing old system code, commented out soon to be deprecated tests
    
    * linting
    
    * deprecating ProjectSystemSerializer class
    
    * small fix
    
    ---------
    
    Co-authored-by: Sal Tijerina <r.sal.tijerina@gmail.com>

--- server/portal/apps/projects/models/__init__.py
+++ server/portal/apps/projects/models/__init__.py
@@ -2,6 +2,5 @@
 
 from portal.apps.projects.models.base import Project, ProjectId
 from portal.apps.projects.models.metadata import ProjectMetadata
-from portal.apps.projects.models.base import ProjectSystemSerializer
 
-__all__ = ['Project', 'ProjectId', 'ProjectMetadata', 'ProjectSystemSerializer']
+__all__ = ['Project', 'ProjectId', 'ProjectMetadata']
