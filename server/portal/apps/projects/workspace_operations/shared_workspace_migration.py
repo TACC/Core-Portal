ae7940a8e5724dbd76bdfb9fbb62fd931d5f75d1
Sal Tijerina <r.sal.tijerina@gmail.com>
2023-07-19 10:57:19 -0500

    Quick: handle 3dem workspace migrations (#833)
    * handle 3dem workspace migrations
    
    * move print statements to reflect success of migration
    
    * update error handling when calling create_workspace_system

--- server/portal/apps/projects/workspace_operations/shared_workspace_migration.py
+++ server/portal/apps/projects/workspace_operations/shared_workspace_migration.py
@@ -9,6 +9,7 @@ from portal.libs.agave.utils import service_account
 
 from portal.settings.settings_secret import _AGAVE_SUPER_TOKEN as v2_token
 from portal.settings.settings_secret import _AGAVE_TENANT_BASEURL as v2_url
+from portal.settings import settings_custom
 from django.core.exceptions import MultipleObjectsReturned
 from tapipy.errors import NotFoundError, BaseTapyException
 
@@ -21,7 +22,7 @@ ROLE_MAP = {
 
 
 def get_role(project_id, username):
-    system_id = f"{settings.PORTAL_PROJECTS_SYSTEM_PREFIX}.{project_id}"
+    system_id = f"{getattr(settings_custom, '_PORTAL_PROJECTS_SYSTEM_PREFIX_V2', settings.PORTAL_PROJECTS_SYSTEM_PREFIX)}.{project_id}"
     headers = {"Authorization": "Bearer {}".format(v2_token)}
     req = requests.get(f"{v2_url}/systems/v2/{system_id}/roles/{username}", headers=headers)
     if req.status_code != 200:
@@ -31,6 +32,7 @@ def get_role(project_id, username):
 
 
 def migrate_project(project_id):
+    print(f'Beginning migration for project: {project_id}')
     client = service_account()
     system_id = f"{settings.PORTAL_PROJECTS_SYSTEM_PREFIX}.{project_id}"
     try:
@@ -42,19 +44,21 @@ def migrate_project(project_id):
     try:
         owner = v2_project.owner.username
     except AttributeError:
-        pass
-    try:
-        owner = v2_project.pi.username
-    except AttributeError:
-        print('No owner or PI specified')
-        return
+        try:
+            owner = v2_project.pi.username
+        except AttributeError:
+            print('No owner or PI specified')
+            return
 
     try:
         create_workspace_system(client, project_id, v2_project.title, v2_project.description)
     except BaseTapyException as e:
         if 'SYSAPI_SYS_EXISTS' in e.message:
             print('A Tapis V3 workspace already exists for this system.')
-            return
+        else:
+            print('Error creating workspace system')
+            print(e)
+        return
 
     for co_pi in v2_project.co_pis.all():
         v2_role = get_role(project_id, co_pi.username)
@@ -76,9 +80,9 @@ def migrate_project(project_id):
 
     client.systems.changeSystemOwner(systemId=system_id, userName=owner)
 
+    print(f'Successfully migrated project id: {project_id}')
+
 
 def migrate_all_projects():
     for prj in ProjectMetadata.objects.all():
-        print(f'Beginning migration for project: {prj.project_id}')
         migrate_project(prj.project_id)
-        print(f'Successfully migrated project id: {prj.project_id}')
