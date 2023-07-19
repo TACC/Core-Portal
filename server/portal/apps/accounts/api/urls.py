a7edd5603c2eab540013afb1406c72b7d6fbd66c
Van Go <35277477+van-go@users.noreply.github.com>
2023-07-19 14:07:00 -0500

    server-side linting fix

--- server/portal/apps/accounts/api/urls.py
+++ server/portal/apps/accounts/api/urls.py
@@ -8,5 +8,5 @@ from portal.apps.accounts.api.views.systems import SystemKeysView
 app_name = 'portal_accounts_api'
 urlpatterns = [
     re_path(r'^systems/(?P<system_id>[\w.\-\/]+)/keys/?$',
-        SystemKeysView.as_view()),
+            SystemKeysView.as_view()),
 ]
