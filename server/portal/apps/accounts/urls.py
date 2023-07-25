e0a88e52c0bf12e85e5aaff8a87f300169add8b2
Van Go <35277477+van-go@users.noreply.github.com>
2023-07-25 10:48:16 -0500

    urls and gettext_lazy (#838)
    * urls and gettext_lazy
    
    * server-side linting fix
    
    ---------
    
    Co-authored-by: Nathan Franklin <nathan.franklin@gmail.com>
    Co-authored-by: Sal Tijerina <r.sal.tijerina@gmail.com>

--- server/portal/apps/accounts/urls.py
+++ server/portal/apps/accounts/urls.py
@@ -2,7 +2,7 @@
 .. module:: portal.apps.accounts.urls
    :synopsis: Accounts URLs
 """
-from django.conf.urls import url
+from django.conf.urls import re_path
 from django.contrib.auth.views import LogoutView
 from portal.apps.accounts.views import accounts
 from portal.apps.accounts import views
@@ -10,7 +10,7 @@ from portal.apps.accounts import views
 
 app_name = 'portal_accounts'
 urlpatterns = [
-    url(r'^logout/?', LogoutView.as_view(), name='logout'),
-    url(r'^profile/', accounts, name='manage_profile'),
-    url(r'^api/profile/data/', views.get_profile_data, name='get_profile_data'),
+    re_path(r'^logout/?', LogoutView.as_view(), name='logout'),
+    re_path(r'^profile/', accounts, name='manage_profile'),
+    re_path(r'^api/profile/data/', views.get_profile_data, name='get_profile_data'),
 ]
