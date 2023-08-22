b4fe0625c55bade3fe7599e68502dcb61c92d16c
Van Go <35277477+van-go@users.noreply.github.com>
2023-08-22 13:52:29 -0500

    prettier

--- client/src/components/_common/CommonUtils.jsx
+++ client/src/components/_common/CommonUtils.jsx
@@ -1,16 +1,20 @@
 /**
  * Checks that the field is a non-empty string
- * @param {Object} props - 
+ * @param {Object} props -
  * @param {String} propName - name of the property
  * @param {String} componentName - name of the component
  * @returns {String} Message if error, otherwise null
-*/
+ */
 
 export default function isNotEmptyString(props, propName, componentName) {
-    if (!props[propName] || props[propName] !== 'string'|| props[propName].replace(/ /g, '') === '') {
-      return new Error(
-        `No text passed to <${componentName}> prop "${propName}". Validation failed.`
-      );
-    }
-    return null;
+  if (
+    !props[propName] ||
+    props[propName] !== 'string' ||
+    props[propName].replace(/ /g, '') === ''
+  ) {
+    return new Error(
+      `No text passed to <${componentName}> prop "${propName}". Validation failed.`,
+    );
   }
+  return null;
+}
