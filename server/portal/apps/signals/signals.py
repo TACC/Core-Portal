1fc16f5d5e05f33c3ec04d3c318425bbbf5a2e0c
Van Go <35277477+van-go@users.noreply.github.com>
2023-08-30 15:28:56 -0500

    removed commented out code

--- server/portal/apps/signals/signals.py
+++ server/portal/apps/signals/signals.py
@@ -1,7 +1,4 @@
 from django.dispatch import Signal
 
-# Generic event signal. Not in use as of 6.29.20
-# portal_event = Signal(providing_args=['event_type', 'event_data', 'event_users'])
-
 # Changes for Django 3.*
 portal_event = Signal()
