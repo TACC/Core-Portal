a51d3fa8509cafef9e44d87f2ed20dade196e7de
Carrie Arnold <cmarnold@tacc.utexas.edu>
2023-09-05 14:32:18 -0500

    did these changes on a fresh branch, last PR was very stale codebase

--- client/jest.setup.js
+++ client/jest.setup.js
@@ -1,1 +1,11 @@
 jest.mock('@niivue/niivue', () => {});
+
+global.console = {
+  ...console,
+  // uncomment to ignore a specific log level
+  //log: jest.fn(),
+  //debug: jest.fn(),
+  //info: jest.fn(),
+  //warn: jest.fn(),
+  //error: jest.fn(),
+};
