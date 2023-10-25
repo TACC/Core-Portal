c78fefc02cc5b8571af933fac385e8e0b9d9c57e
Asim Regmi <asimregmi.ar@gmail.com>
2023-10-25 16:08:12 -0500

    fixed label name

--- client/src/components/Onboarding/OnboardingAdmin.jsx
+++ client/src/components/Onboarding/OnboardingAdmin.jsx
@@ -234,7 +234,7 @@ OnboardingAdminList.propTypes = {
 const OnboardingAdmin = () => {
   const dispatch = useDispatch();
   const [eventLogModalParams, setEventLogModalParams] = useState(null);
-  const [showIncompleteOnly, setShowIncompleteOnly] = useState(false); // Add state to set the incomplete
+  const [showIncompleteOnly, setShowIncompleteOnly] = useState(false);
 
   const toggleShowIncomplete = () => {
     setShowIncompleteOnly((prev) => !prev);
@@ -314,7 +314,6 @@ const OnboardingAdmin = () => {
                 id="incompleteuser"
                 role="checkbox"
                 aria-label="Show Incomplete Only"
-                name="sasas"
                 tabIndex={0}
                 onClick={toggleShowIncomplete}
               />
