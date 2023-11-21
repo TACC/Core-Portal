e48240ccd41394449f9664052cb1b816575b8536
shayanaijaz <shayanaijaz@gmail.com>
2023-11-21 16:35:10 -0600

    testing webhook fix

--- server/portal/asgi.py
+++ server/portal/asgi.py
@@ -5,12 +5,19 @@ defined in the ASGI_APPLICATION setting.
 
 import os
 import django
-from channels.routing import ProtocolTypeRouter
+from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+from channels.auth import AuthMiddlewareStack
+import portal.apps.notifications.routing
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portal.settings.settings')
 django.setup()
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            portal.apps.notifications.routing.websocket_urlpatterns
+        )
+    ),
 })
--- server/portal/routing.py
+++ server/portal/routing.py
@@ -1,12 +1,12 @@
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
-import portal.apps.notifications.routing
+# from channels.auth import AuthMiddlewareStack
+# from channels.routing import ProtocolTypeRouter, URLRouter
+# import portal.apps.notifications.routing
 
-application = ProtocolTypeRouter({
-    # (http->django views is added by default)
-    'websocket': AuthMiddlewareStack(
-        URLRouter(
-            portal.apps.notifications.routing.websocket_urlpatterns
-        )
-    ),
-})
+# application = ProtocolTypeRouter({
+#     # (http->django views is added by default)
+#     'websocket': AuthMiddlewareStack(
+#         URLRouter(
+#             portal.apps.notifications.routing.websocket_urlpatterns
+#         )
+#     ),
+# })
