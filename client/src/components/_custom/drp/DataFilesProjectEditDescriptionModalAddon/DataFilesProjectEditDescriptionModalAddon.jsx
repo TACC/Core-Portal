656eef6781f4126ca6aeb2789d2edef50c19e13b
shayanaijaz <shayanaijaz@gmail.com>
2024-03-19 15:44:35 -0500

    Added project edit functionality

--- client/src/components/_custom/drp/DataFilesProjectEditDescriptionModalAddon/DataFilesProjectEditDescriptionModalAddon.jsx
+++ client/src/components/_custom/drp/DataFilesProjectEditDescriptionModalAddon/DataFilesProjectEditDescriptionModalAddon.jsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useQuery } from 'react-query';
 import { fetchUtil } from 'utils/fetchUtil';
 import { DynamicForm } from '_common/Form/DynamicForm';
+import { useSelector } from 'react-redux';
+import { useFormikContext } from 'formik'
+
 
 const DataFilesProjectEditDescriptionModalAddon = () => {
+
+  const { setFieldValue } = useFormikContext();
   
   const { data: form, isLoading } = useQuery('form_EDIT_PROJECT', () =>
     fetchUtil({
@@ -13,9 +18,24 @@ const DataFilesProjectEditDescriptionModalAddon = () => {
       },
     })
   );
+
+  const { metadata } = useSelector(state => state.projects)
+
+  useEffect(() => {
+    if (!isLoading && form && metadata) {
+      form.form_fields.forEach(field => {
+        if (metadata.hasOwnProperty(field.name)) {
+          setFieldValue(field.name, metadata[field.name])
+        }
+      });
+    }
+  }, [form])
+
   return (
     <div>
+      {!isLoading && form &&
         <DynamicForm formFields={form?.form_fields ?? []} />
+      }
     </div>
   );
 };
