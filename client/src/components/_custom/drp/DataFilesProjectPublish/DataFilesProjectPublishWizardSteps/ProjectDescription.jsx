c67db85112990556948f71064d269c48199a0c45
shayanaijaz <shayanaijaz@gmail.com>
2024-05-20 12:18:38 -0500

    format date and add keyword support for projects

--- client/src/components/_custom/drp/DataFilesProjectPublish/DataFilesProjectPublishWizardSteps/ProjectDescription.jsx
+++ client/src/components/_custom/drp/DataFilesProjectPublish/DataFilesProjectPublishWizardSteps/ProjectDescription.jsx
@@ -7,6 +7,7 @@ import {
 } from '_common';
 import styles from './DataFilesProjectPublishWizard.module.scss';
 import { useDispatch } from 'react-redux';
+import { formatDate } from 'utils/timeFormat';
 
 const ProjectDescription = ({ project }) => {
   const dispatch = useDispatch();
@@ -34,8 +35,9 @@ const ProjectDescription = ({ project }) => {
       <DescriptionList
         data={{
           Title: project.title,
-          Created: project.created,
+          Created: formatDate(new Date(project.created)),
           Abstract: <ShowMore> {project.description} </ShowMore>,
+          Keywords: project.keywords ?? '',
         }}
         direction={'vertical'}
       />
