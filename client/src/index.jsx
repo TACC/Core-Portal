1475dcbc6ca834085450fb30fb744ec13aece16e
Jake Rosenberg <jrosenberg@tacc.utexas.edu>
2024-10-31 15:12:46 -0500

    update react-query

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,7 +2,7 @@ import React, { Suspense } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import LoadingSpinner from '_common/LoadingSpinner';
-import { QueryClient, QueryClientProvider } from 'react-query';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 const AppRouter = React.lazy(() => import('./components/Workbench'));
 import store from './redux/store';
 
