1958bba289a4c3d1a7a90a6f395dcbca83f1476e
Jacob Lowe <jalowe13@gmail.com>
2024-11-06 14:11:45 -0600

    for each file and manual headers

--- client/src/hooks/datafiles/mutations/useUpload.ts
+++ client/src/hooks/datafiles/mutations/useUpload.ts
@@ -1,6 +1,9 @@
 import { useSelector, useDispatch, shallowEqual } from 'react-redux';
 import { apiClient } from 'utils/apiClient';
+import Cookies from 'js-cookie';
+import truncateMiddle from 'utils/truncateMiddle';
 import { useMutation } from '@tanstack/react-query';
+
 export async function uploadUtil({
   api,
   scheme,
@@ -24,10 +27,16 @@ export async function uploadUtil({
   formData.append('uploaded_file', fileField);
   let url = `/api/datafiles/${api}/upload/${scheme}/${system}/${apiPath}/`;
   url.replace(/\/{2,}/g, '/');
-  const response = await apiClient.put<{ file: FormData; path: string }>(url, {
+  const request = await fetch(url, {
+    method: 'POST',
+    headers: { 'X-CSRFToken': Cookies.get('csrftoken') || '' },
+    credentials: 'same-origin',
     body: formData,
   });
-  return response.data;
+  if (!request.ok) {
+    throw new Error(request.status.toString());
+  }
+  return { file, path: apiPath };
 }
 
 function useUpload() {
@@ -47,50 +56,67 @@ function useUpload() {
   const { mutate } = useMutation({ mutationFn: uploadUtil });
 
   const upload = ({
-    api,
-    scheme,
     system,
     path,
-    file,
-    index,
+    files,
+    reloadCallback,
   }: {
-    api: string;
-    scheme: string;
     system: string;
     path: string;
-    file: FormData;
-    index: string;
+    files: { data: File; id: string }[];
+    reloadCallback: () => void;
   }) => {
-    dispatch({
-      type: 'DATA_FILES_SET_OPERATION_STATUS_BY_KEY',
-      payload: { status: 'UPLOADING', key: index, operation: 'upload' },
-    });
-    mutate(
-      {
-        api,
-        scheme,
-        system,
-        path,
-        file,
-      },
-      {
-        onSuccess: (resp) => {
-          dispatch({
-            type: 'DATA_FILES_SET_OPERATION_STATUS_BY_KEY',
-            payload: { status: 'SUCCESS', key: index, operation: 'upload' },
-          });
-        },
-        onError: () => {
-          dispatch({
-            type: 'DATA_FILES_SET_OPERATION_STATUS_BY_KEY',
-            payload: { status: 'ERROR', key: index, operation: 'upload' },
-          });
-        },
-      }
-    );
-  };
+    const api = 'tapis';
+    const scheme = 'private';
 
+    files.forEach((fileObj) => {
+      const { data: file, id: index } = fileObj;
+      dispatch({
+        type: 'DATA_FILES_SET_OPERATION_STATUS_BY_KEY',
+        payload: { status: 'UPLOADING', key: index, operation: 'upload' },
+      });
+
+      const formData = new FormData();
+      formData.append('uploaded_file', file);
+
+      mutate(
+        {
+          api,
+          scheme,
+          system,
+          path,
+          file: formData,
+        },
+        {
+          onSuccess: () => {
+            dispatch({
+              type: 'DATA_FILES_SET_OPERATION_STATUS_BY_KEY',
+              payload: { status: 'SUCCESS', key: index, operation: 'upload' },
+            });
+            dispatch({ type: 'DATA_FILES_MODAL_CLOSE' });
+          },
+          onError: (error) => {
+            console.log('The Error is', error);
+            dispatch({
+              type: 'DATA_FILES_SET_OPERATION_STATUS_BY_KEY',
+              payload: { status: 'ERROR', key: index, operation: 'upload' },
+            });
+          },
+        }
+      );
+    });
+
+    dispatch({
+      type: 'ADD_TOAST',
+      payload: {
+        message: `${
+          files.length > 1 ? `${files.length} files` : 'File'
+        } uploaded to ${truncateMiddle(path, 20) || '/'}`,
+      },
+    });
+
+    reloadCallback();
+  };
   return { upload, status, setStatus };
 }
-
 export default useUpload;
