640591da3588b58ad5508960356d7c4dcb341cea
Jacob Lowe <jalowe13@gmail.com>
2024-11-06 14:25:06 -0600

    initial typescript

--- client/src/hooks/datafiles/mutations/useCopy.ts
+++ client/src/hooks/datafiles/mutations/useCopy.ts
@@ -7,20 +7,34 @@ function useCopy() {
   const { selectedFiles: selected } = useSelectedFiles();
 
   const status = useSelector(
-    (state) => state.files.operationStatus.copy,
+    (state: any) => state.files.operationStatus.copy,
     shallowEqual
   );
 
-  const setStatus = (newStatus) =>
+  const setStatus = (newStatus: string) =>
     dispatch({
       type: 'DATA_FILES_SET_OPERATION_STATUS',
       payload: { operation: 'copy', status: newStatus },
     });
 
-  const copy = ({ srcApi, destApi, destSystem, destPath, name, callback }) => {
+  const copy = ({
+    srcApi,
+    destApi,
+    destSystem,
+    destPath,
+    name,
+    callback,
+  }: {
+    srcApi: string;
+    destApi: string;
+    destSystem: string;
+    destPath: string;
+    name: string;
+    callback: any;
+  }) => {
     const filteredSelected = selected
-      .filter((f) => status[f.id] !== 'SUCCESS')
-      .map((f) => ({ ...f, api: srcApi }));
+      .filter((f: any) => status[f.id] !== 'SUCCESS')
+      .map((f: any) => ({ ...f, api: srcApi }));
     dispatch({
       type: 'DATA_FILES_COPY',
       payload: {
