9f706ef1f822e46d72bc3a1278d001a74ad8dc2c
Jeff McMillen <jmcmillenmusic@gmail.com>
2024-11-11 16:00:42 -0600

    Update client/src/hooks/datafiles/mutations/useMove.ts
    Co-authored-by: Sal Tijerina <r.sal.tijerina@gmail.com>

--- client/src/hooks/datafiles/mutations/useMove.ts
+++ client/src/hooks/datafiles/mutations/useMove.ts
@@ -109,6 +109,14 @@ function useMove() {
         }
       );
     });
+    dispatch({
+      type: 'DATA_FILES_SET_OPERATION_STATUS',
+      payload: { operation: 'move', status: {} },
+    });
+    dispatch({
+      type: 'DATA_FILES_TOGGLE_MODAL',
+      payload: { operation: 'move', props: {} },
+    });
   };
 
   return { move, status, setStatus };
