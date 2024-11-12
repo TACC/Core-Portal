f090f03e637eb817c742b63a4724846701369159
van-go <35277477+van-go@users.noreply.github.com>
2024-11-12 13:48:23 -0600

    refactor: remove unnecessary comment in drp.sagas.js

--- client/src/redux/sagas/_custom/drp.sagas.js
+++ client/src/redux/sagas/_custom/drp.sagas.js
@@ -50,7 +50,7 @@ function* executeOperation(
           ? `${path}/${file.path.split('/').pop()}`
           : path;
    
-      // Check if the file name has changed. If not, keep the same path.
+      // Check if the file name has changed. If not, keep the same path
       const reloadPath =
         isEdit && file.name !== values.name
           ? newPath.replace(`/${file.name}`, `/${values.name}`)
