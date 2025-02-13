60f123ad65a6301efa31f9138e1f3b7bd4236cda
shayanaijaz <shayanaijaz@gmail.com>
2025-02-13 16:39:04 -0600

    update drp sql utils

--- server/portal/apps/projects/migration_utils/sql_db_utils.py
+++ server/portal/apps/projects/migration_utils/sql_db_utils.py
@@ -10,63 +10,63 @@ def run_query(query, params=None):
         return [dict(zip(columns, row)) for row in cursor.fetchall()]
 
 def query_projects():
-    query = "SELECT * FROM dev.upload_project WHERE access = %s;"
+    query = "SELECT * FROM upload_project WHERE access = %s;"
     params = [2]
     return run_query(query, params)
 
 def query_published_projects():
-    query = "SELECT * FROM dev.upload_project WHERE access = %s;"
+    query = "SELECT * FROM upload_project WHERE access = %s;"
     params = [1]
     return run_query(query, params)
 
 def get_project_by_id(project_id):
-    query = "SELECT * FROM dev.upload_project WHERE id = %s;"
+    query = "SELECT * FROM upload_project WHERE id = %s;"
     params = [project_id]
     return run_query(query, params)
 
 def query_related_publications(project_id):
-    query = "SELECT * FROM dev.upload_publication WHERE project_id = %s;"
+    query = "SELECT * FROM upload_publication WHERE project_id = %s;"
     params = [project_id]
     return run_query(query, params)
 
 def query_authors(project_id):
-    query = "SELECT * FROM dev.upload_collaborator WHERE project_id = %s;"
+    query = "SELECT * FROM upload_collaborator WHERE project_id = %s;"
     params = [project_id]
     return run_query(query, params)
 
 def query_user(user_id):
-    query = "SELECT * FROM dev.upload_myuser WHERE id = %s;"
+    query = "SELECT * FROM upload_myuser WHERE id = %s;"
     params = [user_id]
     return run_query(query, params)
 
 def query_samples(project_id):
-    query = "SELECT * FROM dev.upload_sample WHERE project_id = %s;"
+    query = "SELECT * FROM upload_sample WHERE project_id = %s;"
     params = [project_id]
     return run_query(query, params)
 
 def query_origin_data(project_id, sample_id):
-    query = "SELECT * FROM dev.upload_origin_data WHERE project_id = %s AND sample_id = %s;"
+    query = "SELECT * FROM upload_origin_data WHERE project_id = %s AND sample_id = %s;"
     params = [project_id, sample_id]
     return run_query(query, params)
 
 def query_analysis_data(project_id):
-    query = "SELECT * FROM dev.upload_analysis_data WHERE project_id = %s;"
+    query = "SELECT * FROM upload_analysis_data WHERE project_id = %s;"
     params = [project_id]
     return run_query(query, params)
 
 def query_files(origin_data_id, analysis_data_id):
     if analysis_data_id is None:
-        query = "SELECT * FROM dev.upload_datafile WHERE origin_data_id = %s AND analysis_data_id IS NULL;"
+        query = "SELECT * FROM upload_datafile WHERE origin_data_id = %s AND analysis_data_id IS NULL;"
         params = [origin_data_id]
     elif origin_data_id is None:
-        query = "SELECT * FROM dev.upload_datafile WHERE origin_data_id IS NULL AND analysis_data_id = %s;"
+        query = "SELECT * FROM upload_datafile WHERE origin_data_id IS NULL AND analysis_data_id = %s;"
         params = [analysis_data_id]
     else:
-        query = "SELECT * FROM dev.upload_datafile WHERE origin_data_id = %s AND analysis_data_id = %s;"
+        query = "SELECT * FROM upload_datafile WHERE origin_data_id = %s AND analysis_data_id = %s;"
         params = [origin_data_id, analysis_data_id]
     return run_query(query, params)
 
 def query_advanced_file_metadata(file_id):
-    query = "SELECT * FROM dev.upload_advancedimagefile WHERE datafile_ptr_id = %s;"
+    query = "SELECT * FROM upload_advancedimagefile WHERE datafile_ptr_id = %s;"
     params = [file_id]
     return run_query(query, params)
