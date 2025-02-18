cd9f8449b9924ef469cae8af87b61a3bdb19e3a2
Jake Rosenberg <jrosenberg@tacc.utexas.edu>
2025-02-18 12:41:57 -0600

    Serve published projects at a world-readable /publications URL (#1046)
    Co-authored-by: Shayan Khan <skhan@tacc.utexas.edu>

---
+++ client/src/components/Publications/PublicationDetailPublicView.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import CombinedBreadcrumbs from '../DataFiles/CombinedBreadcrumbs/CombinedBreadcrumbs';
+import DataFilesPreviewModal from '../DataFiles/DataFilesModals/DataFilesPreviewModal';
+import DataFilesProjectCitationModal from '../DataFiles/DataFilesModals/DataFilesProjectCitationModal';
+import DataFilesProjectTreeModal from '../DataFiles/DataFilesModals/DataFilesProjectTreeModal';
+import DataFilesPublicationAuthorsModal from '../DataFiles/DataFilesModals/DataFilesPublicationAuthorsModal';
+import DataFilesShowPathModal from '../DataFiles/DataFilesModals/DataFilesShowPathModal';
+import DataFilesViewDataModal from '../DataFiles/DataFilesModals/DataFilesViewDataModal';
+import DataFilesProjectFileListing from '../DataFiles/DataFilesProjectFileListing/DataFilesProjectFileListing';
+
+function PublicationDetailPublicView({params}) {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        padding: '10px',
+      }}
+    >
+      <CombinedBreadcrumbs
+        api="tapis"
+        scheme="projects"
+        system={params.system}
+        path={params.path || ''}
+        section="FilesListing"
+        basePath="/publications"
+      />
+      <DataFilesProjectFileListing
+        rootSystem={params.root_system}
+        system={params.system}
+        path={params.path || '/'}
+      />
+      <DataFilesPreviewModal />
+      <DataFilesShowPathModal />
+      <DataFilesProjectTreeModal />
+      <DataFilesPublicationAuthorsModal />
+      <DataFilesProjectCitationModal />
+      <DataFilesViewDataModal />
+    </div>
+  );
+}
+
+export default PublicationDetailPublicView;
