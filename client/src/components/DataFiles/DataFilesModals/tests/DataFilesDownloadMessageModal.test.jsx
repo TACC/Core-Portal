78bd2233ab69d96f0e154fe3c4967df2dc8d9e1f
Garrett Edmonds <edmondsgarrett@gmail.com>
2025-04-22 12:10:03 -0500

    Unit test fixes

--- client/src/components/DataFiles/DataFilesModals/tests/DataFilesDownloadMessageModal.test.jsx
+++ client/src/components/DataFiles/DataFilesModals/tests/DataFilesDownloadMessageModal.test.jsx
@@ -7,43 +7,10 @@ import { fireEvent, screen, waitFor } from '@testing-library/react';
 import { vi } from 'vitest';
 
 const mockStore = configureStore();
-const initialMockState = {
-  files: DataFilesDownloadMessageModalFixture,
-};
 
 // Establish a boolean that checks for a folder among selectedFiles
 let containsFolder = false;
 // Create test files for all tests
-const testFileSize1 = 1.5 * 1024 * 1024 * 1024;
-const testFileSize2 = 1 * 1024 * 1024 * 1024;
-const testFile1 = {
-  name: 'test1.txt',
-  type: 'file',
-  length: testFileSize1,
-  path: '/test1.txt',
-  id: 123,
-};
-const testFile2 = {
-  name: 'test2.txt',
-  type: 'file',
-  length: testFileSize1,
-  path: '/test2.txt',
-  id: 456,
-};
-const testFile3 = {
-  name: 'test3.txt',
-  type: 'file',
-  length: testFileSize2,
-  path: '/test3.txt',
-  id: 234,
-};
-// Create a test folder
-const testFolder = {
-  name: 'testFolder',
-  type: 'folder',
-  path: '/testFolder',
-  id: 789,
-};
 
 describe('DataFilesDownloadMessageModal', () => {
   beforeEach(() => {
@@ -52,7 +19,7 @@ describe('DataFilesDownloadMessageModal', () => {
   });
 
   it('renders the data files download message modal', () => {
-    const store = mockStore(initialMockState);
+    const store = mockStore(DataFilesDownloadMessageModalFixture);
     const { getAllByText } = renderComponent(
       <DataFilesDownloadMessageModal />,
       store
@@ -78,27 +45,11 @@ describe('DataFilesDownloadMessageModal', () => {
       <DataFilesDownloadMessageModal />,
       // Create the store
       mockStore({
+        ...DataFilesDownloadMessageModalFixture,
         files: {
-          modals: {
-            downloadMessage: true,
-          },
-          params: {
-            FilesListing: {
-              system: 'frontera.home.username',
-              path: 'home/username',
-              scheme: 'private',
-            },
-          },
-          listing: {
-            FilesListing: [testFile1, testFile2, testFile3, testFolder],
-          },
-          selected: { FilesListing: [3] },
-          operationStatus: {
-            compress: true,
-          },
+          ...DataFilesDownloadMessageModalFixture.files,
+          selected: { FilesListing: [4] },
         },
-        projects: { metadata: [] },
-        authenticatedUser: { user: { username: 'testuser' } },
       })
     );
     // Click on the Compress button to try and download the folder
@@ -132,27 +83,11 @@ describe('DataFilesDownloadMessageModal', () => {
     const { getByText } = renderComponent(
       <DataFilesDownloadMessageModal />,
       mockStore({
+        ...DataFilesDownloadMessageModalFixture,
         files: {
-          modals: {
-            downloadMessage: true,
-          },
-          params: {
-            FilesListing: {
-              system: 'frontera.home.username',
-              path: 'home/username',
-              scheme: 'private',
-            },
-          },
-          listing: {
-            FilesListing: [testFile1, testFile2, testFile3, testFolder],
-          },
-          selected: { FilesListing: [0, 1] },
-          operationStatus: {
-            compress: true,
-          },
+          ...DataFilesDownloadMessageModalFixture.files,
+          selected: { FilesListing: [1, 2] },
         },
-        projects: { metadata: [] },
-        authenticatedUser: { user: { username: 'testuser' } },
       })
     );
     // Click on the Compress button to try and download the file
@@ -187,27 +122,11 @@ describe('DataFilesDownloadMessageModal', () => {
         <DataFilesDownloadMessageModal />,
         // Create the store
         mockStore({
+          ...DataFilesDownloadMessageModalFixture,
           files: {
-            modals: {
-              downloadMessage: true,
-            },
-            params: {
-              FilesListing: {
-                system: 'frontera.home.username',
-                path: 'home/username',
-                scheme: 'private',
-              },
-            },
-            listing: {
-              FilesListing: [testFile1, testFile2, testFile3, testFolder],
-            },
-            selected: { FilesListing: [2] },
-            operationStatus: {
-              compress: true,
-            },
+            ...DataFilesDownloadMessageModalFixture.files,
+            selected: { FilesListing: [3] },
           },
-          projects: { metadata: [] },
-          authenticatedUser: { user: { username: 'testuser' } },
         })
       );
       // Click on the Compress button to try and download the folder
