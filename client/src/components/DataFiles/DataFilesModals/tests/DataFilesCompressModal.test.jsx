cc67ac8a322d398dcc6d41f893b13ed903b295a8
Garrett Edmonds <edmondsgarrett@gmail.com>
2025-05-09 16:34:24 -0500

    Linting for new testing

--- client/src/components/DataFiles/DataFilesModals/tests/DataFilesCompressModal.test.jsx
+++ client/src/components/DataFiles/DataFilesModals/tests/DataFilesCompressModal.test.jsx
@@ -7,52 +7,43 @@ import DataFilesCompressModal from '../DataFilesCompressModal';
 const mockStore = configureStore();
 
 describe('DataFilesCompressModal', () => {
-    it('renders the compress modal', () => {
-        const store = mockStore(DataFilesCompressModalFixture);
-        const { getAllByText } = renderComponent(
-            <DataFilesCompressModal />,
-            store
-        );
+  it('renders the compress modal', () => {
+    const store = mockStore(DataFilesCompressModalFixture);
+    const { getAllByText } = renderComponent(<DataFilesCompressModal />, store);
 
-        expect(getAllByText(/Compressed File Name/)).toBeDefined();
+    expect(getAllByText(/Compressed File Name/)).toBeDefined();
+  });
+
+  it('disables form when submitting compress job', () => {
+    const store = mockStore({
+      ...DataFilesCompressModalFixture,
+      files: {
+        ...DataFilesCompressModalFixture.files,
+        operationStatus: { compress: { type: 'RUNNING' } },
+      },
     });
+    const { getByLabelText, getByText } = renderComponent(
+      <DataFilesCompressModal />,
+      store
+    );
 
-    it('disables form when submitting compress job', () => {
-        const store = mockStore({
-            ...DataFilesCompressModalFixture,
-            files: {
-              ...DataFilesCompressModalFixture.files,
-              operationStatus: { compress: { type: 'RUNNING' } },
-            },
-        });
-        const { getByLabelText, getByText } = renderComponent(
-            <DataFilesCompressModal />,
-            store
-        );
+    const compressInputField = getByLabelText('Compressed File Name');
+    const compressButton = getByText('Compress').closest('button');
+    expect(compressInputField.toBeDisabled);
+    expect(compressButton.toBeDisabled);
+  });
 
-        const compressInputField = getByLabelText('Compressed File Name');
-        const compressButton = getByText('Compress').closest('button');
-        expect(compressInputField.toBeDisabled);
-        expect(compressButton.toBeDisabled);
+  it('displays error state on error status', () => {
+    const store = mockStore({
+      ...DataFilesCompressModalFixture,
+      files: {
+        ...DataFilesCompressModalFixture.files,
+        operationStatus: { compress: { type: 'ERROR' } },
+      },
     });
+    const { getByTestId } = renderComponent(<DataFilesCompressModal />, store);
 
-    it('displays error state on error status', () => {
-        const store = mockStore({
-            ...DataFilesCompressModalFixture,
-            files: {
-              ...DataFilesCompressModalFixture.files,
-              operationStatus: { compress: { type: 'ERROR' } },
-            },
-        });
-        const { getByTestId } = renderComponent(
-            <DataFilesCompressModal />,
-            store
-        );
-
-        const compressErrorIcon = getByTestId('icon-before');
-        expect(compressErrorIcon.toBeVisible);
-
-    })
-
-
+    const compressErrorIcon = getByTestId('icon-before');
+    expect(compressErrorIcon.toBeVisible);
+  });
 });
