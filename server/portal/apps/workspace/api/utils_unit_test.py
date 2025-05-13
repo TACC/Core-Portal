ad5a30f2b9d847b6c92eedd546a72dd0fd062a88
Sal Tijerina <r.sal.tijerina@gmail.com>
2025-05-13 17:18:40 -0500

    linting; adjust tests

--- server/portal/apps/workspace/api/utils_unit_test.py
+++ server/portal/apps/workspace/api/utils_unit_test.py
@@ -1,59 +1,101 @@
-from django.conf import settings
-from portal.apps.workspace.api.views import AppsTrayView
-from portal.apps.workspace.models import AppTrayCategory
-from portal.apps.workspace.models import JobSubmission
-import json
-import os
-import pytest
 from tapipy.tapis import TapisResult
-from tapipy.errors import InternalServerError, UnauthorizedError
-from django.core.management import call_command
-from mock import MagicMock, patch
+from tapipy.errors import UnauthorizedError
+from mock import patch
 from portal.apps.workspace.api.utils import (
     push_keys_required_if_not_credentials_ensured,
-    system_credentials_ok,
-    should_push_keys,
-    create_system_credentials,
 )
 
 
-@patch("portal.apps.workspace.api.utils.system_credentials_ok")
-@patch("portal.apps.workspace.api.utils.should_push_keys")
-@patch("portal.apps.workspace.api.utils.create_system_credentials")
-def test_push_keys_required_if_not_credentials_ensured_no_keys_required(
-    mock_create_system_credentials, mock_should_push_keys, mock_system_credentials_ok, authenticated_user, mock_tapis_client
+def test_push_keys_required_if_not_credentials_ensured_successful_credential_creation(
+    authenticated_user, mock_tapis_client
 ):
-    mock_system_credentials_ok.return_value = False
-    mock_should_push_keys.return_value = False
+    """
+    Test that the push_keys_required_if_not_credentials_ensured function returns False when the user has system credentials
+    and the system does not require keys to be pushed.
+    """
     system = {
-        'host': 'frontera.tacc.utexas.edu',
-        'defaultAuthnMethod': 'PKI_KEYS',
+        "id": "test_system",
+        "host": "frontera.tacc.utexas.edu",
+        "defaultAuthnMethod": "TMS_KEYS",
     }
     tapis_system = TapisResult(**system)
 
+    mock_tapis_client.systems.checkUserCredential.side_effect = UnauthorizedError()
     mock_tapis_client.systems.getSystem.return_value = tapis_system
+    mock_tapis_client.systems.createUserCredential.return_value = {
+        "result": None,
+        "status": "success",
+        "message": "SYSAPI_CRED_UPDATED Credential updated.",
+        "version": "1.8.3",
+        "commit": "9a57c9d7",
+        "build": "2025-04-29T13:47:25Z",
+        "metadata": None,
+    }
 
-    result = push_keys_required_if_not_credentials_ensured("test_system", authenticated_user)
+    result = push_keys_required_if_not_credentials_ensured(
+        "test_system", authenticated_user
+    )
 
     assert result is False
-    mock_system_credentials_ok.assert_called_once_with("test_system", authenticated_user)
-    mock_should_push_keys.assert_called_once_with(tapis_system)
-    mock_create_system_credentials.assert_called_once_with(
-        mock_tapis_client, authenticated_user.username, "test_system", createTmsKeys=True
+    mock_tapis_client.systems.createUserCredential.assert_called_once_with(
+        systemId="test_system",
+        userName=authenticated_user.username,
+        createTmsKeys=True,
+        skipCredentialCheck=False,
     )
 
 
-@patch("portal.apps.workspace.api.utils.system_credentials_ok")
 @patch("portal.apps.workspace.api.utils.should_push_keys")
 @patch("portal.apps.workspace.api.utils.create_system_credentials")
 def test_push_keys_required_if_not_credentials_ensured_credentials_ok(
-    mock_create_system_credentials, mock_should_push_keys, mock_system_credentials_ok, authenticated_user, mock_tapis_client
+    mock_create_system_credentials,
+    mock_should_push_keys,
+    authenticated_user,
+    mock_tapis_client,
 ):
-    mock_system_credentials_ok.return_value = True
+    """
+    Test that the function returns False when the user has valid system credentials.
+    """
+    mock_tapis_client.systems.checkUserCredential.return_value = {
+        "result": None,
+        "status": "success",
+        "message": "SYSAPI_CRED_OK Credential validation succeeded.",
+        "version": "1.8.3",
+        "commit": "9a57c9d7",
+        "build": "2025-04-29T13:47:25Z",
+        "metadata": None,
+    }
 
-    result = push_keys_required_if_not_credentials_ensured("test_system", authenticated_user)
+    result = push_keys_required_if_not_credentials_ensured(
+        "test_system", authenticated_user
+    )
 
     assert result is False
-    mock_system_credentials_ok.assert_called_once_with("test_system", authenticated_user)
     mock_should_push_keys.assert_not_called()
     mock_create_system_credentials.assert_not_called()
+
+
+def test_push_keys_required_if_not_credentials_ensured_push_keys_required(
+    authenticated_user, mock_tapis_client
+):
+    """
+    Test that the push_keys_required_if_not_credentials_ensured function
+    returns True when the user does not have system credentials
+    and the system requires keys to be pushed.
+    """
+    system = {
+        "id": "test_system",
+        "host": "frontera.tacc.utexas.edu",
+        "defaultAuthnMethod": "PKI_KEYS",
+    }
+    tapis_system = TapisResult(**system)
+
+    mock_tapis_client.systems.checkUserCredential.side_effect = UnauthorizedError()
+    mock_tapis_client.systems.getSystem.return_value = tapis_system
+
+    result = push_keys_required_if_not_credentials_ensured(
+        "test_system", authenticated_user
+    )
+
+    assert result is True
+    mock_tapis_client.systems.createUserCredential.assert_not_called()
