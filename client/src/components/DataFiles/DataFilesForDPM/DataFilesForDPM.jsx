a84f12b1004962df6e9e297ac696635f9e1058a4
Wesley B <62723358+wesleyboar@users.noreply.github.com>
2025-06-27 15:31:53 -0500

    fix: DataFilesForDPM.global.css bleed

--- client/src/components/DataFiles/DataFilesForDPM/DataFilesForDPM.jsx
+++ client/src/components/DataFiles/DataFilesForDPM/DataFilesForDPM.jsx
@@ -1,12 +1,32 @@
-import { React } from 'react';  // Remove useEffect
+import { React, useEffect } from 'react';
 import CMSWrapper from '_common/CMSWrapper';
 
 import DataFilesForDPMBreadcrumbs from './DataFilesForDPMBreadcrumbs';
 import DataFilesForDPMBrowse from './DataFilesForDPMBrowse';
 
-import './DataFilesForDPM.global.css';
-
 function DataGallery() {
+  useEffect(() => {
+    // To load external CSS and unload on umount
+    // FAQ: As ….global.css, styles would remain after unmount;
+    //      effect seen on system-status/ table margin bottom
+    const cssUrls = [
+      'https://cdn.jsdelivr.net/gh/TACC/Core-CMS-Custom@5717c8d/digitalrocks_assets/css/cms.css',
+      'https://cdn.jsdelivr.net/gh/TACC/Core-CMS-Custom@5717c8d/digitalrocks_assets/css/for-core-styles.css'
+    ];
+
+    const links = cssUrls.map(url => {
+      const link = document.createElement('link');
+      link.rel = 'stylesheet';
+      link.href = url;
+      document.head.appendChild(link);
+      return link;
+    });
+
+    return () => {
+      links.forEach(link => link.remove());
+    };
+  }, []);
+
   return (
     <CMSWrapper>
       <DataFilesForDPMBreadcrumbs />
