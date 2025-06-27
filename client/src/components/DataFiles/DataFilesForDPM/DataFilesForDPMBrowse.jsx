c6edca90d8c491513d9a2aa0b2bd213f5a20325d
Wesley B <62723358+wesleyboar@users.noreply.github.com>
2025-06-27 16:46:39 -0500

    chore: comment format

--- client/src/components/DataFiles/DataFilesForDPM/DataFilesForDPMBrowse.jsx
+++ client/src/components/DataFiles/DataFilesForDPM/DataFilesForDPMBrowse.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-/* TODO: Replace use of this with component(s) from WC-219 */
+// TODO: Replace use of this with component(s) from WC-219
 export default function DataFilesForDPMBrowse() {
   return (
     <div className="o-section">
