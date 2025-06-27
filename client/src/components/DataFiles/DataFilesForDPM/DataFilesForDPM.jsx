302ec3dbdf2cff5ecacbbe4ec77a008798700242
Wesley B <62723358+wesleyboar@users.noreply.github.com>
2025-06-27 17:45:32 -0500

    feat: navigate sample content

--- client/src/components/DataFiles/DataFilesForDPM/DataFilesForDPM.jsx
+++ client/src/components/DataFiles/DataFilesForDPM/DataFilesForDPM.jsx
@@ -1,14 +1,43 @@
-import { React, useEffect } from 'react';
+import { React, useEffect, useState } from 'react';
 import CMSWrapper from '_common/CMSWrapper';
 
 import DataFilesForDPMBreadcrumbs from './DataFilesForDPMBreadcrumbs';
+import DataFilesForDPMViews from './DataFilesForDPMViews';
+
 import DataFilesForDPMBrowse from './DataFilesForDPMBrowse';
+import DataFilesForDPMRead from './DataFilesForDPMRead';
+import DataFilesForDPMData from './DataFilesForDPMData';
+
+import style from './DataFilesForDPM.module.css';
+
+const tabs = [
+  {
+    label: 'Browse',
+    view: 'browse',
+    id: 'tab-browse',
+    contentId: 'content-browse',
+    Content: DataFilesForDPMBrowse,
+  },
+  {
+    label: 'Read',
+    view: 'read',
+    id: 'tab-read',
+    contentId: 'content-read',
+    Content: DataFilesForDPMRead,
+  },
+  {
+    label: 'Data',
+    view: 'Data',
+    id: 'tab-Data',
+    contentId: 'content-data',
+    Content: DataFilesForDPMData,
+  },
+];
 
 function DataFilesForDPM() {
+  const [view, setView] = useState('browse');
+
   useEffect(() => {
-    // FAQ: If @import'ed via DataFilesForDPM.global.css,
-    //      styles would remain active after component unmount
-    //      (the effect was seen on system-status/ table margin bottom)
     return CMSWrapper.useDynamicStylesheets(
       [
         'https://cdn.jsdelivr.net/gh/TACC/Core-CMS-Custom@5717c8d/digitalrocks_assets/css/cms.css',
@@ -21,7 +50,13 @@ function DataFilesForDPM() {
   return (
     <CMSWrapper>
       <DataFilesForDPMBreadcrumbs />
-      <DataFilesForDPMBrowse />
+      <DataFilesForDPMViews
+        navClassName={style.nav}
+        contentClassName={style.content}
+        tabs={tabs}
+        view={view}
+        setView={setView}
+      />
     </CMSWrapper>
   );
 }
