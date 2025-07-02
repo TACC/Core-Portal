2de31ffaf028486155455eeafb786978f7c99a96
Frank Netscher <fnetscher@tacc.utexas.edu>
2025-07-02 12:17:24 -0500

    Addressing code review suggestions: removing unnecessary typecasting and overloaded variables.

--- server/portal/apps/accounts/views.py
+++ server/portal/apps/accounts/views.py
@@ -26,12 +26,21 @@ class LogoutView(DjangoLogoutView):
         token = str(request.user.tapis_oauth.access_token)
 
         logger.info('Attempting to revoke JWT token for user: ' + str(request.user))
+
+        token = request.user.tapis_oauth.access_token
+
+        logger.info('Attempting to revoke JWT token for user: ' + request.user.username)
+
         self.revoke_token(token)
 
         logout(request)
 
         redirect = getattr(settings, 'LOGOUT_REDIRECT_URL', '/')
         response = HttpResponseRedirect(redirect)
+
+        redirect_uri = getattr(settings, 'LOGOUT_REDIRECT_URL', '/')
+        response = HttpResponseRedirect(redirect_uri)
+
         return response
 
     def revoke_token(self, token):
