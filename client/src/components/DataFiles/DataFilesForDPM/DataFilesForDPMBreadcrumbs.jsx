e0916df490c42514759fe5c6dc209ce4fe1d0326
Wesley B <62723358+wesleyboar@users.noreply.github.com>
2025-07-07 18:21:33 -0500

    refactor: delete sample components + their styles

--- client/src/components/DataFiles/DataFilesForDPM/DataFilesForDPMBreadcrumbs.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-import { CMSBreadcrumbs } from '_common';
-
-/** To load CMS breadcrumbs with Data Files data */
-export default function DataFilesForDPMBreadcrumbs() {
-  return <CMSBreadcrumbs />;
-}
