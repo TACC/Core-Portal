d0812183520542b6dd268a21ef7ad8df6dc28181
shayanaijaz <shayanaijaz@gmail.com>
2025-07-15 15:34:18 -0500

    potential solution for disabling cms styles in workbench

--- client/src/components/Workbench/Workbench.jsx
+++ client/src/components/Workbench/Workbench.jsx
@@ -56,6 +56,11 @@ function Workbench() {
     shallowEqual
   );
 
+  // Ensure CMS styles are deactivated when entering workbench
+  useEffect(() => {
+    window.dispatchEvent(new CustomEvent('cms-styles-deactivated'));
+  }, []);
+
   // Get systems and any other initial data we need from the backend
   useEffect(() => {
     dispatch({
