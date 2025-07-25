95de2b73ca3d6004bf8783e95731ed7fea3779f9
Frank Netscher <fnetscher@tacc.utexas.edu>
2025-07-25 14:22:15 -0500

    Added username to logging per feedback

--- server/portal/apps/accounts/views.py
+++ server/portal/apps/accounts/views.py
@@ -24,7 +24,7 @@ logger = logging.getLogger(__name__)
 class LogoutView(DjangoLogoutView):
     def dispatch(self, request, *args, **kwargs):
         token = request.user.tapis_oauth.access_token
-        logger.info('Attempting logout via Tapis with token "%s"' % token[:8].ljust(len(token), '-'))
+        logger.info('The user %s is attempting to logout via Tapis with token "%s"' % (request.user.username, token[:8].ljust(len(token), '-')))
 
         logout_endpoint = f"{settings.TAPIS_TENANT_BASEURL}/v3/oauth2/logout?redirect_url=https://{request.get_host()}/{settings.LOGOUT_REDIRECT_URL}"
 
