6b41462026bea40b155dabda81db8ef10fa51851
Sal Tijerina <r.sal.tijerina@gmail.com>
2026-04-02 15:47:26 -0500

    WI-414: default to $HOME for file listings

--- server/portal/apps/datafiles/utils.py
+++ server/portal/apps/datafiles/utils.py
@@ -1,6 +1,10 @@
+import logging
+from tapipy.errors import InternalServerError
 from portal.apps.notifications.models import Notification
 from portal.apps.users.utils import get_user_data
 from portal.apps.auth.models import TapisOAuthToken
+
+logger = logging.getLogger(__name__)
 
 NOTIFY_ACTIONS = ["move", "copy", "rename", "trash", "mkdir", "upload", "makepublic"]
 
@@ -17,18 +21,21 @@ def notify(username, operation, status, extra):
     Notification.objects.create(**event_data)
 
 
-def evaluate_datafiles_storage_system(tapis: TapisOAuthToken, system: dict) -> dict:
+def evaluate_datafiles_storage_system(
+    tapis: TapisOAuthToken, system: dict, default_host_eval: str = None
+) -> dict:
     """Evaluate storage system homeDir or hostEval for user
 
     Args:
         tapis (TapisOAuthToken): Tapis OAuth token object
         system (dict): Storage system definition
+        default_host_eval (str, optional): Default environment variable name to evaluate for homeDir if hostEval is not provided.
 
     Returns:
         dict: Evaluated storage system definition
     """
 
-    if "homeDir" in system:
+    if "homeDir" in system and "{tasdir}" in system["homeDir"]:
         tasdir = get_user_data(tapis.user.username)["homeDirectory"]
         evaluated_system = {
             **system,
@@ -36,12 +43,19 @@ def evaluate_datafiles_storage_system(tapis: TapisOAuthToken, system: dict) -> d
                 tasdir=tasdir, username=tapis.user.username
             ),
         }
-    elif "hostEval" in system:
+    elif "hostEval" in system or default_host_eval:
+        try:
+            home_dir = tapis.client.systems.hostEval(
+                systemId=system["system"],
+                envVarName=system.get("hostEval", default_host_eval),
+            ).name
+        except InternalServerError:
+            # If hostEval fails, return system without evaluation
+            return system
+
         evaluated_system = {
             **system,
-            "homeDir": tapis.client.systems.hostEval(
-                systemId=system["system"], envVarName=system["hostEval"]
-            ).name,
+            "homeDir": home_dir,
         }
     else:
         evaluated_system = system
@@ -49,18 +63,24 @@ def evaluate_datafiles_storage_system(tapis: TapisOAuthToken, system: dict) -> d
     return evaluated_system
 
 
-def evaluate_datafiles_storage_systems(tapis: TapisOAuthToken, systems: list) -> list:
+def evaluate_datafiles_storage_systems(
+    tapis: TapisOAuthToken, systems: list, default_host_eval: str = None
+) -> list:
     """Evaluate storage systems homeDir or hostEval for user
 
     Args:
         tapis (TapisOAuthToken): Tapis OAuth token object
         systems (list): List of storage system definitions
+        default_host_eval (str, optional): Default environment variable name to evaluate for homeDir if hostEval is not provided.
 
     Returns:
         list: List of evaluated storage system definitions
     """
 
-    return [evaluate_datafiles_storage_system(tapis, system) for system in systems]
+    return [
+        evaluate_datafiles_storage_system(tapis, system, default_host_eval)
+        for system in systems
+    ]
 
 
 def get_user_storage_systems(tapis: TapisOAuthToken) -> list:
@@ -71,20 +91,23 @@ def get_user_storage_systems(tapis: TapisOAuthToken) -> list:
     Returns:
         list: List of evaluated storage system definitions
     """
-
+    logger.info("Getting user storage systems for user: %s", tapis.user.username)
     systems = tapis.client.systems.getSystems(
         listType="ALL", limit="-1", select="id,notes", orderBy="id"
     )
 
-    return [
+    available_systems = [
         {
             "name": system.notes.get("label", system.notes.get("title", system.id)),
             "system": system.id,
             "scheme": "private",
             "api": "tapis",
-            "homeDir": "/",
             "icon": None,
             "default": False,
         }
         for system in systems
     ]
+
+    return evaluate_datafiles_storage_systems(
+        tapis, available_systems, default_host_eval="HOME"
+    )
