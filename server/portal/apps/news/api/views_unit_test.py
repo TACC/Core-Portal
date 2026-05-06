41c5f3de165dffb8df8854b34fbef94efc26aed7
shayanaijaz <shayanaijaz@gmail.com>
2026-05-06 16:22:01 -0500

    update tests and linting

--- server/portal/apps/news/api/views_unit_test.py
+++ server/portal/apps/news/api/views_unit_test.py
@@ -1,7 +1,7 @@
 from unittest.mock import patch
 
 
-@patch('portal.apps.news.views.requests.get')
+@patch('portal.apps.news.api.views.requests.get')
 def test_api_news_success_without_sanitize(mock_get, client, authenticated_user):
     mock_get.return_value.json.return_value = {
         'status': 'success',
@@ -22,7 +22,7 @@ def test_api_news_success_without_sanitize(mock_get, client, authenticated_user)
     assert body['response'][0]['updates'][0]['content'] == '<div>Update</div>'
 
 
-@patch('portal.apps.news.views.requests.get')
+@patch('portal.apps.news.api.views.requests.get')
 def test_api_news_success_with_sanitize(mock_get, client, authenticated_user):
     mock_get.return_value.json.return_value = {
         'status': 'success',
@@ -43,7 +43,7 @@ def test_api_news_success_with_sanitize(mock_get, client, authenticated_user):
     assert body['response'][0]['updates'][0]['content'] == 'Update'
 
 
-@patch('portal.apps.news.views.requests.get')
+@patch('portal.apps.news.api.views.requests.get')
 def test_api_news_failure_from_tas(mock_get, client, authenticated_user):
     mock_get.return_value.json.return_value = {
         'status': 'error',
