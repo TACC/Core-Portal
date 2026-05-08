7bb827d6bbc460d6af786eb7f2b70dfcb9b5fa3f
Sal Tijerina <r.sal.tijerina@gmail.com>
2026-05-08 14:10:07 -0500

    update push keys message; handle select field type for queue scheduler options

--- client/src/components/DataFiles/DataFilesTable/DataFilesTable.jsx
+++ client/src/components/DataFiles/DataFilesTable/DataFilesTable.jsx
@@ -169,7 +169,7 @@ const DataFilesTablePlaceholder = ({ section, data }) => {
                 href="#"
                 onClick={pushKeys}
               >
-                push your keys
+                create your system Tapis system credentials.
               </a>
             </span>
           );
