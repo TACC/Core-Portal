6f038c6a662ad35f7802be7badb66456bcd2d8b0
Frank Netscher <fnetscher@tacc.utexas.edu>
2026-05-15 15:35:43 -0500

    Added part to allocation check that validates if a user has an allocation with access to a specific machine.

--- server/portal/apps/onboarding/steps/allocation.py
+++ server/portal/apps/onboarding/steps/allocation.py
@@ -1,7 +1,7 @@
 from portal.apps.onboarding.steps.abstract import AbstractStep
 from portal.apps.onboarding.state import SetupState
 from portal.apps.users.utils import get_allocations
-
+from django.conf import settings
 
 class AllocationStep(AbstractStep):
     def __init__(self, user):
@@ -35,4 +35,23 @@ class AllocationStep(AbstractStep):
                 """User {0} does not have any allocations""".format(self.user.username),
             )
         else:
+            if self.settings.get("expected_hosts"):
+                # checking if expected hosts are included in allocation hosts
+                expected_hosts = self.settings["expected_hosts"]
+                hosts = [
+                    h
+                    for h in allocations["hosts"].keys()
+                    if h.lower() in expected_hosts
+                ]
+
+                if not hosts:
+                    self.state = SetupState.FAILED
+                    self.log(
+                        "User {0} does not have active allocations on any expected hosts: {1}".format(
+                            self.user.username, expected_hosts
+                        )
+                    )
+                    return
+                self.log("Expected host allocations found: {0}".format(hosts))
+
             self.complete("Allocations retrieved", data=allocations)
