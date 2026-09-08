f1fa27a53fcae776a0e5d45b267e613220e3a16a
jgentle <jgentle@tacc.utexas.edu>
2026-09-08 17:47:16 -0500

    Missing import.

--- server/portal/apps/public_data/urls.py
+++ server/portal/apps/public_data/urls.py
@@ -8,7 +8,7 @@ import re
 from django.conf import settings
 from django.urls import re_path
 
-from portal.apps.public_data.views import IndexView
+from portal.apps.public_data.views import IndexView, DataciteJsonPreviewView
 
 app_name = "public_data"
 
