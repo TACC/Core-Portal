f72c06dac8a1f3a7cace5176c4ef71a6b19a34c9
Jake Rosenberg <jrosenberg@tacc.utexas.edu>
2026-09-16 10:50:51 -0500

    workspace config and CSS fix for loading spinner

--- client/vitest.setup.ts
+++ client/vitest.setup.ts
@@ -2,6 +2,6 @@ import { beforeAll, afterEach, afterAll } from 'vitest';
 import { server } from '@tacc/test-fixtures';
 import '@testing-library/jest-dom';
 
-beforeAll(() => server.listen());
+beforeAll(() => server.listen({ onUnhandledRequest: 'error' }));
 afterEach(() => server.resetHandlers());
 afterAll(() => server.close());
